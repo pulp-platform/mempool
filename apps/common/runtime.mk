265266886caad7a8c894ac439ed5d152604efa2f
Matheus Cavalcante <matheusd@iis.ee.ethz.ch>
2020-02-07 09:44:14 +0100

    [apps] Fix compilation of hello_world

--- apps/common/runtime.mk
+++ apps/common/runtime.mk
@@ -4,8 +4,9 @@ LLVM_INSTALL_DIR   ?= $(INSTALL_DIR)/llvm
 HALIDE_INSTALL_DIR ?= $(INSTALL_DIR)/halide
 
 RISCV_XLEN    ?= 32
-RISCV_ABI     ?= rv$(RISCV_XLEN)im
-RISCV_PREFIX  ?= riscv$(RISCV_XLEN)-unknown-elf-
+RISCV_ARCH    ?= rv$(RISCV_XLEN)im
+RISCV_ABI     ?= ilp32
+RISCV_PREFIX  ?= $(GCC_INSTALL_DIR)/bin/riscv$(RISCV_XLEN)-unknown-elf-
 RISCV_CC      ?= $(RISCV_PREFIX)gcc
 RISCV_CXX     ?= $(RISCV_PREFIX)g++
 RISCV_OBJDUMP ?= $(RISCV_PREFIX)objdump
@@ -15,7 +16,7 @@ RISCV_AR      ?= $(RISCV_PREFIX)ar
 RISCV_LD      ?= $(RISCV_PREFIX)ld
 RISCV_STRIP   ?= $(RISCV_PREFIX)strip
 
-RISCV_FLAGS    ?= -march=$(RISCV_ABI) -I$(CURDIR)/common -DITERATIONS=10 -mcmodel=medany -static -std=gnu99 -O3 -ffast-math -fno-common -fno-builtin-printf
+RISCV_FLAGS    ?= -march=$(RISCV_ARCH) -mabi=$(RISCV_ABI) -I$(CURDIR)/common -DITERATIONS=10 -mcmodel=medany -static -std=gnu99 -O3 -ffast-math -fno-common -fno-builtin-printf
 RISCV_CCFLAGS  ?= $(RISCV_FLAGS)
 RISCV_CXXFLAGS ?= $(RISCV_FLAGS)
 RISCV_LDFLAGS  ?= -static -nostartfiles -lm -lgcc $(RISCV_FLAGS)
@@ -24,10 +25,10 @@ RUNTIME ?= common/crt0.S.o common/printf.c.o common/string.c.o common/serial.c.o
 HDR ?=     common/runtime.h
 
 %.S.o: %.S
-	$(GCC_INSTALL_DIR)/bin/$(RISCV_CC) -Iinclude $(RISCV_CCFLAGS) -c $< -o $@
+	$(RISCV_CC) -Iinclude $(RISCV_CCFLAGS) -c $< -o $@
 
 %.c.o: %.c
-	$(GCC_INSTALL_DIR)/bin/$(RISCV_CC) -Iinclude $(RISCV_CCFLAGS) -c $< -o $@
+	$(RISCV_CC) -Iinclude $(RISCV_CCFLAGS) -c $< -o $@
 
 %.cpp.o: %.cpp
-	$(GCC_INSTALL_DIR)/bin/$(RISCV_CXX) $(RISCV_CXXFLAGS) -c $< -o $@
+	$(RISCV_CXX) $(RISCV_CXXFLAGS) -c $< -o $@
