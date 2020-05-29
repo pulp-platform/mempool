6f1d88692926084adcd5ce1f2200d862e8652064
Samuel Riedel <sriedel@iis.ee.ethz.ch>
2020-05-29 10:25:40 +0200

    [hardware] Add TCDM adapter
    This module is inspired by the `amo_shim`, but it also handles the
    protocol conversion from valid/ready to req/gnt and it manages the meta
    data used in the mempool system.

---
+++ hardware/src/tcdm_adapter.sv
@@ -0,0 +1,235 @@
+/// Description: Handles the protocol conversion from valid/ready to req/gnt and correctly returns
+/// the metadata. Additionally, it handles atomics. Hence, it needs to be instantiated in front of
+/// an SRAM over which it has exclusive access.
+
+/// Author: Samuel Riedel <sriedel@iis.ee.ethz.ch>
+`include "common_cells/registers.svh"
+
+module tcdm_adapter #(
+  parameter int unsigned AddrWidth    = 32,
+  parameter int unsigned DataWidth    = 32,
+  parameter type metadata_t           = logic,
+  parameter bit          RegisterAmo  = 1'b0, // Cut path between request and response at the cost of increased AMO latency
+  // Dependent parameters. DO NOT CHANGE.
+  localparam int unsigned BeWidth     = DataWidth/8
+) (
+  input  logic                 clk_i,
+  input  logic                 rst_ni,
+  // master side
+  input  logic                 in_valid_i,   // Bank request
+  output logic                 in_ready_o,   // Bank grant
+  input  logic [AddrWidth-1:0] in_address_i, // Address
+  input  logic [3:0]           in_amo_i,     // Atomic Memory Operation
+  input  logic                 in_write_i,   // 1: Store, 0: Load
+  input  logic [DataWidth-1:0] in_wdata_i,   // Write data
+  input  metadata_t            in_meta_i,    // Meta data
+  input  logic [BeWidth-1:0]   in_be_i,      // Byte enable
+  output logic                 in_valid_o,   // Read data
+  input  logic                 in_ready_i,   // Read data
+  output logic [DataWidth-1:0] in_rdata_o,   // Read data
+  output metadata_t            in_meta_o,    // Meta data
+  // slave side
+  output logic                 out_req_o,   // Bank request
+  output logic [AddrWidth-1:0] out_add_o,   // Address
+  output logic                 out_write_o, // 1: Store, 0: Load
+  output logic [DataWidth-1:0] out_wdata_o, // Write data
+  output logic [DataWidth-1:0] out_be_o,    // Bit enable
+  input  logic [DataWidth-1:0] out_rdata_i  // Read data
+);
+
+  typedef enum logic [3:0] {
+      AMONone = 4'h0,
+      AMOSwap = 4'h1,
+      AMOAdd  = 4'h2,
+      AMOAnd  = 4'h3,
+      AMOOr   = 4'h4,
+      AMOXor  = 4'h5,
+      AMOMax  = 4'h6,
+      AMOMaxu = 4'h7,
+      AMOMin  = 4'h8,
+      AMOMinu = 4'h9,
+      AMOLR   = 4'hA,
+      AMOSC   = 4'hB
+  } amo_op_t;
+
+  logic meta_valid, meta_ready;
+  logic rdata_valid, rdata_ready;
+  logic out_gnt;
+  logic pop_resp;
+
+  enum logic [1:0] {
+      Idle, DoAMO, WriteBackAMO
+  } state_q, state_d;
+  logic                 load_amo;
+  amo_op_t              amo_op_q;
+  logic [BeWidth-1:0]   be_expand;
+  logic [AddrWidth-1:0] addr_q;
+
+  logic [31:0] amo_operand_a;
+  logic [31:0] amo_operand_b_q;
+  logic [31:0] amo_result, amo_result_q;
+
+  // Store the metadata at handshake
+  spill_register #(
+    .T      (metadata_t),
+    .Bypass (1'b0      )
+  ) i_metadata_register (
+    .clk_i   (clk_i                  ),
+    .rst_ni  (rst_ni                 ),
+    .valid_i (in_valid_i & in_ready_o & !in_write_i),
+    .ready_o (meta_ready             ),
+    .data_i  (in_meta_i              ),
+    .valid_o (meta_valid             ),
+    .ready_i (pop_resp               ),
+    .data_o  (in_meta_o              )
+  );
+
+  // Store response if it's not accepted immediately
+  fall_through_register #(
+    .T (logic [DataWidth-1:0])
+  ) i_rdata_register (
+    .clk_i      (clk_i      ),
+    .rst_ni     (rst_ni     ),
+    .clr_i      (1'b0       ),
+    .testmode_i (1'b0       ),
+    .data_i     (out_rdata_i),
+    .valid_i    (out_gnt    ),
+    .ready_o    (rdata_ready),
+    .data_o     (in_rdata_o ),
+    .valid_o    (rdata_valid),
+    .ready_i    (pop_resp   )
+  );
+
+  // Ready to output data if both meta and read data are available (the read data will always be last)
+  assign in_valid_o = meta_valid & rdata_valid;
+  // Only pop the data from the registers once both registers are ready
+  assign pop_resp   = in_ready_i & in_valid_o;
+
+  // Generate out_gnt one cycle after sending read request to the bank
+  `FFSRN(out_gnt, out_req_o & !out_write_o, 1'b0, clk_i, rst_ni);
+
+  always_comb begin
+    // feed-through
+    in_ready_o  = in_valid_o & ~in_ready_i ? 1'b0 : 1'b1;
+    out_req_o   = in_valid_i & in_ready_o;
+    out_add_o   = in_address_i;
+    out_write_o = in_write_i;
+    out_wdata_o = in_wdata_i;
+    be_expand   = in_be_i;
+
+    state_d     = state_q;
+    load_amo    = 1'b0;
+
+    unique case (state_q)
+      Idle: begin
+        if (in_valid_i && amo_op_t'(in_amo_i) != AMONone) begin
+          load_amo = 1'b1;
+          state_d = DoAMO;
+        end
+      end
+      // Claim the memory interface
+      DoAMO, WriteBackAMO: begin
+        in_ready_o  = 1'b0;
+        state_d     = (RegisterAmo && state_q != WriteBackAMO) ?  WriteBackAMO : Idle;
+        // Commit AMO
+        out_req_o   = 1'b1;
+        out_write_o = 1'b1;
+        out_add_o   = addr_q;
+        be_expand   = 4'b1111;
+        // serve from register if we cut the path
+        if (RegisterAmo) begin
+          out_wdata_o = amo_result_q;
+        end else begin
+          out_wdata_o = amo_result;
+        end
+      end
+      default:;
+    endcase
+  end
+
+  // Expand byte-enable into bit-enable
+  for (genvar be_byte = 0; be_byte < BeWidth; be_byte++) begin: gen_mem_be
+    assign out_be_o[8*be_byte+:8] = {8{be_expand[be_byte]}};
+  end
+
+  if (RegisterAmo) begin : gen_amo_slice
+    `FFLNR(amo_result_q, amo_result, (state_q == DoAMO), clk_i)
+  end
+
+  always_ff @(posedge clk_i or negedge rst_ni) begin
+    if (!rst_ni) begin
+      state_q         <= Idle;
+      amo_op_q        <= amo_op_t'('0);
+      addr_q          <= '0;
+      amo_operand_b_q <= '0;
+    end else begin
+      state_q         <= state_d;
+      if (load_amo) begin
+        amo_op_q        <= amo_op_t'(in_amo_i);
+        addr_q          <= in_address_i;
+        amo_operand_b_q <= in_wdata_i;
+      end else begin
+        amo_op_q        <= AMONone;
+      end
+    end
+  end
+
+  // ----------------
+  // AMO ALU
+  // ----------------
+  logic [33:0] adder_sum;
+  logic [32:0] adder_operand_a, adder_operand_b;
+
+  assign amo_operand_a = out_rdata_i;
+  assign adder_sum     = adder_operand_a + adder_operand_b;
+  /* verilator lint_off WIDTH */
+  always_comb begin : amo_alu
+
+    adder_operand_a = $signed(amo_operand_a);
+    adder_operand_b = $signed(amo_operand_b_q);
+
+    amo_result = amo_operand_b_q;
+
+    unique case (amo_op_q)
+      // the default is to output operand_b
+      AMOSwap:;
+      AMOAdd: amo_result = adder_sum[31:0];
+      AMOAnd: amo_result = amo_operand_a & amo_operand_b_q;
+      AMOOr:  amo_result = amo_operand_a | amo_operand_b_q;
+      AMOXor: amo_result = amo_operand_a ^ amo_operand_b_q;
+      AMOMax: begin
+        adder_operand_b = -$signed(amo_operand_b_q);
+        amo_result = adder_sum[32] ? amo_operand_b_q : amo_operand_a;
+      end
+      AMOMin: begin
+        adder_operand_b = -$signed(amo_operand_b_q);
+        amo_result = adder_sum[32] ? amo_operand_a : amo_operand_b_q;
+      end
+      AMOMaxu: begin
+        adder_operand_a = $unsigned(amo_operand_a);
+        adder_operand_b = -$unsigned(amo_operand_b_q);
+        amo_result = adder_sum[32] ? amo_operand_b_q : amo_operand_a;
+      end
+      AMOMinu: begin
+        adder_operand_a = $unsigned(amo_operand_a);
+        adder_operand_b = -$unsigned(amo_operand_b_q);
+        amo_result = adder_sum[32] ? amo_operand_a : amo_operand_b_q;
+      end
+      default: amo_result = '0;
+    endcase
+  end
+
+  // pragma translate_off
+  // Check for unsopported parameters
+  if (DataWidth != 32) begin
+    $error($sformatf("Module currently only supports DataWidth = 32. DataWidth is currently set to: %0d", DataWidth));
+  end
+
+  `ifndef VERILATOR
+    rdata_full : assert property(
+      @(posedge clk_i) disable iff (~rst_ni) (out_gnt |-> rdata_ready))
+      else $fatal (1, "Trying to push new data although the i_rdata_register is not ready.");
+  `endif
+  // pragma translate_on
+
+endmodule
