a70c5a503cc0bf34b46e275b19998131f17edc11
Samuel Riedel <sriedel@iis.ee.ethz.ch>
2020-08-04 15:22:13 +0200

    [apps] Clang-format all applications

--- apps/hello_world/main.c
+++ apps/hello_world/main.c
@@ -16,36 +16,38 @@
 
 // Author: Matheus Cavalcante, ETH Zurich
 
-#include "printf.h"
 #include <stdint.h>
 #include <string.h>
+
 #include "encoding.h"
+#include "printf.h"
 #include "runtime.h"
 #include "synchronization.h"
 
-volatile uint32_t atomic __attribute__ ((section (".l1")));
+volatile uint32_t atomic __attribute__((section(".l1")));
 
 extern volatile uint32_t tcdm_start_address_reg;
 extern volatile uint32_t tcdm_end_address_reg;
 
 int main(int argc, char **argv) {
-    uint32_t core_id = (uint32_t) argc;
-    uint32_t num_cores = (uint32_t) argv;
-    // Initialize synchronization variables
-    if (core_id == 0) {
-      mempool_barrier_init();
-      atomic = 0;
-    } else {
-      mempool_wait(2000);
-    }
-    mempool_barrier(core_id, num_cores);
+  uint32_t core_id = (uint32_t)argc;
+  uint32_t num_cores = (uint32_t)argv;
+  // Initialize synchronization variables
+  if (core_id == 0) {
+    mempool_barrier_init();
+    atomic = 0;
+  } else {
+    mempool_wait(2000);
+  }
+  mempool_barrier(core_id, num_cores);
 
-    while (atomic != core_id);
+  while (atomic != core_id)
+    ;
 
-    printf("Core %d says Hello!\n", core_id);
-    __atomic_fetch_add(&atomic, 1, __ATOMIC_SEQ_CST);
+  printf("Core %d says Hello!\n", core_id);
+  __atomic_fetch_add(&atomic, 1, __ATOMIC_SEQ_CST);
 
-    // wait until all cores have finished
-    mempool_barrier(core_id, num_cores);
-    return 0;
+  // wait until all cores have finished
+  mempool_barrier(core_id, num_cores);
+  return 0;
 }
