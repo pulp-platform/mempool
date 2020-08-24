dc2c7a561af75592774a6f2829a88797c2226d40
Samuel Riedel <sriedel@iis.ee.ethz.ch>
2020-08-24 02:46:06 +0200

    [scripts] Clean and extend the result parsing script

--- hardware/scripts/parse_results.py
+++ hardware/scripts/parse_results.py
@@ -1,150 +1,165 @@
 #!/usr/bin/env python3
 
+# Copyright 2019 ETH Zurich and University of Bologna.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Author: Samuel Riedel, ETH Zurich
+
 import argparse
-import sys
-import pandas as pd
-import numpy as np
 import ast
-import seaborn as sns
 import itertools
+import sys
 
 import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
 from matplotlib.font_manager import FontProperties
 
 
 def get_runtime(data):
-    # Get runtime of each segment in each run
-    columns = ['run', 'section', 'runtime', 'cycles']
-    result = pd.DataFrame(columns=columns)
-    for run in data['run'].unique():
-        for sec in data[data['run'] == run]['section'].unique():
-            subset = (data[(data['run'] == run) & (data['section'] == sec)])
-            runtime = subset['end'].max() - subset['start'].min()
-            s = pd.Series([run, sec, runtime, subset['cycles'].to_list()], index=columns)
-            result = result.append(s, ignore_index=True)
-    return result
+	# Get runtime of each segment in each run
+	columns = ['run', 'section', 'runtime', 'cycles']
+	result = pd.DataFrame(columns=columns)
+	for run in data['run'].unique():
+		for sec in data[data['run'] == run]['section'].unique():
+			subset = (data[(data['run'] == run) & (data['section'] == sec)])
+			runtime = subset['end'].max() - subset['start'].min()
+			s = pd.Series([run, sec, runtime, subset['cycles'].to_list()], index=columns)
+			result = result.append(s, ignore_index=True)
+	return result
 
 def get_speedup(data):
-    runs = data['run'].unique()
-    columns = ['section']
-    result = pd.DataFrame(index=data['section'].unique())
-    for i, pair in enumerate(itertools.permutations(runs, 2)):
-        runtime_old = data.loc[data.run == pair[0],'runtime'].to_list()
-        index_old   = data.loc[data.run == pair[0],'section'].to_list()
-        runtime_old = [runtime_old[i] for i in index_old]
-        runtime_new = data.loc[data.run == pair[1],'runtime'].to_list()
-        index_new   = data.loc[data.run == pair[1],'section'].to_list()
-        runtime_new = [runtime_new[i] for i in index_new]
-        speedup = np.divide(runtime_old, runtime_new)
-        result.insert(i, ''.join(pair), speedup)
-    return result
+	runs = data['run'].unique()
+	columns = ['section']
+	result = pd.DataFrame(index=data['section'].unique())
+	for i, pair in enumerate(itertools.permutations(runs, 2)):
+		runtime_old = data.loc[data.run == pair[0],'runtime'].to_list()
+		index_old   = data.loc[data.run == pair[0],'section'].to_list()
+		runtime_old = [runtime_old[i] for i in index_old]
+		runtime_new = data.loc[data.run == pair[1],'runtime'].to_list()
+		index_new   = data.loc[data.run == pair[1],'section'].to_list()
+		runtime_new = [runtime_new[i] for i in index_new]
+		speedup = np.divide(runtime_old, runtime_new)
+		result.insert(i, ''.join(pair), speedup)
+	return result
+
+def boxplot(x, y, data, hue=None, whis=[10,90], fliersize=0.1, palette='vlag', flierprops=None, violin=False):
+	if violin:
+		ax = sns.violinplot(x=x, y=y, hue=hue, data=data, palette=palette)
+	else:
+		if flierprops is None:
+			flierprops = dict(marker='o', markersize=1, markerfacecolor='none')
+		ax = sns.boxplot(x=x, y=y, hue=hue, data=data, whis=whis, fliersize=fliersize, palette=palette, flierprops=flierprops)
+	return ax
 
 def main():
-    # Parse the arguments.
-    parser = argparse.ArgumentParser()
-    parser.add_argument("filename", nargs="+", help="File to parse")
-    parser.add_argument("--name", nargs="*", help="Names to use in plot")
-    parser.add_argument("-o", "--output", metavar="PNG", nargs="?", help="PNG file to write; interactive if omitted")
-    args = parser.parse_args()
+	# Parse the arguments.
+	parser = argparse.ArgumentParser()
+	parser.add_argument("filename", nargs="+", help="File to parse")
+	parser.add_argument("--name", nargs="*", help="Names to use in plot")
+	parser.add_argument("--sections", action='store_true', help="Use Sections")
+	parser.add_argument("--violin", action='store_true', help="Use Violin plots")
+	parser.add_argument("-o", "--output", metavar="PNG", nargs="?", help="PNG file to write; interactive if omitted")
+	args = parser.parse_args()
 
-    print(args)
-    # Parse optional names for data runs
-    names = args.filename[:]
-    if args.name is not None:
-        if len(args.filename) > len(args.name):
-            print("Warning: More input files than names")
-            for i, name in enumerate(args.name):
-                names[i] = name
-        else:
-            names = args.name
+	print(args)
+	# Parse optional names for data runs
+	names = args.filename[:]
+	if args.name is not None:
+		if len(args.filename) > len(args.name):
+			print("Warning: More input files than names")
+			for i, name in enumerate(args.name):
+				names[i] = name
+		else:
+			names = args.name
 
-    # Read data from files specified in arguments
-    dataframes = []
-    # Convert the lists in snitch_load_latency to lists instead of interpreting them as strings
-    generic = lambda x: ast.literal_eval(x)
-    conv = {'snitch_load_latency': generic}
-    for i, file in enumerate(args.filename):
-        dataframes.append(pd.read_csv(file, converters=conv))
-        dataframes[-1]['run'] = names[i]
+	# Read data from files specified in arguments
+	dataframes = []
+	# Convert the lists in snitch_load_latency to lists instead of interpreting them as strings
+	generic = lambda x: ast.literal_eval(x)
+	conv = {'snitch_load_latency': generic}
+	for i, file in enumerate(args.filename):
+		dataframes.append(pd.read_csv(file, converters=conv))
+		dataframes[-1]['run'] = names[i]
 
-    data = pd.concat(dataframes)
-    data = data.sort_values(by=['run', 'section', 'core'])
-    # Extract data
-    runtime = get_runtime(data)
+	data = pd.concat(dataframes)
+	data = data.sort_values(by=['run', 'section', 'core'])
+	# Extract data
+	runtime = get_runtime(data)
 
-    plt.figure(1)
-    # ax = runtime.set_index(['section', 'run']).unstack('run').plot.bar(colormap='Pastel2')
-#    ax = sns.barplot(x='section', y='runtime', hue='run', data=runtime,
-#        palette='vlag')
-    ax = sns.barplot(x='run', y='runtime', data=runtime, palette='vlag')
-    ax.set_ylabel('Runtime [cycles]')
-    ax.set_title('Runtime')
+	load_latency = data[data['section'] == 0]
+	load_latency = load_latency.explode('snitch_load_latency').infer_objects()
+	load_latency_1tile = load_latency[load_latency['core'] < 8]
 
-    plt.figure(2)
-    # df = sns.load_dataset('tips')
-    # sns.violinplot(x='day', y="total_bill", hue="smoker", data=df, palette="Pastel1")
-    # sns.violinplot(x='section', y='cycles', hue='run', data=data, palette="Pastel2")
-    sns.violinplot(x='run', y='cycles', data=data, palette="vlag")
-    sns.swarmplot(x='run', y='cycles', data=data, size=2, color='.3', linewidth=0);
-    #plt.show()
+	speedup = get_speedup(runtime)
+	with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+		print(runtime.drop(columns='cycles').set_index(['section', 'run']).unstack('run'))
+		print(speedup)
 
-    plt.figure(3, figsize=(16, 8), dpi=300)
-    load_latency = data[data['section'] == 0]
-    load_latency = load_latency.explode('snitch_load_latency').infer_objects()
-    # df = sns.load_dataset('tips')
-    # sns.violinplot(x='day', y="total_bill", hue="smoker", data=df, palette="Pastel1")
-    sns.violinplot(x='core', y='snitch_load_latency', hue='run',
-        data=load_latency[load_latency['core'] < 4], palette="vlag")
-#    sns.boxplot(x='core', y='snitch_load_latency', hue='run',
-#        data=load_latency[load_latency['core'] < 4],
-#        whis=[0,100], palette='vlag')
-#    sns.swarmplot(x='core', y='snitch_load_latency', hue='run',
-#        data=load_latency[load_latency['core'] < 1], size=2, color='.3',
-#        linewidth=0)
+	load_latency_cores = data[data['section'] == 0]
+	load_latency_cores = load_latency_cores[['core','run','snitch_load_latency']]
+	explode = load_latency_cores.explode('snitch_load_latency').infer_objects()
+	info = None
+	for run in data['run'].unique():
+		statistics = explode[explode['run'] == run]['snitch_load_latency'].describe(percentiles=[0.1,0.25,0.5,0.75,0.9]).to_frame(run)
+		if info is None:
+			info = statistics
+		else:
+			info = info.join(statistics)
+	print(info)
 
-    # plt.figure(3)
-    # ax = runtime.set_index(['section','run']).unstack('run').plot.bar()
-    # ax.set_ylabel('Runtime [cycles]')
-    # plt.show()
+	# Plotting
+	# colorpalette = 'vlag'
+	colorpalette = 'RdBu'
+	# colorpalette = 'icefire'
+	# colorpalette = 'mako_r'
+	# colorpalette = 'GnBu_r'
+	# colorpalette = 'gnuplot'
 
-    speedup = get_speedup(runtime)
-    print(speedup)
-    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-        print(runtime.drop(columns='cycles').set_index(['section', 'run']).unstack('run'))
-        print(speedup)
+	f = plt.figure(1, figsize=(8, 4), dpi=600)
+	if (args.sections):
+		ax = sns.barplot(x='section', y='runtime', hue='run', data=runtime, palette=colorpalette)
+		ax.set_xlabel('Section')
+	else:
+		ax = sns.barplot(x='run', y='runtime', data=runtime, palette=colorpalette)
+		ax.set_xlabel('Configuration')
+	ax.set_ylabel('Runtime [cycles]')
+	ax.set_title('Runtime')
+	plt.show()
+	f.savefig("Runtime.pdf", bbox_inches='tight')
 
-    plt.figure(4)
-    sns.heatmap(speedup, annot=True, linewidth=.5)
+	f = plt.figure(2, figsize=(8, 4), dpi=600)
+	if (args.sections):
+		ax = boxplot(x='run', y='cycles', data=data, hue='run', palette=colorpalette, violin=args.violin)
+		ax.set_xlabel('Section')
+	else:
+		ax = boxplot(x='run', y='cycles', data=data, palette=colorpalette, violin=args.violin)
+		ax.set_xlabel('Configuration')
+	ax.set_ylabel('Runtime [cycles]')
+	ax.set_title('Runtime')
+	plt.show()
+	f.savefig("Runtime_Distribution.pdf", bbox_inches='tight')
 
-    plt.show()
-    exit(0)
-
-    X = np.arange(4)
-    fig = plt.figure()
-    ax = fig.add_axes([0, 0, 1, 1])
-    ax.bar(X + 0.00, data[0], color='b', width=0.25)
-    ax.bar(X + 0.25, data[1], color='g', width=0.25)
-    ax.bar(X + 0.50, data[2], color='r', width=0.25)
-
-    # Plot the data.
-    font = FontProperties()
-    font.set_size('larger')
-    labels = ["Best Cost Function"]
-    plt.figure(figsize=(12.5, 6))
-    plt.plot(range(len(data["best"])), data["best"], label=labels[0])
-    plt.xlabel("Iteration #")
-    plt.ylabel("Value [-]")
-    plt.legend(loc="best", prop=font)
-    plt.grid()
-
-    if args.output is None:
-        plt.show()
-    else:
-        plt.savefig(args.output)
-
-    print(data["solution"])
-    print(data["best"][-1])
-
+	f = plt.figure(3, figsize=(8, 4), dpi=600)
+	ax = boxplot(x='core', y='snitch_load_latency', hue='run', palette=colorpalette,
+		data=load_latency_1tile, violin=args.violin)
+	ax.set_xlabel('Core')
+	ax.set_ylabel('Load Latency [cycles]')
+	ax.set_title('Load Latency distribution')
+	plt.show()
+	f.savefig("LoadLatency.pdf", bbox_inches='tight')
 
 if __name__ == '__main__':
-    sys.exit(main())
+	sys.exit(main())
