952e6e0a870e8693d85d52a616d5618ec8e30d8e
Joan Mihali <jmihali@student.ethz.ch>
2020-10-09 11:30:52 +0200

    [apps] Update barrier function call with new signature

--- apps/sleep_wakeup/main.c
+++ apps/sleep_wakeup/main.c
@@ -11,22 +11,22 @@ volatile uint32_t atomic __attribute__((section(".l2"))) = -1;
 extern volatile uint32_t tcdm_start_address_reg;
 extern volatile uint32_t tcdm_end_address_reg;
 
-int main(int argc, char **argv) {
-  uint32_t core_id = (uint32_t)argc;
-  uint32_t num_cores = (uint32_t)argv;
-  
+int main() {
+  uint32_t core_id = mempool_get_core_id();
+  uint32_t num_cores = mempool_get_core_count();
+
   mempool_barrier_init(core_id, num_cores);
-  
+
   if (core_id == 0) {
   // Do a lot of work
-    mempool_wait(10000);
+    mempool_wait(1000);
     wake_up(core_id + 1);
   } else {
     mempool_wfi();
     printf("Core %d woke up\n", core_id);
     wake_up(core_id + 1);
   }
-  mempool_barrier(core_id, num_cores, num_cores * 4); 
+  mempool_barrier(num_cores, num_cores * 4);
 
 return 0;
 }
