682f89857e72eda08744b47b5712f4f3a756400d
Sergio Mazzola <smazzola@student.ethz.ch>
2020-10-29 21:23:38 +0100

    [apps] Align MemPool tests runtime to new eoc address and format

--- apps/common/riscv_test.h
+++ apps/common/riscv_test.h
@@ -9,59 +9,59 @@
 // Begin Macro
 //-----------------------------------------------------------------------
 
-#define RVTEST_RV64U                                                    \
-  .macro init;                                                          \
+#define RVTEST_RV64U                                                           \
+  .macro init;                                                                 \
   .endm
 
-#define RVTEST_RV64UF                                                   \
-  .macro init;                                                          \
-  RVTEST_FP_ENABLE;                                                     \
+#define RVTEST_RV64UF                                                          \
+  .macro init;                                                                 \
+  RVTEST_FP_ENABLE;                                                            \
   .endm
 
-#define RVTEST_RV32U                                                    \
-  .macro init;                                                          \
+#define RVTEST_RV32U                                                           \
+  .macro init;                                                                 \
   .endm
 
-#define RVTEST_RV32UF                                                   \
-  .macro init;                                                          \
-  RVTEST_FP_ENABLE;                                                     \
+#define RVTEST_RV32UF                                                          \
+  .macro init;                                                                 \
+  RVTEST_FP_ENABLE;                                                            \
   .endm
 
-#define RVTEST_RV64M                                                    \
-  .macro init;                                                          \
-  RVTEST_ENABLE_MACHINE;                                                \
+#define RVTEST_RV64M                                                           \
+  .macro init;                                                                 \
+  RVTEST_ENABLE_MACHINE;                                                       \
   .endm
 
-#define RVTEST_RV64S                                                    \
-  .macro init;                                                          \
-  RVTEST_ENABLE_SUPERVISOR;                                             \
+#define RVTEST_RV64S                                                           \
+  .macro init;                                                                 \
+  RVTEST_ENABLE_SUPERVISOR;                                                    \
   .endm
 
-#define RVTEST_RV32M                                                    \
-  .macro init;                                                          \
-  RVTEST_ENABLE_MACHINE;                                                \
+#define RVTEST_RV32M                                                           \
+  .macro init;                                                                 \
+  RVTEST_ENABLE_MACHINE;                                                       \
   .endm
 
-#define RVTEST_RV32S                                                    \
-  .macro init;                                                          \
-  RVTEST_ENABLE_SUPERVISOR;                                             \
+#define RVTEST_RV32S                                                           \
+  .macro init;                                                                 \
+  RVTEST_ENABLE_SUPERVISOR;                                                    \
   .endm
 
-#define RVTEST_ENABLE_SUPERVISOR                                        \
-  li a0, MSTATUS_MPP & (MSTATUS_MPP >> 1);                              \
-  csrs mstatus, a0;                                                     \
-  li a0, SIP_SSIP | SIP_STIP;                                           \
-  csrs mideleg, a0;                                                     \
+#define RVTEST_ENABLE_SUPERVISOR                                               \
+  li a0, MSTATUS_MPP & (MSTATUS_MPP >> 1);                                     \
+  csrs mstatus, a0;                                                            \
+  li a0, SIP_SSIP | SIP_STIP;                                                  \
+  csrs mideleg, a0;
 
-#define RVTEST_ENABLE_MACHINE                                           \
-  li a0, MSTATUS_MPP;                                                   \
-  csrs mstatus, a0;                                                     \
+#define RVTEST_ENABLE_MACHINE                                                  \
+  li a0, MSTATUS_MPP;                                                          \
+  csrs mstatus, a0;
 
 #define RVTEST_FP_ENABLE
 
-#define RISCV_MULTICORE_DISABLE                                         \
-  csrr a0, mhartid;                                                     \
-  1: bnez a0, 1b
+#define RISCV_MULTICORE_DISABLE                                                \
+        csrr a0, mhartid;                                                      \
+1:      bnez a0, 1b
 
 #define EXTRA_TVEC_USER
 #define EXTRA_TVEC_MACHINE
@@ -70,45 +70,41 @@
 
 #define INTERRUPT_HANDLER j other_exception /* No interrupts should occur */
 
-#define RVTEST_CODE_BEGIN                                               \
-        .section .text.init;                                            \
-        .align  6;                                                      \
-        .globl _start;                                                  \
-_start:                                                                 \
-        /* reset vector */                                              \
-        j reset_vector;                                                 \
-        .align 2;                                                       \
-reset_vector:                                                           \
-        RISCV_MULTICORE_DISABLE;                                        \
-        li TESTNUM, 0;                                                  \
-        init;                                                           \
-        EXTRA_INIT;                                                     \
-        EXTRA_INIT_TIMER;                                               \
-//-----------------------------------------------------------------------
-// End Macro
-//-----------------------------------------------------------------------
+#define RVTEST_CODE_BEGIN                                                      \
+        .section .text.init;                                                   \
+        .align 6;                                                              \
+        .globl _start;                                                         \
+_start:                                                                        \
+        /* reset vector */                                                     \
+        j reset_vector;                                                        \
+        .align 2;                                                              \
+reset_vector:                                                                  \
+        RISCV_MULTICORE_DISABLE;                                               \
+        li TESTNUM, 0;                                                         \
+        init;                                                                  \
+        EXTRA_INIT;                                                            \
+        EXTRA_INIT_TIMER;                                                      \
+  //-----------------------------------------------------------------------
+  // End Macro
+  //-----------------------------------------------------------------------
 
-#define RVTEST_CODE_END                                                 \
-        csrrw x0, cycle, x0;                                            \
-        .section ".eoc_address","aw",@progbits;                         \
-        .align 6;                                                       \
-        .globl eoc_address;                                             \
-        eoc_address: .dword 0;
+#define RVTEST_CODE_END                                                        \
+        csrrw x0, cycle, x0;
 
 //-----------------------------------------------------------------------
 // Pass/Fail Macro
 //-----------------------------------------------------------------------
 #define TESTNUM gp
 
-#define RVTEST_PASS                                                     \
-        li TESTNUM, 0;                                                  \
-1:      la t0, eoc_address;                                             \
-        sw TESTNUM, 0(t0);                                              \
+#define RVTEST_PASS                                                            \
+        li TESTNUM, 0;                                                         \
+1:      la t0, eoc_reg;                                                        \
+        sw TESTNUM, 0(t0);                                                     \
         jal x0, 1b;
 
-#define RVTEST_FAIL                                                     \
-1:      beqz TESTNUM, 1b;                                               \
-        la t0, eoc_address;                                             \
+#define RVTEST_FAIL                                                            \
+1:      beqz TESTNUM, 1b;                                                      \
+        la t0, eoc_reg;                                                        \
         sw TESTNUM, 0(t0);
 
 //-----------------------------------------------------------------------
@@ -117,10 +113,15 @@ reset_vector:                                                           \
 
 #define EXTRA_DATA
 
-#define RVTEST_DATA_BEGIN                                               \
-        EXTRA_DATA                                                      \
-        .align 4; .global begin_signature; begin_signature:
+#define RVTEST_DATA_BEGIN                                                      \
+        EXTRA_DATA                                                             \
+        .align 4;                                                              \
+        .global begin_signature;                                               \
+begin_signature:
 
-#define RVTEST_DATA_END .align 4; .global end_signature; end_signature:
+#define RVTEST_DATA_END                                                        \
+        .align 4;                                                              \
+        .global end_signature;                                                 \
+end_signature:
 
 #endif
