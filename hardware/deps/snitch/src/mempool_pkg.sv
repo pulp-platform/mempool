aa8dd0dd645d01a4233d22173c3692254e909c9a
Samuel Riedel <sriedel@iis.ee.ethz.ch>
2020-11-12 17:29:26 +0100

    [snitch] Fix indentation

--- hardware/deps/snitch/src/mempool_pkg.sv
+++ hardware/deps/snitch/src/mempool_pkg.sv
@@ -1,65 +1,65 @@
 /// Snitch Configuration.
 package snitch_pkg;
 
-  localparam DataWidth                  = 32                            ;
-  localparam StrbWidth                  = DataWidth/8                   ;
-  localparam int NumFPOutstandingLoads  = 4                             ;
-  localparam int NumIntOutstandingLoads = 8                             ;
+  localparam DataWidth                  = 32;
+  localparam StrbWidth                  = DataWidth/8;
+  localparam int NumFPOutstandingLoads  = 4;
+  localparam int NumIntOutstandingLoads = 8;
   localparam ReorderIdWidth             = $clog2(NumIntOutstandingLoads);
 
   typedef struct packed {
     logic [31:0] BootAddress;
-    int unsigned NrCores    ;
+    int unsigned NrCores;
   } SnitchCfg;
 
   typedef struct packed {
-    logic [31:0] addr            ;
+    logic [31:0] addr;
     logic [ReorderIdWidth-1:0] id;
-    logic [3:0] amo              ;
-    logic write                  ;
-    logic [DataWidth-1:0] data   ;
-    logic [StrbWidth-1:0] strb   ;
+    logic [3:0] amo;
+    logic write;
+    logic [DataWidth-1:0] data;
+    logic [StrbWidth-1:0] strb;
   } dreq_t;
 
   typedef struct packed {
-    logic [DataWidth-1:0] data   ;
+    logic [DataWidth-1:0] data;
     logic [ReorderIdWidth-1:0] id;
-    logic error                  ;
+    logic error;
   } dresp_t;
 
   typedef struct packed {
-    logic [31:0] addr              ;
-    logic [4:0] id                 ;
-    logic [31:0] data_op           ;
+    logic [31:0] addr;
+    logic [4:0] id;
+    logic [31:0] data_op;
     logic [DataWidth-1:0] data_arga;
     logic [DataWidth-1:0] data_argb;
     logic [DataWidth-1:0] data_argc;
   } acc_req_t;
 
   typedef struct packed {
-    logic [4:0] id            ;
-    logic error               ;
+    logic [4:0] id;
+    logic error;
     logic [DataWidth-1:0] data;
   } acc_resp_t;
 
   typedef struct packed {
-    logic [31:0] addr         ;
-    logic write               ;
+    logic [31:0] addr;
+    logic write;
     logic [DataWidth-1:0] data;
     logic [StrbWidth-1:0] strb;
   } inst_req_t;
 
   // Number of instructions the sequencer can hold
-  localparam int FPUSequencerInstr      = 16           ;
+  localparam int FPUSequencerInstr      = 16;
   // SSRs
-  localparam logic [31:0] SSR_ADDR_BASE = 32'h20_4800  ;
+  localparam logic [31:0] SSR_ADDR_BASE = 32'h20_4800;
   localparam logic [31:0] SSR_ADDR_MASK = 32'hffff_fe00;
-  localparam logic [11:0] CSR_SSR       = 12'h7C0      ;
-  localparam int SSRNrCredits           = 4            ;
+  localparam logic [11:0] CSR_SSR       = 12'h7C0;
+  localparam int SSRNrCredits           = 4;
   // Registers which are used as SSRs
-  localparam [4:0] FT0                  = 5'b0         ;
-  localparam [4:0] FT1                  = 5'b1         ;
-  localparam [1:0][4:0] SSRRegs         = {FT1, FT0}   ;
+  localparam [4:0] FT0                  = 5'b0;
+  localparam [4:0] FT1                  = 5'b1;
+  localparam [1:0][4:0] SSRRegs         = {FT1, FT0};
   function automatic logic is_ssr(logic [4:0] register);
     unique case (register)
       FT0, FT1: return 1'b1;
@@ -74,7 +74,7 @@ package snitch_pkg;
   // Address Map
   // TCDM, everything below 0x4000_0000
   localparam logic [31:0] TCDMStartAddress = 32'h0000_0000;
-  localparam logic [31:0] TCDMMask         = '1 << 28     ;
+  localparam logic [31:0] TCDMMask         = '1 << 28;
 
   // Slaves on Cluster AXI Bus
   typedef enum integer {
@@ -92,13 +92,13 @@ package snitch_pkg;
   localparam int unsigned NrSlaves  = 3;
   localparam int unsigned NrMasters = 3;
 
-  localparam int IdWidth      = 2                          ;
+  localparam int IdWidth      = 2;
   localparam int IdWidthSlave = $clog2(NrMasters) + IdWidth;
 
   //                                                    3. SoC         2. Cluster Peripherals  3. TCDM
-  localparam logic [NrSlaves-1:0][31:0] StartAddress = {32'h8000_0000, 32'h4000_0000, TCDMStartAddress}                ;
+  localparam logic [NrSlaves-1:0][31:0] StartAddress = {32'h8000_0000, 32'h4000_0000, TCDMStartAddress};
   localparam logic [NrSlaves-1:0][31:0] EndAddress   = {32'hFFFF_FFFF, 32'h5000_0000, TCDMStartAddress + 32'h1000_0000};
-  localparam logic [NrSlaves-1:0] ValidRule          = {{NrSlaves}{1'b1}}                                              ;
+  localparam logic [NrSlaves-1:0] ValidRule          = {{NrSlaves}{1'b1}};
 
   // Cluster Peripheral Registers
   typedef enum logic [31:0] {
@@ -119,18 +119,18 @@ package snitch_pkg;
   function automatic logic shared_offload (logic [31:0] instr);
     logic offload;
     unique casez (instr)
-      riscv_instr::MUL   ,
-      riscv_instr::MULH  ,
+      riscv_instr::MUL,
+      riscv_instr::MULH,
       riscv_instr::MULHSU,
-      riscv_instr::MULHU ,
-      riscv_instr::DIV   ,
-      riscv_instr::DIVU  ,
-      riscv_instr::REM   ,
-      riscv_instr::REMU  ,
-      riscv_instr::MULW  ,
-      riscv_instr::DIVW  ,
-      riscv_instr::DIVUW ,
-      riscv_instr::REMW  ,
+      riscv_instr::MULHU,
+      riscv_instr::DIV,
+      riscv_instr::DIVU,
+      riscv_instr::REM,
+      riscv_instr::REMU,
+      riscv_instr::MULW,
+      riscv_instr::DIVW,
+      riscv_instr::DIVUW,
+      riscv_instr::REMW,
       riscv_instr::REMUW: offload = 1;
       default: offload            = 0;
     endcase
@@ -140,10 +140,10 @@ package snitch_pkg;
   // Event strobes per core, counted by the performance counters in the cluster
   // peripherals.
   typedef struct packed {
-    logic issue_fpu        ; // core operations performed in the FPU
-    logic issue_fpu_seq    ; // includes load/store operations
+    logic issue_fpu;         // core operations performed in the FPU
+    logic issue_fpu_seq;     // includes load/store operations
     logic issue_core_to_fpu; // instructions issued from core to FPU
-    logic retired_insts    ; // number of instructions retired by the core
+    logic retired_insts;     // number of instructions retired by the core
   } core_events_t;
 
 endpackage
