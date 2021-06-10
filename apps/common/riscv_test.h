75e93abd7843145c27a19cc562bb184f9768ef53
Samuel Riedel <sriedel@iis.ee.ethz.ch>
2021-06-10 23:30:32 +0200

    [apps] Update license header

--- apps/common/riscv_test.h
+++ apps/common/riscv_test.h
@@ -1,4 +1,6 @@
-// See LICENSE for license details.
+// Copyright 2021 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
 
 // clang-format off
 
