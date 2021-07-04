b306d1b08315a9193246fb42d499f847cd1ed8a3
Samuel Riedel <sriedel@iis.ee.ethz.ch>
2021-07-04 15:05:19 +0200

    [software] Update `gradient` example for MemPool

--- software/halide/gradient/main.c
+++ software/halide/gradient/main.c
@@ -9,74 +9,71 @@
  * and the C buffer to be compatible.
  */
 
-#include "gradient_riscv.h"
-#include "rt/rt_api.h"
+#include "encoding.h"
+#include "halide_pipeline.riscv.h"
+#include "halide_runtime.h"
+#include "printf.h"
+#include "runtime.h"
+#include "synchronization.h"
 #include <stdint.h>
-#include <stdio.h>
+#include <string.h>
 
 int main() {
-  // Specify a two-dimensional buffer
-  halide_dimension_t buffer_dim[2];
-  // Fill both dimensions' parameters
-  buffer_dim[0].min = 0;
-  buffer_dim[0].extent = 15;
-  buffer_dim[0].stride = 1;
-  buffer_dim[0].flags = 0;
-  buffer_dim[1].min = 0;
-  buffer_dim[1].extent = 21;
-  buffer_dim[1].stride = buffer_dim[0].extent;
-  buffer_dim[1].flags = 0;
+  if (mempool_get_core_id() == 0) {
+    // Specify a two-dimensional buffer
+    halide_dimension_t buffer_dim[2];
+    // Fill both dimensions' parameters
+    buffer_dim[0].min = 0;
+    buffer_dim[0].extent = 15;
+    buffer_dim[0].stride = 1;
+    buffer_dim[0].flags = 0;
+    buffer_dim[1].min = 0;
+    buffer_dim[1].extent = 21;
+    buffer_dim[1].stride = buffer_dim[0].extent;
+    buffer_dim[1].flags = 0;
 
-  // Specify the type of data in the buffer
-  struct halide_type_t buffer_type;
-  buffer_type.code = halide_type_uint;
-  buffer_type.bits = 8;
-  buffer_type.lanes = 1;
+    // Specify the type of data in the buffer
+    struct halide_type_t buffer_type;
+    buffer_type.code = halide_type_uint;
+    buffer_type.bits = 8;
+    buffer_type.lanes = 1;
 
-  // Allocate the buffer and the structure to hold the buffer
-  void *buffer =
-      rt_alloc(RT_ALLOC_CL_DATA,
-               buffer_dim[0].extent * buffer_dim[1].extent * sizeof(uint8_t));
-  if (buffer == NULL) {
-    printf("Buffer could not be allocated\n");
-    return 1;
-  }
+    // Allocate the buffer and the structure to hold the buffer
+    uint8_t buffer[buffer_dim[0].extent * buffer_dim[1].extent];
 
-  halide_buffer_t *output = rt_alloc(RT_ALLOC_CL_DATA, sizeof(halide_buffer_t));
-  if (output == NULL) {
-    printf("Output could not be allocated\n");
-    return 1;
-  }
+    // Assign all parameters to the buffer structure
+    halide_buffer_t output;
+    output.device = 0;
+    output.device_interface = NULL;
+    output.host = buffer;
+    output.flags = 1;
+    output.type = buffer_type;
+    output.dimensions = 2;
+    output.dim = buffer_dim;
+    output.padding = 0;
 
-  // Assign all parameters to the buffer structure
-  output->device = 0;
-  output->device_interface = NULL;
-  output->host = buffer;
-  output->flags = 1;
-  output->type = buffer_type;
-  output->dimensions = 2;
-  output->dim = buffer_dim;
-  output->padding = 0;
+    // Set arguments:
+    unsigned center_x = 7;
+    unsigned center_y = 7;
 
-  // Set arguments:
-  unsigned center_x = 7;
-  unsigned center_y = 7;
+    // Call the Halide pipeline
+    printf("Start calculation around x=%d, y=%d\n", center_x, center_y);
+    int error = gradient(center_x, center_y, &output);
 
-  // Call the Halide pipeline
-  printf("Start calculation around x=%d, y=%d\n", center_x, center_y);
-  int error = gradient(center_x, center_y, output);
-
-  // Print the result
-  printf("Gradient finished with exit code %d\n", error);
-  for (int y = 0; y < buffer_dim[1].extent; ++y) {
-    for (int x = 0; x < buffer_dim[0].extent; ++x) {
-      uint8_t val =
-          ((uint8_t *)output
-               ->host)[x * buffer_dim[0].stride + y * buffer_dim[1].stride];
-      printf("%2x ", val);
+    // Print the result
+    printf("Gradient finished with exit code %d\n", error);
+    for (int y = 0; y < buffer_dim[1].extent; ++y) {
+      for (int x = 0; x < buffer_dim[0].extent; ++x) {
+        uint8_t val =
+            (output.host)[x * buffer_dim[0].stride + y * buffer_dim[1].stride];
+        printf("%2x ", val);
+      }
+      printf("\n");
     }
-    printf("\n");
-  }
 
+    wake_up((uint32_t)-1);
+  } else {
+    mempool_wfi();
+  }
   return 0;
 }
