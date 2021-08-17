78780911650e100a77b8e7b390153f46dd219aad
Gua Hao Khov <khovg@student.ethz.ch>
2021-08-18 01:38:49 +0200

    [apps] Implement 3x3 submatrix matmul for QLR

--- software/apps/systolic/matmul_qlr/main.c
+++ software/apps/systolic/matmul_qlr/main.c
@@ -27,30 +27,35 @@
 #include "synchronization.h"
 
 // Dimensions of matrices
-#define DIM_M 24
-#define DIM_N 24
-#define DIM_P 24
+#define DIM_M 96 // TODO: ASSERT DIVISABLE BY 3
+#define DIM_N 96
+#define DIM_P 96 // TODO: ASSERT DIVISABLE BY 3
 
 uint32_t *core_map;
 
 int32_t *matrix_A;
 int32_t *matrix_B;
-
-uint32_t rep_count;
-
-systolic_matrix_t *syst_matrix_A;
-systolic_matrix_t *syst_matrix_B;
-systolic_matrix_t *syst_matrix_C;
+int32_t *matrix_C;
 
 void generate_gradient_matrix(int32_t **matrix, uint32_t num_rows,
                               uint32_t num_cols) {
-  int32_t *new_matrix = (int32_t *)simple_malloc(num_rows * num_cols * sizeof(int32_t));
+  int32_t *new_matrix = (int32_t *)simple_malloc(num_rows * num_cols * 4);
   for (uint32_t y = 0; y < num_rows; ++y) {
     for (uint32_t x = 0; x < num_cols; ++x) {
       new_matrix[y * num_cols + x] = (int32_t)(y + x);
     }
   }
   *matrix = new_matrix;
+}
+
+void fill_gradient_matrix(int32_t *matrix, uint32_t num_rows,
+                          uint32_t num_cols, uint32_t core_id,
+                          uint32_t num_cores) {
+  for (uint32_t y = core_id; y < num_rows; y += num_cores) {
+    for (uint32_t x = 0; x < num_cols; ++x) {
+      matrix[y * num_cols + x] = (int32_t)(y + x);
+    }
+  }
 }
 
 void print_matrix(int32_t const *matrix, uint32_t num_rows,
@@ -70,17 +75,16 @@ int main() {
   uint32_t tile_id = core_id / 4;
 
   // Initialize synchronization variables
-  mempool_barrier_init(core_id);
+  mempool_barrier_init(core_id, num_cores);
 
   // Initialization
   mempool_init(core_id, num_cores);
 
   // Allocate systolic grid mapping
   if (core_id == 0) {
-    core_map = (uint32_t *)simple_malloc(num_cores * sizeof(uint32_t));
+    core_map = (uint32_t *)simple_malloc(num_cores * 4);
   }
 
-#if NUM_CORES == 16
   // ----------
   // 16 CORES
   // ----------
@@ -94,14 +98,13 @@ int main() {
   // uint32_t row_idx = core_id % 4;
 
   // Assign grid position (square wise)
-  uint32_t col_idx = tile_id % 2;
-  col_idx *= 2;
-  col_idx += core_id % 2;
-  uint32_t row_idx = tile_id / 2;
-  row_idx *= 2;
-  row_idx += (core_id % 4) / 2;
+  // uint32_t col_idx = tile_id % 2;
+  // col_idx *= 2;
+  // col_idx += core_id % 2;
+  // uint32_t row_idx = tile_id / 2;
+  // row_idx *= 2;
+  // row_idx += (core_id % 4) / 2;
 
-#elif NUM_CORES == 256
   // ----------
   // 256 CORES
   // ----------
@@ -111,8 +114,8 @@ int main() {
   // uint32_t row_idx = core_id / 16;
 
   // Assign grid position (col wise)
-  // uint32_t col_idx = core_id / 16;
-  // uint32_t row_idx = core_id % 16;
+  uint32_t col_idx = core_id / 16;
+  uint32_t row_idx = core_id % 16;
 
   // Assign grid position (square wise)
   // uint32_t col_idx = tile_id % 8;
@@ -123,29 +126,26 @@ int main() {
   // row_idx += (core_id % 4) / 2;
 
   // Assign grid position (square square wise)
-  uint32_t group_id = tile_id / 16;
-  uint32_t add_col = group_id % 2;
-  uint32_t add_row = group_id / 2;
-  uint32_t col_idx = tile_id % 4;
-  col_idx *= 2;
-  col_idx += core_id % 2;
-  col_idx += add_col * 8;
-  uint32_t row_idx = (tile_id % 16) / 4;
-  row_idx *= 2;
-  row_idx += (core_id % 4) / 2;
-  row_idx += add_row * 8;
-#else
-#error Unsupported NUM_CORES
-#endif
+  // uint32_t group_id = tile_id / 16;
+  // uint32_t add_col = group_id % 2;
+  // uint32_t add_row = group_id / 2;
+  // uint32_t col_idx = tile_id % 4;
+  // col_idx *= 2;
+  // col_idx += core_id % 2;
+  // col_idx += add_col * 8;
+  // uint32_t row_idx = (tile_id % 16) / 4;
+  // row_idx *= 2;
+  // row_idx += (core_id % 4) / 2;
+  // row_idx += add_row * 8;
 
   // Wait for all cores
-  mempool_barrier(num_cores);
+  mempool_barrier(num_cores, num_cores * 4);
 
   // Set tile and core mapping
   core_map[row_idx * SYSTOLIC_SIZE + col_idx] = core_id;
 
   // Wait for all cores
-  mempool_barrier(num_cores);
+  mempool_barrier(num_cores, num_cores * 4);
 
   // Setup
   if (core_id == 0) {
@@ -157,65 +157,63 @@ int main() {
     // Initialize systolic array
     systolic_init(core_map);
 
-    // Create systolic matrices
-    generate_gradient_matrix(&matrix_A, DIM_M, DIM_N);
-    systolic_matrix_create(&syst_matrix_A, matrix_A, DIM_M, DIM_N);
-    simple_free(matrix_A);
-    generate_gradient_matrix(&matrix_B, DIM_N, DIM_P);
-    systolic_matrix_create(&syst_matrix_B, matrix_B, DIM_N, DIM_P);
-    simple_free(matrix_B);
-    systolic_matrix_allocate(&syst_matrix_C, DIM_M, DIM_P);
-
-    // Print out systolic matrices A & B
-    // printf("> Print Systolic Matrices A & B\n");
-    // systolic_matrix_print(syst_matrix_A);
-    // systolic_matrix_print(syst_matrix_B);
-
-    // Set repetition count per submatrix of C (A->num_cols == B->num_rows)
-    rep_count = syst_matrix_A->num_cols / 2;
+    // Create matrices
+    matrix_A = (int32_t *)simple_malloc(DIM_M * DIM_N * 4);
+    matrix_B = (int32_t *)simple_malloc(DIM_N * DIM_P * 4);
+    matrix_C = (int32_t *)simple_malloc(DIM_M * DIM_P * 4);
   }
 
   // Wait for all cores
-  mempool_barrier(num_cores);
+  mempool_barrier(num_cores, num_cores * 4);
 
+  // Fill matrices with gradient
+  fill_gradient_matrix(matrix_A, DIM_M, DIM_N, core_id, num_cores);
+  fill_gradient_matrix(matrix_B, DIM_N, DIM_P, core_id, num_cores);
+
+  // Start message
   if (core_id == 0) {
+    // Print out matrices A & B
+    // printf("> Print Matrices A & B\n");
+    // print_matrix(matrix_A, DIM_M, DIM_N);
+    // print_matrix(matrix_B, DIM_N, DIM_P);
+
     printf("> Start\n");
   }
 
   // Start benchmark for all cores
-  mempool_barrier(num_cores);
+  mempool_barrier(num_cores, num_cores * 4);
   mempool_start_benchmark();
 
   if ((row_idx == 0) && (col_idx == 0)) {
-    systolic_rcp_pe(rep_count, syst_matrix_A, syst_matrix_B, syst_matrix_C);
+    systolic_rcp_pe(DIM_M, DIM_N, DIM_P, matrix_A, matrix_B, matrix_C);
   }
 
   if ((row_idx == 0) && (col_idx != 0)) {
-    systolic_cp_pe(col_idx, rep_count, syst_matrix_B, syst_matrix_C);
+    systolic_cp_pe(col_idx, DIM_M, DIM_N, DIM_P, matrix_B, matrix_C);
   }
 
   if ((row_idx != 0) && (col_idx == 0)) {
-    systolic_rp_pe(row_idx, rep_count, syst_matrix_A, syst_matrix_C);
+    systolic_rp_pe(row_idx, DIM_M, DIM_N, DIM_P, matrix_A, matrix_C);
   }
 
   if ((row_idx != 0) && (col_idx != 0)) {
-    systolic_np_pe(row_idx, col_idx, rep_count, syst_matrix_C);
+    systolic_np_pe(row_idx, col_idx, DIM_M, DIM_N, DIM_P, matrix_C);
   }
 
   // Stop benchmark for all cores
   mempool_stop_benchmark();
-  mempool_barrier(num_cores);
+  mempool_barrier(num_cores, num_cores * 4);
 
   // Print out benchmark
   if (core_id == 0) {
     printf("> End\n");
 
-    // Print out systolic matrix C
-    // printf("> Print Systolic Matrix C\n");
-    // systolic_matrix_print(syst_matrix_C);
+    // Print out matrix C
+    // printf("> Print Matrix C\n");
+    // print_matrix(matrix_C, DIM_M, DIM_P);
   }
 
   // wait until all cores have finished
-  mempool_barrier(num_cores);
+  mempool_barrier(num_cores, num_cores * 4);
   return 0;
 }
