2173c51369515abdb36dd038076778f35675257a
Samuel Riedel <sriedel@iis.ee.ethz.ch>
2021-08-26 17:22:08 +0200

    [software] Replace polling based `hello_world` with `sleep_wakeup`

--- software/apps/hello_world/main.c
+++ software/apps/hello_world/main.c
@@ -12,26 +12,18 @@
 #include "runtime.h"
 #include "synchronization.h"
 
-volatile uint32_t atomic __attribute__((section(".l2"))) = NUM_CORES;
-
-extern volatile uint32_t tcdm_start_address_reg;
-extern volatile uint32_t tcdm_end_address_reg;
-
 int main() {
   uint32_t core_id = mempool_get_core_id();
   uint32_t num_cores = mempool_get_core_count();
   // Initialize synchronization variables
   mempool_barrier_init(core_id);
-  if (core_id == 0) {
-    atomic = 0;
+
+  if (core_id != 0) {
+    mempool_wfi();
   }
-  mempool_barrier(num_cores);
 
-  while (atomic != core_id)
-    mempool_wait(2 * num_cores);
-
-  printf("Core %d says Hello!\n", core_id);
-  atomic++;
+  printf("Core %3d says Hello!\n", core_id);
+  wake_up(core_id + 1);
 
   // wait until all cores have finished
   mempool_barrier(num_cores);
