358c7b2ec28e4ab9570c0ec437be9337ec32149f
Samuel Riedel <sriedel@iis.ee.ethz.ch>
2021-08-26 15:21:05 +0200

    [software] Use `wfi` to implement barrier

--- software/runtime/synchronization.c
+++ software/runtime/synchronization.c
@@ -11,44 +11,26 @@
 #include "synchronization.h"
 
 uint32_t volatile barrier __attribute__((section(".l1")));
-uint32_t volatile barrier_iteration __attribute__((section(".l1")));
-uint32_t volatile barrier_init __attribute__((section(".l2"))) = 0;
 
-void mempool_barrier_init(uint32_t core_id, uint32_t num_cores) {
+void mempool_barrier_init(uint32_t core_id) {
   if (core_id == 0) {
+    // Initialize the barrier
     barrier = 0;
-    barrier_iteration = 0;
-    barrier_init = 1;
-  } else {
-    while (!barrier_init)
-      mempool_wait(2 * num_cores);
-  }
-}
-
-void mempool_barrier_init_sleep(uint32_t core_id, uint32_t num_cores) {
-  if (core_id == 0) {
-    // Give other cores time to go to sleep
-    mempool_wait(4 * num_cores);
-    barrier = 0;
-    barrier_iteration = 0;
-    barrier_init = 1;
-    wake_up((uint32_t)-1);
+    wake_up_all();
+    mempool_wfi();
   } else {
     mempool_wfi();
   }
 }
 
-void mempool_barrier(uint32_t num_cores, uint32_t cycles) {
-  // Remember previous iteration
-  uint32_t iteration_old = barrier_iteration;
-
+void mempool_barrier(uint32_t num_cores) {
   // Increment the barrier counter
-  if ((num_cores - 1) == __atomic_fetch_add(&barrier, 1, __ATOMIC_SEQ_CST)) {
-    barrier = 0;
-    __atomic_fetch_add(&barrier_iteration, 1, __ATOMIC_SEQ_CST);
-  } else {
-    // Some threads have not reached the barrier --> Let's wait
-    while (iteration_old == barrier_iteration)
-      mempool_wait(cycles);
+  if ((num_cores - 1) == __atomic_fetch_add(&barrier, 1, __ATOMIC_RELAXED)) {
+    __atomic_store_n(&barrier, 0, __ATOMIC_RELAXED);
+    __sync_synchronize(); // Full memory barrier
+    wake_up_all();
   }
+  // Some threads have not reached the barrier --> Let's wait
+  // Clear the wake-up trigger for the last core reaching the barrier as well
+  mempool_wfi();
 }
--- software/runtime/synchronization.h
+++ software/runtime/synchronization.h
@@ -8,7 +8,7 @@
 #define __SYNCHRONIZATION_H__
 
 // Barrier functions
-void mempool_barrier_init(uint32_t core_id, uint32_t num_cores);
-void mempool_barrier(uint32_t num_cores, uint32_t cycles);
+void mempool_barrier_init(uint32_t core_id);
+void mempool_barrier(uint32_t num_cores);
 
 #endif // __SYNCHRONIZATION_H__
