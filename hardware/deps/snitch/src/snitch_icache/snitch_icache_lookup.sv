bcc1387f40c1b4bcc8447c7e34cc4df3444bfd87
Samuel Riedel <sriedel@iis.ee.ethz.ch>
2021-10-26 12:10:27 +0200

    [hardware] Give write priority in lookup module to remove data register

--- hardware/deps/snitch/src/snitch_icache/snitch_icache_lookup.sv
+++ hardware/deps/snitch/src/snitch_icache/snitch_icache_lookup.sv
@@ -46,13 +46,11 @@ module snitch_icache_lookup #(
     initial assert(CFG != '0);
     `endif
 
-    localparam int unsigned DataAddrWdith = CFG.SET_ALIGN + CFG.COUNT_ALIGN;
+    localparam int unsigned DATA_ADDR_WIDTH = CFG.SET_ALIGN + CFG.COUNT_ALIGN;
 
     typedef struct packed {
         logic [CFG.FETCH_AW-1:0]     addr;
-        logic [CFG.LINE_WIDTH-1:0]   data;
         logic [CFG.ID_WIDTH_REQ-1:0] id;
-        logic                        write;
     } tag_req_t;
 
     typedef struct packed {
@@ -73,13 +71,19 @@ module snitch_icache_lookup #(
 
     // Multiplex read and write access to the RAMs onto one port, prioritizing
     // write accesses.
+    logic [CFG.COUNT_ALIGN:0]   init_count_q;
+    logic                       init_phase;
+
     logic [CFG.COUNT_ALIGN-1:0] tag_addr;
     logic [CFG.SET_COUNT-1:0]   tag_enable;
     logic [CFG.TAG_WIDTH+1:0]   tag_wdata, tag_rdata [CFG.SET_COUNT];
     logic                       tag_write;
-    logic [CFG.COUNT_ALIGN:0]   init_count_q;
-    logic [CFG.COUNT_ALIGN-1:0] data_addr;
-    logic [DataAddrWdith-1:0]   data_bank_addr;
+
+    logic [DATA_ADDR_WIDTH-1:0] data_addr;
+    logic                       data_enable;
+    data_rsp_t                  data_wdata, data_rdata;
+    logic                       data_write;
+
     logic                       req_valid, req_ready;
     tag_req_t                   tag_req_d, tag_req_q;
     tag_rsp_t                   tag_rsp_d, tag_rsp_q;
@@ -95,7 +99,21 @@ module snitch_icache_lookup #(
 
     logic [CFG.SET_ALIGN-1:0] out_set;
 
-    always_comb begin : p_portmux
+    // Initialization and flush FSM
+    always_ff @(posedge clk_i, negedge rst_ni) begin
+        if (!rst_ni)
+            init_count_q <= '0;
+        else if (flush_valid_i)
+            init_count_q <= '0;
+        else if (init_count_q != $unsigned(CFG.LINE_COUNT))
+            init_count_q <= init_count_q + 1;
+    end
+
+    assign init_phase = init_count_q != $unsigned(CFG.LINE_COUNT);
+    assign flush_ready_o = flush_valid_i & (init_count_q == $unsigned(CFG.LINE_COUNT));
+
+    // Tag bank port mux
+    always_comb begin
         write_ready_o = 0;
         in_ready_o = 0;
 
@@ -107,21 +125,16 @@ module snitch_icache_lookup #(
 
         tag_req_d = tag_req_q;
 
-        if (init_count_q != $unsigned(CFG.LINE_COUNT)) begin
+        if (init_phase) begin
             tag_addr   = init_count_q;
             tag_enable = '1;
             tag_write  = 1'b1;
             tag_wdata  = '0;
-        end else  if (write_valid_i) begin
+        end else if (write_valid_i) begin
             tag_addr   = write_addr_i;
             tag_enable = $unsigned(1 << write_set_i);
             tag_write  = 1'b1;
-            write_ready_o = req_ready; // From Fall-through register
-            // Store request to data bank
-            req_valid       = 1'b1;
-            tag_req_d.addr  = {write_set_i, write_addr_i} << CFG.LINE_ALIGN;
-            tag_req_d.data  = write_data_i;
-            tag_req_d.write = 1'b1;
+            write_ready_o = 1'b1;
         end else if (in_valid_i) begin
             // Read the tag banks
             tag_enable = '1;
@@ -130,19 +143,7 @@ module snitch_icache_lookup #(
             req_valid       = 1'b1;
             tag_req_d.addr  = in_addr_i;
             tag_req_d.id    = in_id_i;
-            tag_req_d.write = 1'b0;
         end
-    end
-
-    assign flush_ready_o = flush_valid_i & (init_count_q == $unsigned(CFG.LINE_COUNT));
-
-    always_ff @(posedge clk_i, negedge rst_ni) begin
-        if (!rst_ni)
-            init_count_q <= '0;
-        else if (flush_valid_i)
-            init_count_q <= '0;
-        else if (init_count_q != $unsigned(CFG.LINE_COUNT))
-            init_count_q <= init_count_q + 1;
     end
 
     // Tag stage
@@ -188,7 +189,7 @@ module snitch_icache_lookup #(
         .ready_o    (req_ready),
         .data_i     (tag_req_d),
         .valid_o    (tag_valid),
-        .ready_i    (tag_ready),
+        .ready_i    (tag_ready && !data_write),
         .data_o     (tag_req_q)
     );
 
@@ -203,7 +204,7 @@ module snitch_icache_lookup #(
             line_hit[i] = tag_rdata[i][CFG.TAG_WIDTH+1] && tag_rdata[i][CFG.TAG_WIDTH-1:0] == required_tag;
             errors[i] = tag_rdata[i][CFG.TAG_WIDTH] && line_hit[i];
         end
-        tag_rsp_d.hit = |line_hit & ~tag_req_q.write; // Don't let refills trigger "valid" lookups
+        tag_rsp_d.hit = |line_hit;
         tag_rsp_d.error = |errors;
     end
 
@@ -213,8 +214,8 @@ module snitch_icache_lookup #(
         .empty_o  (               )
     );
 
-    `FF(req_handshake, req_valid && req_ready && !tag_req_d.write, 1'b0, clk_i, rst_ni)
-    `FF(tag_handshake, tag_valid && tag_ready && !tag_req_q.write, 1'b0, clk_i, rst_ni)
+    `FF(req_handshake, req_valid && req_ready, 1'b0, clk_i, rst_ni)
+    `FF(tag_handshake, tag_valid && tag_ready && !data_write, 1'b0, clk_i, rst_ni)
     //`FF(data_handshake, data_valid && data_ready, 1'b0, clk_i, rst_ni)
     assign data_handshake = data_valid && data_ready;
 
@@ -229,23 +230,38 @@ module snitch_icache_lookup #(
         .ready_o      (req_ready_ft),
         .data_i       (tag_rsp_d   ),
         .valid_o      (tag_valid_ft),
-        .ready_i      (tag_handshake),
+        .ready_i      (tag_valid && tag_ready && !data_write),
         .data_o       (tag_rsp_q   )
     );
 
     //`ASSERT(req_ftready, req_ready_ft == req_ready)
     //`ASSERT(tag_ftvalid, tag_valid_ft == tag_valid)
     `ASSERT(reqhandshake, req_handshake |-> req_ready_ft);
-    `ASSERT(taghandshake, tag_handshake |-> tag_valid_ft && tag_ready_ft);
+    `ASSERT(taghandshake, tag_valid && tag_ready && !data_write |->  tag_valid_ft);
+    `ASSERT(tagpophandshake, tag_handshake |->  tag_ready_ft);
     `ASSERT(datahandshake, data_handshake |-> data_valid_ft);
 
     // Data stage
-    // Single data bank for all sets
+    // Data bank port mux
     always_comb begin
-        data_bank_addr = tag_req_q.addr >> CFG.LINE_ALIGN;
-        if (!tag_req_q.write) begin
-            data_bank_addr[CFG.COUNT_ALIGN +: CFG.SET_ALIGN] = tag_rsp_q.set;
+        // Write interface
+        data_addr   = '0;
+        data_enable = 1'b0;
+        data_wdata  = '0;
+        data_write  = '0;
+
+        if (!init_phase && write_valid_i) begin
+            // Write
+            data_addr   = {write_set_i, write_addr_i};
+            data_enable = 1'b1;
+            data_wdata  = write_data_i;
+            data_write  = 1'b1;
+        end else if (tag_valid) begin
+            data_addr   = {tag_rsp_q.set, tag_req_q.addr[CFG.LINE_ALIGN +: CFG.COUNT_ALIGN]};
+            data_enable = 1'b1;
+            data_write  = 1'b0;
         end
+        // TODO: simplify
     end
 
     tc_sram #(
@@ -253,14 +269,14 @@ module snitch_icache_lookup #(
         .NumWords  ( CFG.LINE_COUNT * CFG.SET_COUNT ),
         .NumPorts  ( 1                              )
     ) i_data (
-        .clk_i   ( clk_i           ),
-        .rst_ni  ( rst_ni          ),
-        .req_i   ( tag_valid       ),
-        .we_i    ( tag_req_q.write ),
-        .addr_i  ( data_bank_addr  ),
-        .wdata_i ( tag_req_q.data  ),
-        .be_i    ( '1              ),
-        .rdata_o ( data_rsp_d      )
+        .clk_i   ( clk_i       ),
+        .rst_ni  ( rst_ni      ),
+        .req_i   ( data_enable ),
+        .we_i    ( data_write  ),
+        .addr_i  ( data_addr   ),
+        .wdata_i ( data_wdata  ),
+        .be_i    ( '1          ),
+        .rdata_o ( data_rdata  )
     );
 
     fall_through_register_2 #(
@@ -272,7 +288,7 @@ module snitch_icache_lookup #(
         .testmode_i   (1'b0      ),
         .valid_i      (tag_handshake),
         .ready_o      (tag_ready_ft),
-        .data_i       (data_rsp_d ),
+        .data_i       (data_rdata   ),
         .valid_o      (data_valid_ft),
         .ready_i      (data_handshake),
         .data_o       (data_rsp_q)
@@ -292,7 +308,7 @@ module snitch_icache_lookup #(
     ) i_data_register (
         .clk_i      (clk_i                        ),
         .rst_ni     (rst_ni                       ),
-        .valid_i    (tag_valid && !tag_req_q.write),
+        .valid_i    (tag_valid && !data_write     ),
         .ready_o    (tag_ready                    ),
         .data_i     (data_req_d                   ),
         .valid_o    (data_valid                   ),
