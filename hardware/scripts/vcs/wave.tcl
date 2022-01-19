f83a616b1d6cc622cc8dcb094e3cec66002a04cf
Matheus Cavalcante <matheusd@iis.ee.ethz.ch>
2022-01-19 14:06:54 +0100

    [vcs] Use Verdi instead of DVE

--- hardware/scripts/vcs/wave.tcl
+++ hardware/scripts/vcs/wave.tcl
@@ -2,16 +2,12 @@
 # Solderpad Hardware License, Version 0.51, see LICENSE for details.
 # SPDX-License-Identifier: SHL-0.51
 
+# Create an nWave window
+wvCreateWindow
+
 # Add a vector of the core's wfi signal to quickly see which cores are active
-add_wave -group wfi /mempool_tb/wfi
-
-# Add all cores from group 0 tile 0
-for {set core 0}  {$core < [get -radix dec mempool_pkg::NumCoresPerTile]} {incr core} {
-    do ../scripts/vcs/wave_core.tcl 0 0 $core
-}
-
-# Add specific cores from different tiles
-do ../scripts/vcs/wave_core.tcl 1 0 0
+wvAddGroup wfi
+wvAddSignal -group {wfi {mempool_tb/wfi}}
 
 # Add min function (DVE does not support TCL8.5)
 proc min args {
@@ -24,25 +20,99 @@ proc min args {
     return $minval
 }
 
+# Add all cores from group 0 tile 0
+set group 0
+set tile 0
+for {set core 0}  {$core < [min 4 [get -radix dec mempool_pkg::NumCoresPerTile]]} {incr core} {
+    source ../scripts/vcs/wave_core.tcl
+}
+
+# Add specific cores from different tiles
+set group 1
+set tile 0
+set core 0
+source ../scripts/vcs/wave_core.tcl
+
 # Add groups
 for {set group 0} {$group < [get -radix dec mempool_pkg::NumGroups]} {incr group} {
     # Create the group
-    add_wave -group group_\[$group\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/i_local_interco/*
-    delete_group -group group_\[$group\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/i_local_interco/*
+    wvAddGroup group\[$group\]
 
     # Add tiles
-    for {set tile 0} {$tile < [min 4 [get -radix dec mempool_pkg::NumTilesPerGroup]]} {incr tile} {
-        do ../scripts/vcs/wave_tile.tcl $group $tile
+    for {set tile 0} {$tile < [min 2 [get -radix dec mempool_pkg::NumTilesPerGroup]]} {incr tile} {
+        source ../scripts/vcs/wave_tile.tcl
     }
 
     # Interconnects
     for {set tgtgroup 0} {$tgtgroup < [get -radix dec mempool_pkg::NumGroups]} {incr tgtgroup} {
         if {$tgtgroup != $group} {
             set interco_idx [expr $group ^ $tgtgroup]
-            add_wave -group group_\[$group\]|interconnect_to_group\[$tgtgroup\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_remote_interco\[$interco_idx\]/i_remote_interco/*
+            wvSelectGroup group\[$group\]
+            wvAddSubGroup interconnect_to_group\[$tgtgroup\]
+            wvSetPosition [subst {(group\[$group\]/interconnect_to_group\[$tgtgroup\] last)}]
+
+            wvAddSignal mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_remote_interco\[$interco_idx\]/i_remote_interco/clk_i \
+                        mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_remote_interco\[$interco_idx\]/i_remote_interco/rst_ni \
+                        mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_remote_interco\[$interco_idx\]/i_remote_interco/req_valid_i \
+                        mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_remote_interco\[$interco_idx\]/i_remote_interco/req_ready_o \
+                        mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_remote_interco\[$interco_idx\]/i_remote_interco/req_tgt_addr_i \
+                        mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_remote_interco\[$interco_idx\]/i_remote_interco/req_wen_i \
+                        mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_remote_interco\[$interco_idx\]/i_remote_interco/req_wdata_i \
+                        mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_remote_interco\[$interco_idx\]/i_remote_interco/req_be_i \
+                        mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_remote_interco\[$interco_idx\]/i_remote_interco/resp_valid_o \
+                        mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_remote_interco\[$interco_idx\]/i_remote_interco/resp_ready_i \
+                        mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_remote_interco\[$interco_idx\]/i_remote_interco/resp_rdata_o \
+                        mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_remote_interco\[$interco_idx\]/i_remote_interco/req_valid_o \
+                        mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_remote_interco\[$interco_idx\]/i_remote_interco/req_ready_i \
+                        mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_remote_interco\[$interco_idx\]/i_remote_interco/req_ini_addr_o \
+                        mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_remote_interco\[$interco_idx\]/i_remote_interco/req_tgt_addr_o \
+                        mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_remote_interco\[$interco_idx\]/i_remote_interco/req_wen_o \
+                        mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_remote_interco\[$interco_idx\]/i_remote_interco/req_wdata_o \
+                        mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_remote_interco\[$interco_idx\]/i_remote_interco/req_be_o \
+                        mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_remote_interco\[$interco_idx\]/i_remote_interco/resp_valid_i \
+                        mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_remote_interco\[$interco_idx\]/i_remote_interco/resp_ready_o \
+                        mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_remote_interco\[$interco_idx\]/i_remote_interco/resp_ini_addr_i \
+                        mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_remote_interco\[$interco_idx\]/i_remote_interco/resp_rdata_i
         }
     }
-    add_wave -group group_\[$group\]|interconnect_local /mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/i_local_interco/*
+
+    wvSelectGroup group\[$group\]
+    wvAddSubGroup local_interconnect
+    wvSetPosition [subst {(group\[$group\]/local_interconnect last)}]
+
+    wvAddSignal mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/i_local_interco/clk_i \
+                mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/i_local_interco/rst_ni \
+                mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/i_local_interco/req_valid_i \
+                mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/i_local_interco/req_ready_o \
+                mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/i_local_interco/req_tgt_addr_i \
+                mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/i_local_interco/req_wen_i \
+                mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/i_local_interco/req_wdata_i \
+                mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/i_local_interco/req_be_i \
+                mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/i_local_interco/resp_valid_o \
+                mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/i_local_interco/resp_ready_i \
+                mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/i_local_interco/resp_rdata_o \
+                mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/i_local_interco/req_valid_o \
+                mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/i_local_interco/req_ready_i \
+                mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/i_local_interco/req_ini_addr_o \
+                mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/i_local_interco/req_tgt_addr_o \
+                mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/i_local_interco/req_wen_o \
+                mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/i_local_interco/req_wdata_o \
+                mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/i_local_interco/req_be_o \
+                mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/i_local_interco/resp_valid_i \
+                mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/i_local_interco/resp_ready_o \
+                mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/i_local_interco/resp_ini_addr_i \
+                mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/i_local_interco/resp_rdata_i
 }
 
-add_wave -group Control_Registers /mempool_tb/dut/i_ctrl_registers/*
+wvAddGroup Control_Registers
+wvSetPosition {(Control_Registers last)}
+wvAddSignal mempool_tb/dut/i_ctrl_registers/clk_i \
+            mempool_tb/dut/i_ctrl_registers/rst_ni \
+            mempool_tb/dut/i_ctrl_registers/axi_lite_slave_req_i \
+            mempool_tb/dut/i_ctrl_registers/axi_lite_slave_resp_o \
+            mempool_tb/dut/i_ctrl_registers/eoc_o \
+            mempool_tb/dut/i_ctrl_registers/eoc_valid_o \
+            mempool_tb/dut/i_ctrl_registers/wake_up_o \
+            mempool_tb/dut/i_ctrl_registers/tcdm_start_address_o \
+            mempool_tb/dut/i_ctrl_registers/tcdm_end_address_o \
+            mempool_tb/dut/i_ctrl_registers/num_cores_o
--- hardware/scripts/vcs/wave_tile.tcl
+++ hardware/scripts/vcs/wave_tile.tcl
@@ -2,59 +2,67 @@
 # Solderpad Hardware License, Version 0.51, see LICENSE for details.
 # SPDX-License-Identifier: SHL-0.51
 
-# Create group for group $1 tile $2
+# Create group for group $group tile $tile
 
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/clk_i
-delete_group -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/clk_i
-add_wave -group group_\[$1\]|Tile_\[$2\]|Params mempool_pkg::NumBanksPerTile
-add_wave -group group_\[$1\]|Tile_\[$2\]|Params mempool_pkg::NumTiles
-add_wave -group group_\[$1\]|Tile_\[$2\]|Params mempool_pkg::NumBanks
-add_wave -group group_\[$1\]|Tile_\[$2\]|Params /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/TCDMBaseAddr
-add_wave -group group_\[$1\]|Tile_\[$2\]|Params /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/BootAddr
-add_wave -group group_\[$1\]|Tile_\[$2\]|Params /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/gen_caches\[0\].i_snitch_icache.LINE_WIDTH
-add_wave -group group_\[$1\]|Tile_\[$2\]|Params /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/gen_caches\[0\].i_snitch_icache.LINE_COUNT
-add_wave -group group_\[$1\]|Tile_\[$2\]|Params /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/gen_caches\[0\].i_snitch_icache.SET_COUNT
-add_wave -group group_\[$1\]|Tile_\[$2\]|Params mempool_pkg::ICacheLineWidth
-add_wave -group group_\[$1\]|Tile_\[$2\]|Params mempool_pkg::ICacheSizeByte
-add_wave -group group_\[$1\]|Tile_\[$2\]|Params mempool_pkg::ICacheSets
-add_wave -group group_\[$1\]|Tile_\[$2\]|Params mempool_pkg::NumCores
-add_wave -group group_\[$1\]|Tile_\[$2\]|Params /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/TCDMSize
-add_wave -group group_\[$1\]|Tile_\[$2\]|Params /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/TCDMMask
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/clk_i
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/rst_ni
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/scan_enable_i
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/scan_data_i
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/scan_data_o
-add_wave -group group_\[$1\]|Tile_\[$2\] -radix unsigned /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/tile_id_i
+wvSelectGroup group\[$group\]
+wvAddSubGroup tile\[$tile\]
 
-add_wave -group group_\[$1\]|Tile_\[$2\] -divider TCDM
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/tcdm_master_*
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/tcdm_slave_*
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/axi_*
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/refill_*
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/snitch_inst_*
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/snitch_data_*
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/bank_req_*
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/bank_resp_*
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/postreg_tcdm_slave_req_*
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/prereg_tcdm_slave_resp_*
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/prereg_tcdm_master_req_*
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/postreg_tcdm_master_resp_*
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/remote_req_*
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/remote_resp_*
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/local_req_*
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/local_resp_*
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/soc_data_*
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/mask_map
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/soc_req_o
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/soc_resp_i
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/soc_qvalid
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/soc_qready
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/soc_pvalid
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/soc_pready
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/axi_mst_req
-add_wave -group group_\[$1\]|Tile_\[$2\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/axi_mst_resp
+wvSetPosition [subst {(group\[$group\]/tile\[$tile\] last)}]
 
-for {set i 0} {$i < 16} {incr i} {
-	add_wave -group group_\[$1\]|Tile_\[$2\]|tcdm_adapter\[$i\] /mempool_tb/dut/i_mempool_cluster/gen_groups\[$1\]/i_group/gen_tiles\[$2\]/i_tile/gen_banks\[$i\]/i_tcdm_adapter/*
-}
+add_wave -divider Clock
+wvAddSignal mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/clk_i \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/rst_ni \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/tile_id_i \
+
+add_wave -divider TCDM
+wvAddSignal mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/tcdm_master_req_o \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/tcdm_master_req_valid_o \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/tcdm_master_req_ready_i \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/tcdm_master_resp_i \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/tcdm_master_resp_valid_i \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/tcdm_master_resp_ready_o \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/tcdm_slave_req_i \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/tcdm_slave_req_valid_i \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/tcdm_slave_req_ready_o \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/tcdm_slave_resp_o \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/tcdm_slave_resp_valid_o \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/tcdm_slave_resp_ready_i \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/axi_mst_req_o \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/axi_mst_resp_i \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/refill_qaddr \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/refill_qlen \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/refill_qvalid \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/refill_qready \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/refill_pdata \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/refill_perror \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/refill_pvalid \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/refill_plast \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/refill_pready \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/snitch_inst_addr \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/snitch_inst_data \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/snitch_inst_valid \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/snitch_inst_ready \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/snitch_data_qaddr \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/snitch_data_qwrite \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/snitch_data_qamo \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/snitch_data_qdata \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/snitch_data_qstrb \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/snitch_data_qid \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/snitch_data_qvalid \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/snitch_data_qready \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/snitch_data_pdata \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/snitch_data_perror \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/snitch_data_pid \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/snitch_data_pvalid \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/snitch_data_pready \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/mask_map \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/soc_req_o \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/soc_resp_i \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/soc_qvalid \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/soc_qready \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/soc_pvalid \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/soc_pready \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/axi_mst_req \
+            mempool_tb/dut/i_mempool_cluster/gen_groups\[$group\]/i_group/gen_tiles\[$tile\]/i_tile/axi_mst_resp
+
+wvCollapseAllGroups
