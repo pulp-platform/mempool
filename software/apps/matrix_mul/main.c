319fb629e8afc2a148f90d8f8aacb00df3f48854
Matheus Cavalcante <matheusd@iis.ee.ethz.ch>
2022-01-28 10:10:31 +0100

    [software] Control the size of the matrix multiplication kernel

--- software/apps/matrix_mul/main.c
+++ software/apps/matrix_mul/main.c
@@ -15,9 +15,9 @@
 
 // Define Matrix dimensions:
 // C = AB with A=[MxN], B=[NxP], C=[MxP]
-#define M (NUM_CORES / 2)
-#define N (NUM_CORES / 2)
-#define P (NUM_CORES / 2)
+#define M (NUM_CORES >= 256 ? 128 : (NUM_CORES / 2))
+#define N (NUM_CORES >= 256 ? 128 : (NUM_CORES / 2))
+#define P (NUM_CORES >= 256 ? 128 : (NUM_CORES / 2))
 // Specify how the matrices A and B should be initialized
 // The entries will follow this format:
 // a(i,j) = A_a*i + A_b*j + A_c
