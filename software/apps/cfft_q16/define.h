864888e1cd896482160c1289af4c87e245886fc3
Marco Bertuletti <mbertuletti@iis.ee.ethz.ch>
2022-04-26 12:45:07 +0200

    [apps] Modify fft with xpulpimg

--- software/apps/cfft_q16/define.h
+++ software/apps/cfft_q16/define.h
@@ -1,7 +1,8 @@
 /* CFFT mempool data */
-#define N_CSAMPLES 1024
+#define N_CSAMPLES 2048
 #define N_RSAMPLES 2*N_CSAMPLES
-#define TEST_1024
+#define TEST_2048
 #define PARALLEL
-#define SINGLE
+//#define SINGLE
+
 
