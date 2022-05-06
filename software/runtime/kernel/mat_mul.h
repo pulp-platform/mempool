c4221497eb60f7f754ea3953ff0c7f9874e20b29
Samuel Riedel <sriedel@iis.ee.ethz.ch>
2022-05-07 00:54:52 +0200

    [software] Add 4x4 matmul

--- software/runtime/kernel/mat_mul.h
+++ software/runtime/kernel/mat_mul.h
@@ -151,6 +151,186 @@ void mat_mul_unrolled_2x2_parallel(int32_t const *__restrict__ A,
   }
 }
 
+void mat_mul_unrolled_4x4_parallel(int32_t const *__restrict__ A,
+                                   int32_t const *__restrict__ B,
+                                   int32_t *__restrict__ C, uint32_t M,
+                                   uint32_t N, uint32_t P, uint32_t id,
+                                   uint32_t numThreads) {
+  // Parallelize by assigning each core one row
+  uint32_t const c = 16; // How many columns to split the matrix into
+  uint32_t const c_start = (P / c) * (id % c);
+  uint32_t const c_end = (P / c) * ((id % c) + 1);
+  for (uint32_t i = 4 * (id / c); i < M; i += 4 * (numThreads / c)) {
+    for (uint32_t j = c_start; j < c_end; j += 4) {
+      // Initialize 4x4 output tile
+      int32_t c00 = 0, c01 = 0, c02 = 0, c03 = 0;
+      int32_t c10 = 0, c11 = 0, c12 = 0, c13 = 0;
+      int32_t c20 = 0, c21 = 0, c22 = 0, c23 = 0;
+      int32_t c30 = 0, c31 = 0, c32 = 0, c33 = 0;
+      for (uint32_t k = 0; k < N; k += 1) {
+        // Explicitly load the values first to help with scheduling
+        int32_t b0 = B[k * P + j + 0];
+        int32_t b1 = B[k * P + j + 1];
+        int32_t b2 = B[k * P + j + 2];
+        int32_t b3 = B[k * P + j + 3];
+        // A could be local with scrambling
+        int32_t a0 = A[(i + 0) * N + k];
+        int32_t a1 = A[(i + 1) * N + k];
+        int32_t a2 = A[(i + 2) * N + k];
+        int32_t a3 = A[(i + 3) * N + k];
+        // Compute
+        c00 += a0 * b0;
+        c01 += a0 * b1;
+        c02 += a0 * b2;
+        c03 += a0 * b3;
+        c10 += a1 * b0;
+        c11 += a1 * b1;
+        c12 += a1 * b2;
+        c13 += a1 * b3;
+        c20 += a2 * b0;
+        c21 += a2 * b1;
+        c22 += a2 * b2;
+        c23 += a2 * b3;
+        c30 += a3 * b0;
+        c31 += a3 * b1;
+        c32 += a3 * b2;
+        c33 += a3 * b3;
+      }
+      // Store
+      C[(i + 0) * P + j + 0] = c00;
+      C[(i + 0) * P + j + 1] = c01;
+      C[(i + 0) * P + j + 2] = c02;
+      C[(i + 0) * P + j + 3] = c03;
+      C[(i + 1) * P + j + 0] = c10;
+      C[(i + 1) * P + j + 1] = c11;
+      C[(i + 1) * P + j + 2] = c12;
+      C[(i + 1) * P + j + 3] = c13;
+      C[(i + 2) * P + j + 0] = c20;
+      C[(i + 2) * P + j + 1] = c21;
+      C[(i + 2) * P + j + 2] = c22;
+      C[(i + 2) * P + j + 3] = c23;
+      C[(i + 3) * P + j + 0] = c30;
+      C[(i + 3) * P + j + 1] = c31;
+      C[(i + 3) * P + j + 2] = c32;
+      C[(i + 3) * P + j + 3] = c33;
+    }
+  }
+}
+
+#define NN (64)
+
+void mat_mul_unrolled_4x4_parallel_asm(int32_t const *__restrict__ A,
+                                       int32_t const *__restrict__ B,
+                                       int32_t *__restrict__ C, uint32_t M,
+                                       uint32_t N, uint32_t P, uint32_t id,
+                                       uint32_t numThreads) {
+  // Parallelize by assigning each core one row
+  uint32_t const c = 16; // How many columns to split the matrix into
+  uint32_t const c_start = (P / c) * (id % c);
+  uint32_t const c_end = (P / c) * ((id % c) + 1);
+  for (uint32_t i = 4 * (id / c); i < M; i += 4 * (numThreads / c)) {
+    for (uint32_t j = c_start; j < c_end; j += 4) {
+
+      // Address registers
+      int32_t const *addr_a = &A[i * N];
+      int32_t const *addr_b = &B[j];
+      int32_t const *end_b = &B[N * P + j];
+      int32_t const *addr_c = &C[i * P + j];
+      int32_t const N3_1 = (-3 * (int32_t)N + 1) * 4;
+      int32_t const P_3 = ((int32_t)P - 3) * 4;
+      register int32_t k asm("x1") = (int32_t)end_b;
+
+      //      x12 x13 x14 x15
+      //
+      // x3   x16 x17 x18 x19
+      // x9   x20 x21 x22 x23
+      // x10  x24 x25 x26 x27
+      // x11  x28 x29 x30 x31
+      //
+      //
+      __asm__ volatile(
+          ".balign 16 \n\t"
+          // Outer loop: Initialize and preload. Execute this loop P times
+          // TODO arrange
+          "p.lw  x3, %[N](%[addr_a]!) \n\t"
+          "p.lw  x4, %[N](%[addr_a]!) \n\t"
+          "p.lw x10, %[N](%[addr_a]!) \n\t"
+          "p.lw x11, %[N3_1](%[addr_a]!) \n\t" // Increment by -3N+1
+          "p.lw x12, 4(%[addr_b]!) \n\t"
+          "p.lw x13, 4(%[addr_b]!) \n\t"
+          "p.lw x14, 4(%[addr_b]!) \n\t"
+          "p.lw x15, %[P_3](%[addr_b]!) \n\t" // Increment by P-3
+          // Initial computation
+          "mul x16,  x3, x12 \n\t"
+          "mul x17,  x3, x13 \n\t"
+          "mul x18,  x3, x14 \n\t"
+          "mul x19,  x3, x15 \n\t"
+          "mul x20,  x4, x12 \n\t"
+          "mul x21,  x4, x13 \n\t"
+          "mul x22,  x4, x14 \n\t"
+          "mul x23,  x4, x15 \n\t"
+          "mul x24, x10, x12 \n\t"
+          "mul x25, x10, x13 \n\t"
+          "mul x26, x10, x14 \n\t"
+          "mul x27, x10, x15 \n\t"
+          "mul x28, x11, x12 \n\t"
+          "mul x29, x11, x13 \n\t"
+          "mul x30, x11, x14 \n\t"
+          "mul x31, x11, x15 \n\t"
+          // Inner loop: Do this loop N times
+          "1: \n\t"
+          "p.lw  x3, %[N](%[addr_a]!) \n\t"
+          "p.lw  x4, %[N](%[addr_a]!) \n\t"
+          "p.lw x10, %[N](%[addr_a]!) \n\t"
+          "p.lw x11, %[N3_1](%[addr_a]!) \n\t" // Increment by -3N+1
+          "p.lw x12, 4(%[addr_b]!) \n\t"
+          "p.lw x13, 4(%[addr_b]!) \n\t"
+          "p.lw x14, 4(%[addr_b]!) \n\t"
+          "p.lw x15, %[P_3](%[addr_b]!) \n\t" // Increment by P-3
+          "p.mac x16,  x3, x12 \n\t"
+          "p.mac x17,  x3, x13 \n\t"
+          "p.mac x18,  x3, x14 \n\t"
+          "p.mac x19,  x3, x15 \n\t"
+          "p.mac x20,  x4, x12 \n\t"
+          "p.mac x21,  x4, x13 \n\t"
+          "p.mac x22,  x4, x14 \n\t"
+          "p.mac x23,  x4, x15 \n\t"
+          "p.mac x24, x10, x12 \n\t"
+          "p.mac x25, x10, x13 \n\t"
+          "p.mac x26, x10, x14 \n\t"
+          "p.mac x27, x10, x15 \n\t"
+          "p.mac x28, x11, x12 \n\t"
+          "p.mac x29, x11, x13 \n\t"
+          "p.mac x30, x11, x14 \n\t"
+          "p.mac x31, x11, x15 \n\t"
+          "bne %[addr_b], x1, 1b \n\t"
+          "p.sw x16, 4(%[addr_c]!) \n\t"
+          "p.sw x17, 4(%[addr_c]!) \n\t"
+          "p.sw x18, 4(%[addr_c]!) \n\t"
+          "p.sw x19, %[P_3](%[addr_c]!) \n\t"
+          "p.sw x20, 4(%[addr_c]!) \n\t"
+          "p.sw x21, 4(%[addr_c]!) \n\t"
+          "p.sw x22, 4(%[addr_c]!) \n\t"
+          "p.sw x23, %[P_3](%[addr_c]!) \n\t"
+          "p.sw x24, 4(%[addr_c]!) \n\t"
+          "p.sw x25, 4(%[addr_c]!) \n\t"
+          "p.sw x26, 4(%[addr_c]!) \n\t"
+          "p.sw x27, %[P_3](%[addr_c]!) \n\t"
+          "p.sw x28, 4(%[addr_c]!) \n\t"
+          "p.sw x29, 4(%[addr_c]!) \n\t"
+          "p.sw x30, 4(%[addr_c]!) \n\t"
+          "p.sw x31, %[P_3](%[addr_c]!) \n\t"
+          : // Outputs
+          : [addr_a] "r"(addr_a), [addr_b] "r"(addr_b), [addr_c] "r"(addr_c),
+            [N3_1] "r"(N3_1), [P_3] "r"(P_3), [x1] "r"(k),
+            [N] "I"(NN * 4) // Inputs
+          : "x3", "x4", "x10", "x11", "x12", "x13", "x14", "x15", "x16", "x17",
+            "x18", "x19", "x20", "x21", "x22", "x23", "x24", "x25", "x26",
+            "x27", "x28", "x29", "x30", "x31", "memory"); // Clobber
+    }
+  }
+}
+
 void mat_mul_unrolled2_shifted_parallel(int32_t const *__restrict__ A,
                                         int32_t const *__restrict__ B,
                                         int32_t *__restrict__ C, uint32_t M,
