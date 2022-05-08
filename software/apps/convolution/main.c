681fc07d3257898d45f18af3c06990c0b10be125
Samuel Riedel <sriedel@iis.ee.ethz.ch>
2022-05-08 22:15:06 +0200

    [software] Schedule fancy 2D convolution

--- software/apps/convolution/main.c
+++ software/apps/convolution/main.c
@@ -16,38 +16,43 @@
 // #include "convolution_riscv.h"
 // #include "halide_runtime.h"
 
-#define M (20)
+#define M (96)
 #define N (4 * NUM_CORES)
 #define KERNEL_N 3
 // #define VERBOSE
 
+dump(time, 0);
+
 volatile int32_t in[M * N] __attribute__((section(".l1_prio")));
 volatile int32_t out[M * N] __attribute__((section(".l1_prio")));
-volatile int32_t kernel[KERNEL_N * KERNEL_N] __attribute__((section(".l1")));
-volatile int error __attribute__((section(".l1")));
+// volatile int32_t kernel[KERNEL_N * KERNEL_N] __attribute__((section(".l1")));
+volatile int error __attribute__((section(".l2")));
 
 int main() {
   uint32_t core_id = mempool_get_core_id();
   uint32_t num_cores = mempool_get_core_count();
   mempool_barrier_init(core_id);
 
+  int32_t kernel[KERNEL_N * KERNEL_N];
+
   if (core_id == 0) {
 #ifdef VERBOSE
     printf("Initialize\n");
 #endif
     error = 0;
-    kernel[0] = 1;
-    kernel[1] = 2;
-    kernel[2] = 1;
-
-    kernel[3] = 2;
-    kernel[4] = 4;
-    kernel[5] = 2;
-
-    kernel[6] = 1;
-    kernel[7] = 2;
-    kernel[8] = 1;
   }
+
+  kernel[0] = 1;
+  kernel[1] = 2;
+  kernel[2] = 1;
+
+  kernel[3] = 2;
+  kernel[4] = 4;
+  kernel[5] = 2;
+
+  kernel[6] = 1;
+  kernel[7] = 2;
+  kernel[8] = 1;
 
   // Initialize img
   init_conv2d_image(in, N, M, core_id, num_cores);
@@ -81,20 +86,48 @@ int main() {
   // #endif
 
   // Matrices are initialized --> Start calculating
-  for (int i = 2; i < 3; ++i) {
-    // Wait at barrier until everyone is ready
-    mempool_barrier(num_cores);
-    mempool_start_benchmark();
-    conv2d_3x3_crazy_parallel((const int32_t *)in, N, M,
-                              (const int32_t *)kernel, (int32_t *)out, core_id,
-                              num_cores);
-    mempool_stop_benchmark();
-    // Wait at barrier befor checking
-    mempool_barrier(num_cores);
-    // Check result
-    if (verify_conv2d_image(out, N, M, core_id, num_cores)) {
-      __atomic_fetch_or(&error, i, __ATOMIC_SEQ_CST);
-    }
+  // Wait at barrier until everyone is ready
+  mempool_barrier(num_cores);
+  uint32_t start = mempool_get_timer();
+  mempool_start_benchmark();
+  conv2d_3x3_crazy_parallel((const int32_t *)in, N, M, (const int32_t *)kernel,
+                            (int32_t *)out, core_id, num_cores);
+  mempool_stop_benchmark();
+  if (core_id == 44) {
+    dump_time(mempool_get_timer() - start);
+  }
+  mempool_start_benchmark();
+
+  // Wait at barrier befor checking
+  mempool_barrier(num_cores);
+  mempool_stop_benchmark();
+  uint32_t stop = mempool_get_timer();
+  if (core_id == 44) {
+    dump_time(stop - start);
+  }
+
+  // Hot cache
+  start = mempool_get_timer();
+  mempool_start_benchmark();
+  conv2d_3x3_crazy_parallel((const int32_t *)in, N, M, (const int32_t *)kernel,
+                            (int32_t *)out, core_id, num_cores);
+  mempool_stop_benchmark();
+  if (core_id == 44) {
+    dump_time(mempool_get_timer() - start);
+  }
+  mempool_start_benchmark();
+
+  // Wait at barrier befor checking
+  mempool_barrier(num_cores);
+  mempool_stop_benchmark();
+  stop = mempool_get_timer();
+  if (core_id == 44) {
+    dump_time(stop - start);
+  }
+
+  // Check result
+  if (verify_conv2d_image(out, N, M, core_id, num_cores)) {
+    error = 1;
   }
 
   // wait until all cores have finished
