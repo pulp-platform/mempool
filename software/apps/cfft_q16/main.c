dfe1abdc7f46eae87d57f2e692f94417a5709985
Marco Bertuletti <mbertuletti@iis.ee.ethz.ch>
2022-06-09 11:39:28 +0200

    [apps] Add cfft with memory accesses shaped to mempool

--- software/apps/cfft_q16/main.c
+++ software/apps/cfft_q16/main.c
@@ -1,3 +1,9 @@
+// Copyright 2022 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// Author: Marco Bertuletti, ETH Zurich
+
 #include <stdint.h>
 #include <string.h>
 #include <stdio.h>
@@ -11,17 +17,32 @@
 #include "synchronization.h"
 
 /* CFFT mempool libraries */
+#include "define.h"
+#include "xpulp/builtins_v2.h"
 #include "mempool_cfft_q16s.h"
 #include "mempool_cfft_q16p.h"
-#include "define.h"
+#include "mempool_cfft_memsized_q16p.h"
 #include "mempool_cfft_q16_twiddleCoef.h"
 #include "mempool_cfft_q16_BitRevIndexTable.h"
 
+void initialize_vector (int16_t *pSrc, int16_t *pDst, uint32_t N_el) {
+    int lower = SHRT_MIN, upper = SHRT_MAX;
+    srand((unsigned) 1);
+    for (uint32_t i = 0; i < 8 * N_BANKS; i++) {
+      if(i < N_el) {
+        pSrc[i] = (int16_t)((rand() % (upper - lower + 1)) + lower);
+      } else {
+        pSrc[i] = (int16_t) 0;
+      }
+      pDst[i] = (int16_t) 0;
+    }
+    for (uint32_t i = 0; i < N_TWIDDLES; i++) {
+      pCoef16_copy[i] = (int16_t) 0;
+    }
 
-int16_t pSrc[N_RSAMPLES] __attribute__((aligned(N_CSAMPLES), section(".l1")));
+}
+
 int volatile error __attribute__((section(".l1")));
-void initialize_vector (int16_t *pSrc, uint32_t N_el);
-
 int main() {
 
     uint32_t core_id = mempool_get_core_id();
@@ -35,7 +56,7 @@ int main() {
 
       printf("On the run...\n");
       error = 0;
-      initialize_vector(pSrc, N_RSAMPLES);
+      initialize_vector(pSrc, pDst, N_RSAMPLES);
 
   #ifdef  TEST_16
       mempool_start_benchmark();
@@ -122,9 +143,11 @@ int main() {
   #endif
 
       printf("Done SINGLE!\n");
+  #ifdef PRINT_SINGLE
       for(uint32_t i=0; i<N_RSAMPLES; i+=2) {
         printf("{%6d;%6d } \n", pSrc[i], pSrc[i+1]);
       }
+  #endif
 
     }
     mempool_barrier(NUM_CORES);
@@ -139,11 +162,12 @@ int main() {
     if (core_id == 0)  {
       printf("On the run...\n");
       error = 0;
-      initialize_vector(pSrc, N_RSAMPLES);
+      initialize_vector(pSrc, pDst, N_RSAMPLES);
     }
     mempool_barrier(NUM_CORES);
 
   #ifdef  TEST_16
+
     mempool_start_benchmark();
     mempool_cfft_q16p(  (uint16_t) 16,
                         twiddleCoef_16_q16,
@@ -154,6 +178,7 @@ int main() {
     mempool_stop_benchmark();
 
   #elif   defined(TEST_32)
+
     mempool_start_benchmark();
     mempool_cfft_q16p(  (uint16_t) 32,
                         twiddleCoef_32_q16,
@@ -162,7 +187,20 @@ int main() {
                         BITREVINDEXTABLE_FIXED_32_TABLE_LENGTH,
                         0, 0, NUM_CORES);
     mempool_stop_benchmark();
+
   #elif   defined(TEST_64)
+
+    #ifdef MEMSIZED
+    mempool_start_benchmark();
+    mempool_cfft_memsized_q16p( (uint16_t) 64,
+                        twiddleCoef_64_q16,
+                        BitRevIndexTable_fixed_64,
+                        pSrc,
+                        pDst,
+                        BITREVINDEXTABLE_FIXED_64_TABLE_LENGTH,
+                        0, 0, 4);
+    mempool_stop_benchmark();
+    #else
     mempool_start_benchmark();
     mempool_cfft_q16p(  (uint16_t) 64,
                         twiddleCoef_64_q16,
@@ -171,7 +209,10 @@ int main() {
                         BITREVINDEXTABLE_FIXED_64_TABLE_LENGTH,
                         0, 0, NUM_CORES);
     mempool_stop_benchmark();
+    #endif
+
   #elif   defined(TEST_128)
+
     mempool_start_benchmark();
     mempool_cfft_q16p(  (uint16_t) 128,
                         twiddleCoef_128_q16,
@@ -180,7 +221,20 @@ int main() {
                         BITREVINDEXTABLE_FIXED_128_TABLE_LENGTH,
                         0, 0, NUM_CORES);
     mempool_stop_benchmark();
+
   #elif   defined(TEST_256)
+
+    #ifdef MEMSIZED
+    mempool_start_benchmark();
+    mempool_cfft_memsized_q16p( (uint16_t) 256,
+                        twiddleCoef_256_q16,
+                        BitRevIndexTable_fixed_256,
+                        pSrc,
+                        pDst,
+                        BITREVINDEXTABLE_FIXED_256_TABLE_LENGTH,
+                        0, 0, 16);
+    mempool_stop_benchmark();
+    #else
     mempool_start_benchmark();
     mempool_cfft_q16p(  (uint16_t) 256,
                         twiddleCoef_256_q16,
@@ -189,7 +243,10 @@ int main() {
                         BITREVINDEXTABLE_FIXED_256_TABLE_LENGTH,
                         0, 0, NUM_CORES);
     mempool_stop_benchmark();
+    #endif
+
   #elif   defined(TEST_512)
+
     mempool_start_benchmark();
     mempool_cfft_q16p(  (uint16_t) 512,
                         twiddleCoef_512_q16,
@@ -198,7 +255,20 @@ int main() {
                         BITREVINDEXTABLE_FIXED_512_TABLE_LENGTH,
                         0, 0, NUM_CORES);
     mempool_stop_benchmark();
+
   #elif   defined(TEST_1024)
+
+    #ifdef MEMSIZED
+    mempool_start_benchmark();
+    mempool_cfft_memsized_q16p( (uint16_t) 1024,
+                        twiddleCoef_1024_q16,
+                        BitRevIndexTable_fixed_1024,
+                        pSrc,
+                        pDst,
+                        BITREVINDEXTABLE_FIXED_1024_TABLE_LENGTH,
+                        0, 0, 64);
+    mempool_stop_benchmark();
+    #else
     mempool_start_benchmark();
     mempool_cfft_q16p(  (uint16_t) 1024,
                         twiddleCoef_1024_q16,
@@ -207,7 +277,10 @@ int main() {
                         BITREVINDEXTABLE_FIXED_1024_TABLE_LENGTH,
                         0, 0, NUM_CORES);
     mempool_stop_benchmark();
+    #endif
+
   #elif   defined(TEST_2048)
+
     mempool_start_benchmark();
     mempool_cfft_q16p(  (uint16_t) 2048,
                         twiddleCoef_2048_q16,
@@ -216,22 +289,39 @@ int main() {
                         BITREVINDEXTABLE_FIXED_2048_TABLE_LENGTH,
                         0, 0, NUM_CORES);
     mempool_stop_benchmark();
+
   #elif   defined(TEST_4096)
+
+    #ifdef MEMSIZED
     mempool_start_benchmark();
-    mempool_cfft_q16p(  (uint16_t) 4096,
+    mempool_cfft_memsized_q16p( (uint16_t) 4096,
+                        twiddleCoef_4096_q16,
+                        BitRevIndexTable_fixed_4096,
+                        pSrc,
+                        pDst,
+                        BITREVINDEXTABLE_FIXED_4096_TABLE_LENGTH,
+                        0, 0, NUM_CORES);
+    mempool_stop_benchmark();
+    #else
+    mempool_start_benchmark();
+    mempool_cfft_q16p( (uint16_t) 4096,
                         twiddleCoef_4096_q16,
                         BitRevIndexTable_fixed_4096,
                         pSrc,
                         BITREVINDEXTABLE_FIXED_4096_TABLE_LENGTH,
                         0, 0, NUM_CORES);
     mempool_stop_benchmark();
+    #endif
+
   #endif
 
     if(core_id==0) {
       printf("Done PARALLEL!\n");
-      // for(uint32_t i=0; i<N_RSAMPLES; i+=2) {
-        // printf("{%6d;%6d } \n", pSrc[i], pSrc[i+1]);
-      // }
+  #ifdef PRINT_PARALLEL
+      for(uint32_t i=0; i<N_RSAMPLES; i+=2) {
+        printf("{%6d;%6d } \n", pSrc[i], pSrc[i+1]);
+      }
+  #endif
     }
     mempool_barrier(NUM_CORES);
 
@@ -240,12 +330,4 @@ int main() {
 
   return error;
 
-}
-
-void initialize_vector (int16_t *pSrc, uint32_t N_el) {
-    int lower = SHRT_MIN, upper = SHRT_MAX;
-    srand((unsigned) 1);
-    for (uint32_t i = 0; i < N_el; i++) {
-      pSrc[i] = (int16_t)((rand() % (upper - lower + 1)) + lower);
-    }
 }
---
+++ software/apps/cfft_q16/mempool_cfft_memsized_q16p.h
@@ -0,0 +1,378 @@
+// Copyright 2022 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// Author: Marco Bertuletti, ETH Zurich
+
+static void mempool_cfft_memsized_q16p(   uint16_t fftLen,
+                                          int16_t *pTwiddle,
+                                          uint16_t *pBitRevTable,
+                                          int16_t *pSrc,
+                                          int16_t *pDst,
+                                          uint16_t bitReverseLen,
+                                          uint8_t ifftFlag,
+                                          uint8_t bitReverseFlag,
+                                          uint32_t nPE);
+
+static void mempool_memsized_butterfly( int16_t *pSrc16,
+                                        int16_t *pDst16,
+                                        uint32_t fftLen,
+                                        int16_t *pCoef16,
+                                        int16_t *pCoef16_copy,
+                                        uint32_t nPE);
+
+static void fold_radix4(  int16_t *pSrc16,
+                          uint32_t fftLen,
+                          uint32_t nPE);
+
+void mempool_cfft_memsized_q16p( uint16_t fftLen,
+                                 int16_t *pTwiddle,
+                                 uint16_t *pBitRevTable,
+                                 int16_t *pSrc,
+                                 int16_t *pDst,
+                                 uint16_t bitReverseLen,
+                                 uint8_t ifftFlag,
+                                 uint8_t bitReverseFlag,
+                                 uint32_t nPE) {
+
+    if (ifftFlag == 0) {
+        switch (fftLen) {
+        case 16:
+        case 64:
+        case 256:
+        case 1024:
+        case 4096:
+            mempool_memsized_butterfly(pSrc, pDst, fftLen, pTwiddle, pCoef16_copy, nPE);
+            break;
+        case 32:
+        case 128:
+        case 512:
+        case 2048:
+            // TODO
+            break;
+        }
+    }
+
+    if (bitReverseFlag) {
+      mempool_bitreversal_q16p((uint16_t *)pSrc, bitReverseLen, pBitRevTable, nPE);
+    }
+
+}
+
+void mempool_memsized_butterfly(  int16_t *pSrc16,
+                                  int16_t *pDst16,
+                                  uint32_t fftLen,
+                                  int16_t *pCoef16,
+                                  int16_t *pCoef16_copy,
+                                  uint32_t nPE) {
+
+    uint32_t core_id = mempool_get_core_id();
+    v2s R, S, T, U, V, X, Y;
+    v2s CoSi1, CoSi2, CoSi3;
+    uint32_t n1, n2, ic, i0, i1, i2, i3, j, k, s;
+    uint32_t n2_store, i0_store, i1_store, i2_store, i3_store;
+    uint32_t offset, wing_id, bank_id;
+    register int16_t *pTmp;
+
+    #ifdef TWIDDLE_MODIFIER
+    uint32_t twidCoefModifier = 1U;
+    #endif
+
+    if(fftLen <= N_BANKS)
+      fold_radix4(pSrc16, fftLen, nPE);
+
+    n1 = fftLen;
+    n2 = n1 >> 2U;
+    n2_store = n2 >> 2U;
+    for(i0 = core_id * 4; i0 < MIN(core_id * 4 + 4, n2); i0++) {
+
+        #ifdef TWIDDLE_MODIFIER
+        ic = i0;
+        CoSi1 = *(v2s *)&pCoef16[ic * 2U];
+        CoSi2 = *(v2s *)&pCoef16[2U * (ic * 2U)];
+        CoSi3 = *(v2s *)&pCoef16[3U * (ic * 2U)];
+        #else
+        ic = i0;
+        CoSi1 = *(v2s *)&pCoef16[ic * 2U];
+        CoSi2 = *(v2s *)&pCoef16[2U * (ic * 2U)];
+        CoSi3 = *(v2s *)&pCoef16[3U * (ic * 2U)];
+        if(ic % 4 == 0) {
+          for(s = 0; s < 4; s++) {
+            *((v2s *)&pCoef16_copy[2U * (n2 * s + ic / 4)]) = CoSi1;
+            *((v2s *)&pCoef16_copy[2U * (n2 * s + 2U * (ic/4))]) = CoSi2;
+            *((v2s *)&pCoef16_copy[2U * (n2 * s + 3U * (ic/4))]) = CoSi3;
+          }
+        }
+        #endif
+//        dump_ic(ic);
+//        dump_ic_2(ic * 2U);
+//        dump_ic_3(ic * 3U);
+
+        i1 = i0 + N_BANKS;
+        i2 = i1 + N_BANKS;
+        i3 = i2 + N_BANKS;
+        i0_store = (i0 % n2_store) + (i0 / n2) * n2 + ( (i0 % n2) / n2_store) * N_BANKS;
+        i1_store = i0_store + n2_store;
+        i2_store = i1_store + n2_store;
+        i3_store = i2_store + n2_store;
+
+        /* Read ya (real), xa(imag) input */
+        X = __SRA2(*(v2s *)&pSrc16[i0 * 2U], ((v2s){ 2, 2 }));
+        /* Read yc (real), xc(imag) input */
+        Y = __SRA2(*(v2s *)&pSrc16[i2 * 2U], ((v2s){ 2, 2 }));
+        /* Read yb (real), xb(imag) input */
+        T = __SRA2(*(v2s *)&pSrc16[i1 * 2U], ((v2s){ 2, 2 }));
+        /* Read yd (real), xd(imag) input */
+        U = __SRA2(*(v2s *)&pSrc16[i3 * 2U], ((v2s){ 2, 2 }));
+        /* R0 = (ya + yc), R1 = (xa + xc) */
+        R = __ADD2(X, Y);
+        /* S0 = (ya - yc), S1 =(xa - xc) */
+        S = __SUB2(X, Y);
+        /* T0 = (yb + yd), T1 = (xb + xd) */
+        V = __ADD2(T, U);
+
+        /*  writing the butterfly processed i0 sample */
+        /* xa' = xa + xb + xc + xd, ya' = ya + yb + yc + yd */
+        *((v2s *)&pDst16[i0_store * 2U]) = __ADD2(__SRA2(R, ((v2s){ 1, 1 })), __SRA2(V, ((v2s){ 1, 1 })));
+
+        /* R0 = (ya + yc) - (yb + yd), R1 = (xa + xc) - (xb + xd) */
+        R = __SUB2(R, V);
+
+        /*  writing the butterfly processed i0 + fftLen/4 sample */
+        /* xc' = (xa-xb+xc-xd)* co2 + (ya-yb+yc-yd)* (si2), yc' = (ya-yb+yc-yd)* co2 - (xa-xb+xc-xd)* (si2) */
+        *((v2s *)&pDst16[i1_store * 2U]) =
+            __PACK2((int16_t)(__DOTP2(CoSi2, R) >> 16U),
+                    (int16_t)(__DOTP2(__PACK2(-CoSi2[1], CoSi2[0]), R) >> 16U));
+
+        /* T0 = yb-yd, T1 = xb-xd */
+        T = __SUB2(T, U);
+        /* R0 = (ya-yc) + (xb- xd), R1 = (xa-xc) - (yb-yd)) */
+        R = __ADD2(S, __PACK2(-T[1], T[0]));
+        /* S0 = (ya-yc) - (xb- xd), S1 = (xa-xc) + (yb-yd)) */
+        S = __ADD2(S, __PACK2(T[1], -T[0]));
+
+        /*  Butterfly process for the i0+fftLen/2 sample */
+        /* xb' = (xa+yb-xc-yd)* co1 + (ya-xb-yc+xd)* (si1), yb' = (ya-xb-yc+xd)* co1 - (xa+yb-xc-yd)* (si1) */
+        *((v2s *)&pDst16[i2_store * 2U]) =
+            __PACK2((int16_t)(__DOTP2(CoSi1, S) >> 16U),
+                    (int16_t)(__DOTP2(__PACK2(-CoSi1[1], CoSi1[0]), S) >> 16U));
+        /*  Butterfly process for the i0+3fftLen/4 sample */
+        /* xd' = (xa-yb-xc+yd)* Co3 + (ya+xb-yc-xd)* (si3), yd' = (ya+xb-yc-xd)* Co3 - (xa-yb-xc+yd)* (si3) */
+        *((v2s *)&pDst16[i3_store * 2U]) =
+            __PACK2((int16_t)(__DOTP2(CoSi3, R) >> 16U),
+                    (int16_t)(__DOTP2(__PACK2(-CoSi3[1], CoSi3[0]), R) >> 16U));
+
+    }
+
+    pTmp = pSrc16;
+    pSrc16 = pDst16;
+    pDst16 = pTmp;
+
+    pTmp = pCoef16;
+    pCoef16 = pCoef16_copy;
+    pCoef16_copy = pTmp;
+
+    #ifdef TWIDDLE_MODIFIER
+    twidCoefModifier <<= 2U;
+    #endif
+
+    mempool_log_partial_barrier(2, core_id, nPE);
+    // mempool_log_barrier(2, core_id);
+    /* END OF FIRST STAGE PROCESSING */
+
+    for (k = fftLen / 4U; k > 4U; k >>= 2U) {
+
+        n1 = n2;
+        n2 >>= 2U;
+        n2_store = n2 >> 2U;
+        if(core_id < (fftLen >> 4U)) {
+
+            bank_id = core_id / n2_store;
+            wing_id = core_id % n2_store;
+            offset = bank_id * n2;
+            for(j = wing_id * 4; j < MIN(wing_id * 4 + 4, n2); j++) {
+
+                #ifdef TWIDDLE_MODIFIER
+                ic = j*twidCoefModifier;
+                CoSi1 = *(v2s *)&pCoef16[ic * 2U];
+                CoSi2 = *(v2s *)&pCoef16[2U * (ic * 2U)];
+                CoSi3 = *(v2s *)&pCoef16[3U * (ic * 2U)];
+                #else
+                ic = j;
+                CoSi1 = *(v2s *)&pCoef16[2U * (ic + bank_id * n2)];
+                CoSi2 = *(v2s *)&pCoef16[2U * (ic * 2U + bank_id * n2)];
+                CoSi3 = *(v2s *)&pCoef16[2U * (ic * 3U + bank_id * n2)];
+                if(ic % 4 == 0) {
+                  for(s = 0; s < 4; s++) {
+                    *((v2s *)&pCoef16_copy[2U * (bank_id * n1 + n2 * s + ic / 4)]) = CoSi1;
+                    *((v2s *)&pCoef16_copy[2U * (bank_id * n1 + n2 * s + 2U * (ic / 4))]) = CoSi2;
+                    *((v2s *)&pCoef16_copy[2U * (bank_id * n1 + n2 * s + 3U * (ic / 4))]) = CoSi3;
+                  }
+                }
+                #endif
+//                dump_ic(ic);
+//                dump_ic_2(ic * 2U);
+//                dump_ic_3(ic * 3U);
+
+                i0 = offset + j;
+                // for (i0 = offset + j; i0 < fftLen; i0 += fftLen) {
+                i1 = i0 + N_BANKS;
+                i2 = i1 + N_BANKS;
+                i3 = i2 + N_BANKS;
+
+                i0_store = (i0 % n2_store) + (i0 / n2) * n2 + ( (i0 % n2) / n2_store) * N_BANKS;
+                i1_store = i0_store + n2_store;
+                i2_store = i1_store + n2_store;
+                i3_store = i2_store + n2_store;
+                /* Read ya (real), xa(imag) input */
+                X = *(v2s *)&pSrc16[i0 * 2U];
+                /* Read yc (real), xc(imag) input */
+                Y = *(v2s *)&pSrc16[i2 * 2U];
+                /* Read yb (real), xb(imag) input */
+                T = *(v2s *)&pSrc16[i1 * 2U];
+                /* Read yd (real), xd(imag) input */
+                U = *(v2s *)&pSrc16[i3 * 2U];
+                /* R0 = (ya + yc), R1 = (xa + xc) */
+                R = __ADD2(X, Y);
+                /* S0 = (ya - yc), S1 =(xa - xc) */
+                S = __SUB2(X, Y);
+                /* T0 = (yb + yd), T1 = (xb + xd) */
+                V = __ADD2(T, U);
+                /*  writing the butterfly processed i0 sample */
+                /* xa' = xa + xb + xc + xd */
+                /* ya' = ya + yb + yc + yd */
+                *((v2s *)&pDst16[i0_store * 2U]) =
+                    __SRA2(__ADD2(__SRA2(R, ((v2s){ 1, 1 })), __SRA2(V, ((v2s){ 1, 1 }))),
+                           ((v2s){ 1, 1 }));
+                /* R0 = (ya + yc) - (yb + yd), R1 = (xa + xc) - (xb + xd) */
+                R = __SUB2(__SRA2(R, ((v2s){ 1, 1 })), __SRA2(V, ((v2s){ 1, 1 })));
+                /* Writing the butterfly processed i0 + fftLen/4 sample */
+                /* xc' = (xa-xb+xc-xd)* co2 + (ya-yb+yc-yd)* (si2) */
+                /* yc' = (ya-yb+yc-yd)* co2 - (xa-xb+xc-xd)* (si2) */
+                *((v2s *)&pDst16[i1_store * 2U]) =
+                    __PACK2((int16_t)(__DOTP2(CoSi2, R) >> 16U),
+                            (int16_t)(__DOTP2(__PACK2(-CoSi2[1], CoSi2[0]), R) >> 16U));
+                /* T0 = yb-yd, T1 = xb-xd */
+                T = __SRA2(__SUB2(T, U), ((v2s){ 1, 1 }));
+                /* R0 = (ya-yc) + (xb- xd), R1 = (xa-xc) - (yb-yd)) */
+                R = __ADD2(__SRA2(S, ((v2s){ 1, 1 })), __PACK2(-T[1], T[0]));
+                /* S0 = (ya-yc) - (xb- xd), S1 = (xa-xc) + (yb-yd)) */
+                S = __ADD2(__SRA2(S, ((v2s){ 1, 1 })), __PACK2(T[1], -T[0]));
+                /*  Butterfly process for the i0+fftLen/2 sample */
+                /* xb' = (xa+yb-xc-yd)* co1 + (ya-xb-yc+xd)* (si1) */
+                /* yb' = (ya-xb-yc+xd)* co1 - (xa+yb-xc-yd)* (si1) */
+                *((v2s *)&pDst16[i2_store * 2U]) =
+                    __PACK2((int16_t)(__DOTP2(CoSi1, S) >> 16U),
+                            (int16_t)(__DOTP2(__PACK2(-CoSi1[1], CoSi1[0]), S) >> 16U));
+                /* Butterfly process for the i0+3fftLen/4 sample */
+                /* xd' = (xa-yb-xc+yd)* Co3 + (ya+xb-yc-xd)* (si3) */
+                /* yd' = (ya+xb-yc-xd)* Co3 - (xa-yb-xc+yd)* (si3) */
+                *((v2s *)&pDst16[i3_store * 2U]) =
+                    __PACK2((int16_t)(__DOTP2(CoSi3, R) >> 16U),
+                            (int16_t)(__DOTP2(__PACK2(-CoSi3[1], CoSi3[0]), R) >> 16U));
+          }
+      }
+
+      pTmp = pSrc16;
+      pSrc16 = pDst16;
+      pDst16 = pTmp;
+
+      pTmp = pCoef16;
+      pCoef16 = pCoef16_copy;
+      pCoef16_copy = pTmp;
+
+      #ifdef TWIDDLE_MODIFIER
+      twidCoefModifier <<= 2U;
+      #endif
+
+      // mempool_log_barrier(2, core_id);
+      mempool_log_partial_barrier(2, core_id, n2);
+    }
+    /* END OF MIDDLE STAGE PROCESSING */
+
+    /*  Initializations for the last stage */
+    n1 = n2;
+    n2 >>= 2U;
+    for (i0 = core_id * 4; i0 < MIN(core_id * 4 + 4, fftLen >> 2U); i0++) {
+
+        i1 = i0 + N_BANKS;
+        i2 = i1 + N_BANKS;
+        i3 = i2 + N_BANKS;
+        i0_store = i0 * 4;
+        i1_store = i0_store + 1;
+        i2_store = i1_store + 1;
+        i3_store = i2_store + 1;
+
+        /* Read ya (real), xa(imag) input */
+        X = *(v2s *)&pSrc16[i0 * 2U];
+        /* Read yc (real), xc(imag) input */
+        Y = *(v2s *)&pSrc16[i2 * 2U];
+        /* Read yb (real), xb(imag) input */
+        T = *(v2s *)&pSrc16[i1 * 2U];
+        /* Read yd (real), xd(imag) input */
+        U = *(v2s *)&pSrc16[i3 * 2U];
+
+        /* R0 = (ya + yc), R1 = (xa + xc) */
+        R = __ADD2(X, Y);
+        /* S0 = (ya - yc), S1 = (xa - xc) */
+        S = __SUB2(X, Y);
+        /* T0 = (yb + yd), T1 = (xb + xd)) */
+        V = __ADD2(T, U);
+
+        /* Writing the butterfly processed i0 sample */
+        /* xa' = xa + xb + xc + xd, ya' = ya + yb + yc + yd */
+        *((v2s *)&pDst16[i0_store * 2U]) = __ADD2(__SRA2(R, ((v2s){ 1, 1 })), __SRA2(V, ((v2s){ 1, 1 })));
+
+        /* R0 = (ya + yc) - (yb + yd), R1 = (xa + xc) - (xb + xd) */
+        R = __SUB2(__SRA2(R, ((v2s){ 1, 1 })), __SRA2(T, ((v2s){ 1, 1 })));
+
+        /* Writing the butterfly processed i0 + fftLen/4 sample */
+        /* xc' = (xa-xb+xc-xd), yc' = (ya-yb+yc-yd) */
+        *((v2s *)&pDst16[i1_store * 2U]) = R;
+
+        /* T0 = (yb - yd), T1 = (xb - xd)  */
+        T = __SUB2(T, U);
+        T = __SRA2(T, ((v2s){ 1, 1 }));
+        S = __SRA2(S, ((v2s){ 1, 1 }));
+
+        /* Writing the butterfly processed i0 + fftLen/2 sample */
+        /* xb' = (xa+yb-xc-yd), yb' = (ya-xb-yc+xd) */
+        *((v2s *)&pDst16[i2_store * 2U]) = __ADD2(S, __PACK2(T[1], -T[0]));
+
+        /* Writing the butterfly processed i0 + 3fftLen/4 sample */
+        /* xd' = (xa-yb-xc+yd), yd' = (ya+xb-yc-xd) */
+        *((v2s *)&pDst16[i3_store * 2U]) = __ADD2(S, __PACK2(-T[1], T[0]));
+
+    }
+
+    pTmp = pSrc16;
+    pSrc16 = pDst16;
+    pDst16 = pTmp;
+
+    mempool_log_partial_barrier(2, core_id, nPE);
+    /* END OF LAST STAGE PROCESSING */
+
+}
+
+void fold_radix4 (  int16_t *pSrc16,
+                    uint32_t fftLen,
+                    uint32_t nPE) {
+
+    uint32_t n2, i0, i1, i2, i3;
+    uint32_t i1_store, i2_store, i3_store;
+    uint32_t core_id = mempool_get_core_id();
+    n2 = fftLen >> 2U;
+    for(i0 = core_id * 4; i0 < MIN(core_id * 4 + 4, n2); i0++) {
+        i1 = i0 + n2;
+        i2 = i1 + n2;
+        i3 = i2 + n2;
+        i1_store = i0 + N_BANKS;
+        i2_store = i1_store + N_BANKS;
+        i3_store = i2_store + N_BANKS;
+        pSrc16[i1_store * 2U] = pSrc16[i1 * 2U];
+        pSrc16[i2_store * 2U] = pSrc16[i2 * 2U];
+        pSrc16[i3_store * 2U] = pSrc16[i3 * 2U];
+    }
+    // mempool_log_barrier(2, core_id);
+    mempool_log_partial_barrier(2, core_id, nPE);
+}
