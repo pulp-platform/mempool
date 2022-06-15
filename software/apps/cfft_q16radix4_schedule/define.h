0ce676a2862c3a3ca88f8f56c0338301fd42001e
Marco Bertuletti <mbertuletti@iis.ee.ethz.ch>
2022-06-15 10:39:19 +0200

    [apps] Add scheduling of multiple parallel FFTs over different cores

---
+++ software/apps/cfft_q16radix4_schedule/define.h
@@ -0,0 +1,35 @@
+// Copyright 2022 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// Author: Marco Bertuletti, ETH Zurich
+
+/* DEFINES */
+#define N_CSAMPLES 256
+#define N_RSAMPLES (2*N_CSAMPLES)
+#define N_BANKS (1024)
+
+//#define SINGLE
+#define PARALLEL
+#define TEST_256
+
+#if defined(SINGLE)
+  #define N_FFTs 4
+  #define N_BANKS_SINGLE (N_BANKS * ((N_CSAMPLES + N_BANKS - 1) / N_BANKS))
+  int16_t pSrc16[N_FFTs * 2 * N_BANKS_SINGLE] __attribute__((aligned(N_FFTs * 2 * N_BANKS_SINGLE), section(".l1")));
+#endif
+
+#if defined(PARALLEL)
+  #define N_FFTs_ROW 1
+  #define N_FFTs_COL 1
+  #define MAX_COL (1024 / (N_CSAMPLES / 4))
+  int16_t pSrc16[N_FFTs_ROW * 8 * N_BANKS] __attribute__((aligned(N_FFTs_ROW * 8 * N_BANKS), section(".l1")));
+  int16_t pDst16[N_FFTs_ROW * 8 * N_BANKS] __attribute__((aligned(N_FFTs_ROW * 8 * N_BANKS), section(".l1")));
+  int16_t pCoef16_src[8*N_BANKS] __attribute__((aligned(8*N_BANKS), section(".l1")));
+  int16_t pCoef16_dst[8*N_BANKS] __attribute__((aligned(8*N_BANKS), section(".l1")));
+#endif
+
+/* FUNCTIONS */
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+#define MAX(x, y) (((x) < (y)) ? (y) : (x))
+dump(id, 1);
