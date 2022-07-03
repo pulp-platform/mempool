4414d3c1d6f9e6b7f3cab37ad6646ac199c35c71
Samuel Riedel <sriedel@iis.ee.ethz.ch>
2022-07-03 18:20:13 +0200

    Add dotp kernel

--- software/apps/dotp/main.c
+++ software/apps/dotp/main.c
@@ -2,138 +2,112 @@
 // Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
-// Author: Marco Bertuletti, ETH Zurich
+// Author: Samuel Riedel, ETH Zurich
 
 #include <stdint.h>
-#include <stdlib.h>
 #include <string.h>
 
 #include "encoding.h"
+#include "kernel/dotp.h"
 #include "printf.h"
 #include "runtime.h"
 #include "synchronization.h"
 
-#include "define.h"
+#define N (1024 * 96)
 
-#include "dotp_parallel.h"
-#include "dotp_parallel_local.h"
-#include "dotp_parallel_red0.h"
-#include "dotp_parallel_redtree.h"
-#include "dotp_single.h"
+dump(time, 0);
 
-void init_vectors(int32_t *in_a, int32_t *in_b, int32_t *s, int32_t *p_result,
-                  int32_t *p_check, uint32_t Len) {
-  *p_result = 0;
-  *p_check = 0;
-  uint32_t j = 0;
-  uint32_t num_cores = mempool_get_core_count();
-  while (j < Len) {
-    int32_t a = (int32_t)(j % num_cores);
-    int32_t b = (int32_t)(j % 4 + 3);
-    in_a[j] = a;
-    in_b[j] = b;
-    *p_check = *p_check + (int32_t)(a * b);
-    j++;
+int32_t vec_a[N] __attribute__((section(".l1_prio")));
+int32_t vec_b[N] __attribute__((section(".l1_prio")));
+
+volatile int32_t vec_a_l2[N] __attribute__((section(".l2")));
+volatile int32_t vec_b_l2[N] __attribute__((section(".l2")));
+
+void init_vector(volatile int32_t *vec, uint32_t size, uint32_t core_id,
+                 uint32_t num_cores) {
+  const int32_t unroll = 4;
+  for (int32_t i = unroll * (int32_t)core_id; i < (int32_t)size;
+       i += unroll * (int32_t)num_cores) {
+    vec[i + 0] = i - (int32_t)num_cores;
+    vec[i + 1] = -i - (int32_t)num_cores;
+    vec[i + 2] = i + (int32_t)num_cores;
+    vec[i + 3] = -i + (int32_t)num_cores;
   }
-#if defined(PARALLEL_RED0) || defined(PARALLEL_UNROLLED_RED0) ||               \
-    defined(PARALLEL_REDTREE) || defined(PARALLEL_UNROLLED_REDTREE)
-  for (uint32_t k = 0; k < N_BANK; k++) {
-    s[k] = 0;
-    red_barrier[k] = 0;
-  }
-#else
-  *s = 0;
-#endif
 }
 
 int main() {
-
   uint32_t core_id = mempool_get_core_id();
   uint32_t num_cores = mempool_get_core_count();
-  uint32_t time_init, time_end;
-  // initialize synchronization variables
   mempool_barrier_init(core_id);
 
+  int32_t *c = (int32_t *)(128 * 1024);
+  int32_t *dotp_barrier_a = (int32_t *)(64 * 1024);
+  int32_t *dotp_barrier_b = (int32_t *)(192 * 1024);
+
+  // Initialize img
+  init_vector(vec_a, N, core_id, num_cores);
+  init_vector(vec_b, N, core_id, num_cores);
+  mempool_barrier(num_cores);
+
   if (core_id == 0) {
-    error = 0;
-    time_init = 0;
-    time_end = 0;
-#if defined(PARALLEL_RED0) || defined(PARALLEL_UNROLLED_RED0) ||               \
-    defined(PARALLEL_REDTREE) || defined(PARALLEL_UNROLLED_REDTREE)
-    init_vectors(vector_a, vector_b, sum, &result, &check, LEN);
-#else
-    init_vectors(vector_a, vector_b, &sum, &result, &check, LEN);
-#endif
+    *c = 0;
+    // *dotp_barrier_a = 0;
+    // *dotp_barrier_b = 0;
+    // dma_memcpy_blocking(vec_a_l2, vec_a, N * sizeof(int32_t));
+    // dma_memcpy_blocking(vec_b_l2, vec_b, N * sizeof(int32_t));
   }
-  mempool_barrier(num_cores); // wait until all cores have finished
 
-  // Kernel execution
-
-  time_init = mempool_get_timer();
-#ifdef SINGLE
-  dotp_single(vector_a, vector_b, &sum, LEN);
-#elif defined(SINGLE_UNROLLED)
-  dotp_single_unrolled4(vector_a, vector_b, &sum, LEN);
-#endif
-  time_end = mempool_get_timer();
-
-  time_init = mempool_get_timer();
+  // Vectors are initialized --> Start calculating
+  // Wait at barrier until everyone is ready
+  mempool_barrier(num_cores);
+  uint32_t start = mempool_get_timer();
   mempool_start_benchmark();
-/* A) Parallelized workload
-   B) Atomic fetch and add to a single memory location
-   C) Barrier */
-#ifdef PARALLEL
-  dotp_parallel(vector_a, vector_b, &sum, LEN, N_PE);
-#elif defined(PARALLEL_UNROLLED)
-  dotp_parallel_unrolled4(vector_a, vector_b, &sum, LEN, N_PE);
-/* A) Parallelized workload
-   B) Atomic fetch and add to local memory banks
-   C) Barrier
-   D) Final reduction by core 0 incorporated in a barrier */
-#elif defined(PARALLEL_RED0)
-  dotp_parallel_red0(vector_a, vector_b, sum, LEN, N_PE);
-#elif defined(PARALLEL_UNROLLED_RED0)
-  dotp_parallel_unrolled4_red0(vector_a, vector_b, sum, LEN, N_PE);
-/* A) Parallelized workload
-   B) Nested set of barriers: reduction is performed in a logarithmic tree. */
-#elif defined(PARALLEL_REDTREE)
-  dotp_parallel_redtree(vector_a, vector_b, sum, LEN, N_PE);
-#elif defined(PARALLEL_UNROLLED_REDTREE)
-  dotp_parallel_redtree_unrolled(vector_a, vector_b, sum, LEN, N_PE);
-#endif
+  dotp_parallel((const int32_t *)vec_a, (const int32_t *)vec_b, N, c, core_id,
+                num_cores);
+  // dotp_parallel_dma((const int32_t *)vec_a, (const int32_t *)vec_b, N,
+  //               (const int32_t *)vec_a_l2, (const int32_t *)vec_b_l2, N, c,
+  //               core_id, num_cores);
   mempool_stop_benchmark();
-  time_end = mempool_get_timer();
+  // if (core_id == 44) {
+  //   dump_time(mempool_get_timer() - start);
+  // }
+  mempool_start_benchmark();
 
-  /* A) Parallelized workload
-     B) Atomic fetch and add to a single memory location
-     C) Barrier */
-  if (core_id < N_PE) {
-    time_init = mempool_get_timer();
-    mempool_start_benchmark();
-#ifdef PARALLEL_LOCAL
-    dotp_parallel_local(vector_a, vector_b, &sum, LEN, N_PE);
-#elif defined(LOCAL_UNROLLED)
-    dotp_parallel_local_unrolled4(vector_a, vector_b, &sum, LEN, N_PE);
-#endif
-    mempool_stop_benchmark();
-    time_end = mempool_get_timer();
-  }
-
+  // Wait at barrier befor checking
   mempool_barrier(num_cores);
-  // Check results
-  if (core_id == 0) {
-    uint32_t clock_cycles = (time_end - time_init);
-#if defined(PARALLEL_RED0) || defined(PARALLEL_UNROLLED_RED0) ||               \
-    defined(PARALLEL_REDTREE) || defined(PARALLEL_UNROLLED_REDTREE)
-    result = sum[0];
-#else
-    result = sum;
-#endif
-    printf("\nKernel execution takes %d clock cycles\n", clock_cycles);
-    printf("Result ==> %d\n", result);
-    printf("Check  ==> %d\n\n", check);
-  }
+  mempool_stop_benchmark();
+  uint32_t stop = mempool_get_timer();
+  // if (core_id == 44) {
+  //   dump_time(stop - start);
+  // }
+
+  // Hot cache
+  start = mempool_get_timer();
+  mempool_start_benchmark();
+  dotp_parallel((const int32_t *)vec_a, (const int32_t *)vec_b, N, c, core_id,
+                num_cores);
+  // dotp_parallel_dma((const int32_t *)vec_a, (const int32_t *)vec_b, N,
+  //               (const int32_t *)vec_a_l2, (const int32_t *)vec_b_l2, N, c,
+  //               core_id, num_cores);
+  mempool_stop_benchmark();
+  // if (core_id == 44) {
+  //   dump_time(mempool_get_timer() - start);
+  // }
+  mempool_start_benchmark();
+
+  // Wait at barrier befor checking
+  mempool_barrier(num_cores);
+  mempool_stop_benchmark();
+  stop = mempool_get_timer();
+  // if (core_id == 44) {
+  //   dump_time(stop - start);
+  // }
+
+  // Check result
+  // TODO
+
+  // wait until all cores have finished
   mempool_barrier(num_cores);
 
-  return error;
+  return 0;
 }
