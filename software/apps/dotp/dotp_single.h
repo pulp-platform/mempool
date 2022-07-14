d6e775212b8ec7d2f2e57d51fa5f584582c89f5c
Marco Bertuletti <mbertuletti@iis.ee.ethz.ch>
2022-07-14 10:03:52 +0200

    [software] Add assembly in dotp single core

--- software/apps/dotp/dotp_single.h
+++ software/apps/dotp/dotp_single.h
@@ -25,7 +25,8 @@ void dotp_single (	int32_t* in_a,
     mempool_stop_benchmark();
 
 	}
-  mempool_log_barrier(2, core_id);
+  mempool_barrier(NUM_CORES);
+  //mempool_log_barrier(2, core_id);
 }
 
 /* Single-core dot-product unrolled4 */
@@ -41,24 +42,63 @@ void dotp_single_unrolled4 (  int32_t* in_a,
     uint32_t reminder = Len % 4;
     uint32_t i;
 
-    register int32_t a0, b0, a1, b1, a2, b2, a3, b3;
+    int32_t a0 = 0, b0 = 0, a1 = 0, b1 = 0, a2 = 0, b2 = 0, a3 = 0, b3 = 0;
     register int32_t local_sum_1 = 0;
     register int32_t local_sum_2 = 0;
     register int32_t local_sum_3 = 0;
     register int32_t local_sum_4 = 0;
     for(i = 0; i < (Len - reminder); i += 4) {
-        a0 = in_a[i];
-        b0 = in_b[i];
-        a1 = in_a[i + 1];
-        b1 = in_b[i + 1];
-        a2 = in_a[i + 2];
-        b2 = in_b[i + 2];
-        a3 = in_a[i + 3];
-        b3 = in_b[i + 3];
-        local_sum_1 += a0 * b0;
-        local_sum_2 += a1 * b1;
-        local_sum_3 += a2 * b2;
-        local_sum_4 += a3 * b3;
+
+//        asm volatile (
+//          "p.lbu   %[a0], 1(%[idx_a]!);"
+//          "p.lbu   %[b0], 1(%[idx_b]!);"
+//          "p.lbu   %[a1], 1(%[idx_a]!);"
+//          "p.lbu   %[b1], 1(%[idx_b]!);"
+//          "p.lbu   %[a2], 1(%[idx_a]!);"
+//          "p.lbu   %[b2], 1(%[idx_b]!);"
+//          "p.lbu   %[a3], 1(%[idx_a]!);"
+//          "p.lbu   %[b3], 1(%[idx_b]!);"
+//          "p.mac   %[s0], %[a0], %[b0];"
+//          "p.mac   %[s1], %[a1], %[b1];"
+//          "p.mac   %[s2], %[a2], %[b2];"
+//          "p.mac   %[s3], %[a3], %[b3];"
+//          : [a0] "+r" (a0), [a1] "+r" (a1), [a2] "+r" (a2), [a3] "+r" (a3),
+//            [b0] "+r" (b0), [b1] "+r" (b1), [b2] "+r" (b2), [b3] "+r" (b3),
+//            [idx_a] "+r" (in_a), [idx_b] "+r" (in_b),
+//            [s0] "+r" (local_sum_1), [s1] "+r" (local_sum_2), [s2] "+r" (local_sum_3), [s3] "+r" (local_sum_4));
+
+        asm volatile (
+          "lw   %[a0], 0(%[idx_a]);"
+          "lw   %[b0], 0(%[idx_b]);"
+          "lw   %[a1], 4(%[idx_a]);"
+          "lw   %[b1], 4(%[idx_b]);"
+          "lw   %[a2], 8(%[idx_a]);"
+          "lw   %[b2], 8(%[idx_b]);"
+          "lw   %[a3], 12(%[idx_a]);"
+          "lw   %[b3], 12(%[idx_b]);"
+          "addi    %[idx_a], %[idx_a], 16;"
+          "addi    %[idx_b], %[idx_b], 16;"
+          "p.mac   %[s0], %[a0], %[b0];"
+          "p.mac   %[s1], %[a1], %[b1];"
+          "p.mac   %[s2], %[a2], %[b2];"
+          "p.mac   %[s3], %[a3], %[b3];"
+          : [a0] "+r" (a0), [a1] "+r" (a1), [a2] "+r" (a2), [a3] "+r" (a3),
+            [b0] "+r" (b0), [b1] "+r" (b1), [b2] "+r" (b2), [b3] "+r" (b3),
+            [idx_a] "+r" (in_a), [idx_b] "+r" (in_b),
+            [s0] "+r" (local_sum_1), [s1] "+r" (local_sum_2), [s2] "+r" (local_sum_3), [s3] "+r" (local_sum_4));
+
+        //a0 = in_a[i];
+        //b0 = in_b[i];
+        //a1 = in_a[i + 1];
+        //b1 = in_b[i + 1];
+        //a2 = in_a[i + 2];
+        //b2 = in_b[i + 2];
+        //a3 = in_a[i + 3];
+        //b3 = in_b[i + 3];
+        //local_sum_1 += a0 * b0;
+        //local_sum_2 += a1 * b1;
+        //local_sum_3 += a2 * b2;
+        //local_sum_4 += a3 * b3;
     }
     while (i < Len) {
       a0 = in_a[i];
@@ -74,5 +114,6 @@ void dotp_single_unrolled4 (  int32_t* in_a,
     mempool_stop_benchmark();
 
   }
-  mempool_log_barrier(2, core_id);
+  mempool_barrier(NUM_CORES);
+  //mempool_log_barrier(2, core_id);
 }
