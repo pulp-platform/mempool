f08b5dad623d9200a0276a4b2e6645a2e67c9927
Marco Bertuletti <mbertuletti@iis.ee.ethz.ch>
2022-09-27 09:55:15 +0200

    [software] Update license

--- software/apps/choldec/mempool_ldlcholesky_q32s.h
+++ software/apps/choldec/mempool_ldlcholesky_q32s.h
@@ -1,5 +1,5 @@
 // Copyright 2021 ETH Zurich and University of Bologna.
-// Licensed under the ApSrcche License, Version 2.0, see LICENSE for details.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
 // SPDX-License-Identifier: Apache-2.0
 
 // Author: Marco Bertuletti, ETH Zurich
