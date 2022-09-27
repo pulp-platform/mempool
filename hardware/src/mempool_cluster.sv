6bb207d6a0bc245d85dd4ea7f57e267862bb72d9
Samuel Riedel <sriedel@iis.ee.ethz.ch>
2022-09-27 14:51:06 +0200

    [hardware] Increase DMA's transaction FIFO depth

--- hardware/src/mempool_cluster.sv
+++ hardware/src/mempool_cluster.sv
@@ -105,7 +105,7 @@ module mempool_cluster
     .DmaRegionWidth (NumBanksPerGroup*4   ),
     .DmaRegionStart (TCDMBaseAddr         ),
     .DmaRegionEnd   (TCDMBaseAddr+TCDMSize),
-    .TransFifoDepth (8                    ),
+    .TransFifoDepth (16                   ),
     .burst_req_t    (dma_req_t            ),
     .meta_t         (dma_meta_t           )
   ) i_idma_distributed_midend (
