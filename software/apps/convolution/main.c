5ea3902bb96145b72cdfdd8a09536358eba28db8
Samuel Riedel <sriedel@iis.ee.ethz.ch>
2022-10-10 11:12:44 +0200

    [software] Use hierarchical barriers in convolution

--- software/apps/convolution/main.c
+++ software/apps/convolution/main.c
@@ -38,7 +38,7 @@ uint32_t dma_log_barrier(uint32_t step, uint32_t log2_radix, uint32_t core_id) {
   uint32_t* log_barrier = (uint32_t *)(((core_id / step) * step + (step >> log2_radix) - 1) * 1024);
 
   uint32_t val = __atomic_fetch_add(log_barrier, 1, __ATOMIC_RELAXED);
-  dump_barrier(step * 1024*1024+(uint32_t)log_barrier + val);
+  // dump_barrier(step * 1024*1024+(uint32_t)log_barrier + val);
   if (val == (uint32_t)((1 << log2_radix) - 1)) {
     // Last core of this stage
     if (step == NUM_CORES) {
@@ -50,8 +50,8 @@ uint32_t dma_log_barrier(uint32_t step, uint32_t log2_radix, uint32_t core_id) {
       __atomic_store_n(log_barrier, 0, __ATOMIC_RELAXED);
       return dma_log_barrier(step << log2_radix, log2_radix, core_id);
     }
-  } else if (val == 0 && step == NUM_CORES) {
-    // First core of last stage
+  } else if (val == 0 && (uint32_t)log_barrier == 0) {
+    // First core of first barrier in first stage
     // Check that the DMA from the previous iteration is done
     dma_wait();
     // Wake up all cores to get to work
@@ -73,6 +73,11 @@ int main() {
   uint32_t core_id = mempool_get_core_id();
   uint32_t num_cores = mempool_get_core_count();
   mempool_barrier_init(core_id);
+  // Initial setup
+  int32_t *round_barrier = (int32_t *)(core_id * 1024);
+  if (core_id != 0) {
+    *round_barrier = 0;
+  }
 
   int32_t kernel[KERNEL_N * KERNEL_N];
 
@@ -99,23 +104,24 @@ int main() {
   // Double-buffered convolution
   const int last_round = 6;
   // const int first = 0;
-  const int last = (int)4 - 1; // Radix 4 barrier
-  int32_t *round_barrier = (int32_t *)(core_id * 1024);
-
-  // Initial setup
-  if (core_id != 0) {
-    *round_barrier = 0;
-    // wake_up_all();
-  }
+  const uint32_t log2_radix = 4;
+  const uint32_t radix = 1 << log2_radix;
+  const uint32_t last = radix - 1; // Radix 4 barrier
 
   // Matrices are initialized --> Start calculating
   // Wait at barrier until everyone is ready
   mempool_barrier(num_cores);
 
+  // Initial setup
+  if (core_id == 0) {
+    wake_up_all();
+  }
+
   const int32_t *in_comp;
   const int32_t *in_dma;
   int32_t *out_comp;
   int32_t *out_dma;
+  uint32_t bar;
   for (int round = 0; round < last_round; ++round) {
     if (round % 2 == 0) {
       in_comp = (const int32_t *)&in[0];
@@ -129,11 +135,9 @@ int main() {
       out_comp = (int32_t *)&out[N * M / 2];
     }
     mempool_start_benchmark();
-    while(*((int32_t *)(63 * 1024)) > last) {
-      mempool_wait(num_cores);
-    }
+    mempool_wfi();
     // Barrier, launch DMA for next iteration
-    uint32_t bar = dma_log_barrier(4, 2, core_id);
+    bar = dma_log_barrier(radix, log2_radix, core_id);
     if (bar) {
       // We are the last one, reset the barrier
       // The old data can now be overwritten with a new DMA request
@@ -149,9 +153,9 @@ int main() {
       __atomic_store_n((uint32_t*)bar, 0, __ATOMIC_RELAXED);
       // __atomic_fetch_add(round_barrier, -num_cores, __ATOMIC_RELAXED);
       // *round_barrier = 0;
-      // if (round != last_round - 1) {
-      //   wake_up_all();
-      // }
+      if (round != last_round - 1) {
+        wake_up_all();
+      }
     } else {
       // Wait until the core checking the DMA gives the signal
       // mempool_wfi();
@@ -163,6 +167,27 @@ int main() {
                               core_id, num_cores);
     mempool_stop_benchmark();
   }
+  // Last write back
+  // bar = dma_log_barrier(radix, log2_radix, core_id);
+  // if (bar) {
+  //   // We are the last one, reset the barrier
+  //   // The old data can now be overwritten with a new DMA request
+  //   if (round != last_round - 1) {
+  //     dma_memcpy_nonblocking((void *)in_dma, (void *)in_l2,
+  //                            M * N / 2 * sizeof(int32_t));
+  //   }
+  //   if (round != 0) {
+  //     dma_memcpy_nonblocking((void *)out_l2, (void *)out_dma,
+  //                            M * N / 2 * sizeof(int32_t));
+  //   }
+  //   // We are the last one, reset the barrier
+  //   __atomic_store_n((uint32_t*)bar, 0, __ATOMIC_RELAXED);
+  //   // __atomic_fetch_add(round_barrier, -num_cores, __ATOMIC_RELAXED);
+  //   // *round_barrier = 0;
+  //   if (round != last_round - 1) {
+  //     wake_up_all();
+  //   }
+  // }
 
   mempool_start_benchmark();
 
