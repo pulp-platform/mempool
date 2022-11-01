080b8f99b2bb6b96d3a9da3c30206c5a0a99b620
Samuel Riedel <sriedel@iis.ee.ethz.ch>
2022-11-01 13:30:28 +0100

    [config] Update configurations for new AXI parametrization

--- config/minpool.mk
+++ config/minpool.mk
@@ -20,6 +20,15 @@ num_cores_per_tile ?= 4
 # L1 scratchpad banking factor
 banking_factor ?= 4
 
+#########################
+##  AXI configuration  ##
+#########################
+# AXI bus data width (in bits)
+axi_data_width ?= 256
+
+# Read-only cache line width in AXI interconnect (in bits)
+ro_line_width ?= 256
+
 # Number of DMA backends in each group
 dmas_per_group ?= 1
 
