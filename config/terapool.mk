b90ffb2e503fa15fb46c4ff83699aee7a79c9f10
Yichao Zhang <yiczhang@iis.ee.ethz.ch>
2022-12-13 15:50:51 +0100

    [Config] Modify terapool config for sub group hierarchy

--- config/terapool.mk
+++ config/terapool.mk
@@ -12,13 +12,13 @@
 num_cores ?= 1024
 
 # Number of groups
-num_groups ?= 8
+num_groups ?= 4
 
 # Number of cores per Terapool tile
 num_cores_per_tile ?= 8
 
 # Number of sub groups per Terapool group
-num_sub_groups_per_group ?= 1
+num_sub_groups_per_group ?= 4
 
 # L1 scratchpad banking factor
 banking_factor ?= 4
@@ -27,7 +27,7 @@ banking_factor ?= 4
 axi_hier_radix ?= 10
 
 # Number of AXI masters per group
-axi_masters_per_group ?= 2
+axi_masters_per_group ?= 4
 
 # Number of DMA backends in each group
-dmas_per_group ?= 4
+dmas_per_group ?= 8
