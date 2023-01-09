a0bb5a14c70948308462b30224165be5ae3406fa
Marco Bertuletti <mbertuletti@iis.ee.ethz.ch>
2023-01-09 16:31:20 +0100

    [software] Add fp16 test

--- software/apps/fp_test/main.c
+++ software/apps/fp_test/main.c
@@ -7,11 +7,17 @@
 #include <stdint.h>
 #include <string.h>
 
+#define INT32
+#define INT16
+
 #include "encoding.h"
 #include "runtime.h"
 #include "synchronization.h"
+dump(x,1);
 
-volatile float a, b, c;
+//volatile float a, b, c;
+volatile float16 a, b;
+volatile float16 c;
 
 int main() {
   uint32_t core_id = mempool_get_core_id();
@@ -19,15 +25,25 @@ int main() {
   // Initialize synchronization variables
   mempool_barrier_init(core_id);
 
+#if defined(INT32)
   if (core_id == 0) {
     a = 6.3f + (float)core_id;
     b = 7.77f;
-    c = a * b;
-    //    asm volatile("fmul.s %[c], %[a], %[b];"
-    //                 : [c] "=r"(c)
-    //                 : [a] "r"(a), [b] "r"(b));
+    asm volatile("fmul.s %[c], %[a], %[b];"
+                 : [c] "=r"(c)
+                 : [a] "r"(a), [b] "r"(b));
   }
   // wait until all cores have finished
   mempool_barrier(num_cores);
+#elif defined(INT16)
+  if (core_id == 0) {
+    a = (float16)6.3f + (float16)core_id;
+    b = (float16)7.77f;
+    c = a * b;
+  }
+  // wait until all cores have finished
+  mempool_barrier(num_cores);
+#endif
+
   return 0;
 }
