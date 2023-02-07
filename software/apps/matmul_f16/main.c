83634c183daa0905f1827c0e94e7aa8181553bf5
Marco Bertuletti <mbertuletti@iis.ee.ethz.ch>
2023-02-07 14:15:47 +0100

    [software] Add vectorized fp32 and fp16 matmuls

--- software/apps/matmul_f16/main.c
+++ software/apps/matmul_f16/main.c
@@ -8,41 +8,48 @@
 #include <string.h>
 
 #include "encoding.h"
-#include "printf.h"
 #include "runtime.h"
 #include "synchronization.h"
+
+typedef __fp16 v2f16 __attribute__((vector_size(4)));
+typedef union {
+    float f32;
+    v2f16 vec;
+} v2h;
 
 #include "data_matmulf16.h"
 #include "kernel/mat_mul_f16.h"
 
 #define PARALLEL
 
-float16 matrix_a[matrix_M * matrix_N] __attribute__((aligned((matrix_M * matrix_N)/2), section(".l1")));
-float16 matrix_b[matrix_N * matrix_P] __attribute__((aligned((matrix_N * matrix_P)/2), section(".l1")));
-float16 matrix_c[matrix_M * matrix_P] __attribute__((aligned((matrix_M * matrix_P)/2), section(".l1")));
+__fp16 matrix_a[matrix_M * matrix_N] __attribute__((aligned((matrix_M * matrix_N)/2), section(".l1")));
+__fp16 matrix_b[matrix_N * matrix_P] __attribute__((aligned((matrix_N * matrix_P)/2), section(".l1")));
+__fp16 matrix_c[matrix_M * matrix_P] __attribute__((aligned((matrix_M * matrix_P)/2), section(".l1")));
 
 int volatile error __attribute__((section(".l1")));
 
-void init_matrix(float16 *matrix, float16 *input, uint32_t num_rows,
+void init_matrix(__fp16 *matrix, __fp16 *input, uint32_t num_rows,
                  uint32_t num_columns, uint32_t core_id, uint32_t num_cores) {
   for (uint32_t i = core_id; i < (num_columns * num_rows); i += num_cores) {
-    matrix[i] = (float16) input[i];
+    matrix[i] = input[i];
   }
+  return;
 }
 
-int verify_result(float16 *__restrict__ C, float16 *__restrict__ Exp, uint32_t M,
+int verify_result(__fp16 *__restrict__ C, __fp16 *__restrict__ Exp, uint32_t M,
                   uint32_t P, uint32_t core_id, uint32_t num_cores) {
   if (core_id == 0) {
     for (uint32_t i = 0; i < M * P; i++) {
-      float16 error = (float16) 0.0f;
-      float16 exp = (float16) Exp[i];
-      float16 res = (float16) C[i];
-      //printf("%d %d\n", (int32_t) exp, (int32_t) res);
-      asm volatile("fsub.h %[error], %[res], %[exp];"
-                   : [error] "+&r"(error)
-                   : [res] "r"(res), [exp] "r"(exp));
-      if ((int32_t) error != 0) {
-        printf("ERROR!!! %d %d\n", i, error);
+      __fp16 error;
+      __fp16 exp = Exp[i];
+      __fp16 res = C[i];
+      asm volatile(
+        "fsub.h %[error], %[res], %[exp];"
+        : [error] "=&r"(error)
+        : [res] "r"(res), [exp] "r"(exp)
+        : );
+      if (*(int32_t*)&error != 0) {
+        printf("ERROR(%d): %d - %d - %d\n", i, *(int32_t*)&error, *(int32_t*)&exp, *(int32_t*)&res);
       }
     }
     // Wait at barrier before checking
@@ -56,11 +63,9 @@ int main() {
   uint32_t num_cores = mempool_get_core_count();
   // Initialize barrier and synchronize
   mempool_barrier_init(core_id);
-
   if (core_id == 0) {
     error = 0;
   }
-
   // Initialize Matrices
   init_matrix(matrix_a, A, matrix_M, matrix_N, core_id, num_cores);
   init_matrix(matrix_b, B, matrix_N, matrix_P, core_id, num_cores);
@@ -72,8 +77,6 @@ int main() {
   mempool_start_benchmark();
   matmul_2x2_parallel_f16_zfinx(matrix_a, matrix_b, matrix_c, matrix_M,
                                 matrix_N, matrix_P, core_id, num_cores);
-  //matmul_4x4_parallel_f16_zfinx(matrix_a, matrix_b, matrix_c, matrix_M,
-  //                              matrix_N, matrix_P, core_id, num_cores);
   //dump_id(core_id);
   mempool_stop_benchmark();
   // Wait at barrier before checking
@@ -92,6 +95,6 @@ int main() {
 
   verify_result(matrix_c, C, matrix_M, matrix_P, core_id, num_cores);
   mempool_barrier(num_cores);
-
   return error;
+  return 0;
 }
--- software/runtime/kernel/mat_mul_f32.h
+++ software/runtime/kernel/mat_mul_f32.h
@@ -166,6 +166,78 @@ void matmul_2x2_parallel_f32_zfinx(float const *__restrict__ A,
   }
 }
 
+
+void matmul_4x2_parallel_f32vec_zfinx(const float *__restrict__ pSrcA,
+                                      const float *__restrict__ pSrcB,
+                                      float *__restrict__ pDstC,
+                                      uint32_t M, uint32_t N,
+                                      uint32_t P, uint32_t core_id,
+                                      uint32_t numThreads) {
+  uint32_t i = 0; // loop counter for M
+  uint32_t j = 0; // loop counter for N
+  uint32_t k = 0; // loop counter for P
+  for (k = core_id; k < P / 2; k += numThreads) {
+    for (i = 0; i < M / 4; i++) {
+      float volatile sum00 = 0.0f;
+      float volatile sum01 = 0.0f;
+      float volatile sum10 = 0.0f;
+      float volatile sum11 = 0.0f;
+      float volatile sum20 = 0.0f;
+      float volatile sum21 = 0.0f;
+      float volatile sum30 = 0.0f;
+      float volatile sum31 = 0.0f;
+      for (j = 0; j < N / 2; j++) {
+        float a00 = pSrcA[(i * 4) * N + (j * 2)];
+        float a01 = pSrcA[(i * 4) * N + (j * 2) + 1];
+        float a10 = pSrcA[(i * 4 + 1) * N + (j * 2)];
+        float a11 = pSrcA[(i * 4 + 1) * N + (j * 2) + 1];
+        float a20 = pSrcA[(i * 4 + 2) * N + (j * 2)];
+        float a21 = pSrcA[(i * 4 + 2) * N + (j * 2) + 1];
+        float a30 = pSrcA[(i * 4 + 3) * N + (j * 2)];
+        float a31 = pSrcA[(i * 4 + 3) * N + (j * 2) + 1];
+        float b00 = pSrcB[(j * 2) * P + (k * 2)];
+        float b01 = pSrcB[(j * 2) * P + (k * 2) + 1];
+        float b10 = pSrcB[(j * 2 + 1) * P + (k * 2)];
+        float b11 = pSrcB[(j * 2 + 1) * P + (k * 2) + 1];
+        v2h aVec0, aVec1, aVec2, aVec3;
+        v2h bVec0, bVec1;
+        asm volatile(
+          "vfcpka.h.s %[aVec0], %[a00], %[a01];"
+          "vfcpka.h.s %[aVec1], %[a10], %[a11];"
+          "vfcpka.h.s %[aVec2], %[a20], %[a21];"
+          "vfcpka.h.s %[aVec3], %[a30], %[a31];"
+          "vfcpka.h.s %[bVec0], %[b00], %[b10];"
+          "vfcpka.h.s %[bVec1], %[b01], %[b11];"
+          "vfdotpex.s.h %[sum00], %[aVec0], %[bVec0];"
+          "vfdotpex.s.h %[sum01], %[aVec0], %[bVec1];"
+          "vfdotpex.s.h %[sum10], %[aVec1], %[bVec0];"
+          "vfdotpex.s.h %[sum11], %[aVec1], %[bVec1];"
+          "vfdotpex.s.h %[sum20], %[aVec2], %[bVec0];"
+          "vfdotpex.s.h %[sum21], %[aVec2], %[bVec1];"
+          "vfdotpex.s.h %[sum30], %[aVec3], %[bVec0];"
+          "vfdotpex.s.h %[sum31], %[aVec3], %[bVec1];"
+          : [sum00] "=r" (sum00), [sum01] "=r" (sum01), [sum10] "=r" (sum10), [sum11] "=r" (sum11),
+            [sum20] "=r" (sum20), [sum21] "=r" (sum21), [sum30] "=r" (sum30), [sum31] "=r" (sum31),
+            [aVec0] "=r" (aVec0), [aVec1] "=r" (aVec1), [aVec2] "=r" (aVec2), [aVec3] "=r" (aVec3),
+            [bVec0] "=r" (bVec0), [bVec1] "=r" (bVec1)
+          : [b00] "r" (b00), [b01] "r" (b01), [b10] "r" (b10), [b11] "r" (b11),
+            [a00] "r" (a00), [a01] "r" (a01), [a10] "r" (a10), [a11] "r" (a11),
+            [a20] "r" (a20), [a21] "r" (a21), [a30] "r" (a30), [a31] "r" (a31)
+          :
+        );
+      }
+      pDstC[(i * 4) * P + (k * 2)]         = sum00;
+      pDstC[(i * 4) * P + (k * 2 + 1)]     = sum01;
+      pDstC[(i * 4 + 1) * P + (k * 2)]     = sum10;
+      pDstC[(i * 4 + 1) * P + (k * 2 + 1)] = sum11;
+      pDstC[(i * 4 + 2) * P + (k * 2)]     = sum20;
+      pDstC[(i * 4 + 2) * P + (k * 2 + 1)] = sum21;
+      pDstC[(i * 4 + 3) * P + (k * 2)]     = sum30;
+      pDstC[(i * 4 + 3) * P + (k * 2 + 1)] = sum31;
+    }
+  }
+}
+
 ///*******************************/
 ///* ASM CODE KERNEL START BELOW */
 ///*******************************/
