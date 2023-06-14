ba4c332396460d8d194db73a0d5b9b43fb053f95
Yichao Zhang <yiczhang@iis.ee.ethz.ch>
2023-06-14 16:53:26 +0200

    [DRAM] DRAM update to support interleaved address mapping

--- hardware/dram_rtl_sim/test/axi_to_dram_tb.sv
+++ hardware/dram_rtl_sim/test/axi_to_dram_tb.sv
@@ -21,10 +21,11 @@ module axi_to_dram_tb;
     //////////////////////
 
     localparam int unsigned                 AXI_ADDR_WIDTH              = 32;
-    localparam int unsigned                 AXI_DATA_WIDTH              = 64;
+    localparam int unsigned                 AXI_DATA_WIDTH              = 512;
     localparam int unsigned                 AXI_STRB_WIDTH              = AXI_DATA_WIDTH / 8;
     localparam int unsigned                 AXI_ID_WIDTH                = 5;
     localparam int unsigned                 AXI_USER_WIDTH              = 5;
+    localparam int unsigned                 BASE                        = 32'h8000_0000;
 
     typedef logic [AXI_ADDR_WIDTH-1:0]      axi_addr_t;
     typedef logic [AXI_DATA_WIDTH-1:0]      axi_data_t;
@@ -80,13 +81,14 @@ module axi_to_dram_tb;
     //        DUT       //
     //////////////////////
 
-    dram_sim_engine #(.ClkPeriodNs(10)) i_dram_sim_engine (.clk_i(clk), .rst_ni(rst_n));
+    dram_sim_engine #(.ClkPeriodNs(1)) i_dram_sim_engine (.clk_i(clk), .rst_ni(rst_n));
 
     axi_dram_sim #(
         .AxiAddrWidth(AXI_ADDR_WIDTH),
         .AxiDataWidth(AXI_DATA_WIDTH),
         .AxiIdWidth  (AXI_ID_WIDTH),
         .AxiUserWidth(AXI_USER_WIDTH),
+        .BASE        (BASE),
         .axi_req_t   (axi_req_t),
         .axi_resp_t  (axi_resp_t),
         .axi_ar_t    (axi_ar_chan_t),
@@ -120,11 +122,44 @@ module axi_to_dram_tb;
 
     axi_master_t axi_master = new(axi_bus_dv);
 
+    //Speed test
+    task speedTest(int count);
+        automatic axi_master_t::ax_beat_t ar = new ;
+        automatic axi_master_t::r_beat_t r = new ;
+
+        ar = axi_master.new_rand_burst(0);
+        ar.ax_len = 0;
+        ar.ax_size = $clog2(AXI_DATA_WIDTH/8);
+        ar.ax_atop = axi_pkg::ATOP_NONE;
+        ar.ax_addr = (ar.ax_addr>>$clog2(AXI_DATA_WIDTH/8))<<$clog2(AXI_DATA_WIDTH/8);
+
+        $display("speedTest start!");
+
+        fork
+            //send ar
+            begin
+                for (int i = 0; i < count; i++) begin
+                    ar.ax_addr = ar.ax_addr + 64;
+                    axi_master.drv.send_ar(ar);
+                end
+            end
+            //receive r
+            begin
+                for (int i = 0; i < (count*(ar.ax_len+1)); i++) begin
+                    axi_master.drv.recv_r(r);
+                end
+            end
+        join
+
+        $display("speedTest done!");
+    endtask
+
     initial begin
         axi_master.reset();
-        axi_master.add_memory_region(0, 65636, axi_pkg::NORMAL_NONCACHEABLE_NONBUFFERABLE);
+        axi_master.add_memory_region(BASE + 0, BASE + 65636, axi_pkg::NORMAL_NONCACHEABLE_NONBUFFERABLE);
         @(posedge rst_n);
-        axi_master.run(1000,1000);
+        // axi_master.run(1000,1000);
+        speedTest(10000);
         $finish;
     end
 
