5e1c600ee848d3447451a5a1337f9b32d0bd584c
Yichao Zhang <yiczhang@iis.ee.ethz.ch>
2023-07-19 15:35:31 +0200

    [Format] Format DRAM python script for CI check

--- hardware/scripts/dramsys_plot.py
+++ hardware/scripts/dramsys_plot.py
@@ -15,21 +15,34 @@ license = """\
 //
 """
 
-def plot_power(df_pow, figsize=(8,2)):
+
+def plot_power(df_pow, figsize=(8, 2)):
     fig, ax = plt.subplots(figsize=figsize)
-    ax.plot(df_pow['time']*(10**9),df_pow['AveragePower'],'-', color='blue',label='power')
+    ax.plot(df_pow['time'] * (10**9),
+            df_pow['AveragePower'],
+            '-',
+            color='blue',
+            label='power')
     ax.set_ylabel('power (mW)')
     ax.set_xlabel('time (ns)')
-	  plt.show()
+
+    plt.show()
     pass
 
-def plot_bw(df_bw, figsize=(8,2)):
+
+def plot_bw(df_bw, figsize=(8, 2)):
     fig, ax = plt.subplots(figsize=figsize)
     ax.set_ylabel('bandwidth utilization')
     ax.set_xlabel('time (ns)')
-    ax.plot(df_bw['Time']*(10**9),df_bw['AverageBandwidth'],'-', color='red',label='bandwidth')
+    ax.plot(df_bw['Time'] * (10**9),
+            df_bw['AverageBandwidth'],
+            '-',
+            color='red',
+            label='bandwidth')
+
     plt.show()
     pass
+
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
@@ -38,8 +51,12 @@ if __name__ == '__main__':
         print("Error. Give me one argument: the DRAMSys database file path.")
         sys.exit()
         pass
-	  con = sqlite3.connect(db_file_name)
-	  df_bw = pd.read_sql_query('SELECT * FROM Bandwidth', con)
-	  plot_bw(df_bw)
-	  df_pow = pd.read_sql_query('SELECT * FROM Power', con)
-	  plot_power(df_pow)
+
+    con = sqlite3.connect(db_file_name)
+    df_bw = pd.read_sql_query('SELECT * FROM Bandwidth', con)
+
+    plot_bw(df_bw)
+
+    df_pow = pd.read_sql_query('SELECT * FROM Power', con)
+
+    plot_power(df_pow)
