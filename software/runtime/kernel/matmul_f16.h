5c65520e3b8664c566e86eb7e5187f48eeef3811
Marco Bertuletti <mbertuletti@iis.ee.ethz.ch>
2023-07-20 11:36:24 +0200

    [software] Fix SIMD matmul_f16

--- software/runtime/kernel/matmul_f16.h
+++ software/runtime/kernel/matmul_f16.h
@@ -13,10 +13,9 @@
 
 #include "xpulp/builtins_v2.h"
 
-void matmul_2x2_single_f16_zfinx(__fp16 const *__restrict__ A,
-                                 __fp16 const *__restrict__ B,
-                                 __fp16 *__restrict__ C, uint32_t M, uint32_t N,
-                                 uint32_t P) {
+void matmul_2x2_single_f16(__fp16 const *__restrict__ A,
+                           __fp16 const *__restrict__ B, __fp16 *__restrict__ C,
+                           uint32_t M, uint32_t N, uint32_t P) {
   for (uint32_t i = 0; i < M; i += 2) {
     for (uint32_t j = 0; j < P; j += 2) {
       __fp16 c00 = 0.0f;
@@ -87,11 +86,10 @@ void matmul_2x2_single_f16_zfinx(__fp16 const *__restrict__ A,
   }
 }
 
-void matmul_2x2_parallel_f16_zfinx(__fp16 const *__restrict__ A,
-                                   __fp16 const *__restrict__ B,
-                                   __fp16 *__restrict__ C, uint32_t M,
-                                   uint32_t N, uint32_t P, uint32_t id,
-                                   uint32_t numThreads) {
+void matmul_2x2_parallel_f16(__fp16 const *__restrict__ A,
+                             __fp16 const *__restrict__ B,
+                             __fp16 *__restrict__ C, uint32_t M, uint32_t N,
+                             uint32_t P, uint32_t id, uint32_t numThreads) {
   // Parallelize by assigning each core one row
   uint32_t const c = 8; // How many columns to split the matrix into
   uint32_t const c_start = (P / c) * (id % c);
@@ -166,11 +164,11 @@ void matmul_2x2_parallel_f16_zfinx(__fp16 const *__restrict__ A,
   }
 }
 
-void matmul_4x2_parallel_f16_zfinx(const __fp16 *__restrict__ pSrcA,
-                                   const __fp16 *__restrict__ pSrcB,
-                                   __fp16 *__restrict__ pDstC, uint32_t M,
-                                   uint32_t N, uint32_t P, uint32_t core_id,
-                                   uint32_t numThreads) {
+void matmul_4x2_parallel_f16(const __fp16 *__restrict__ pSrcA,
+                             const __fp16 *__restrict__ pSrcB,
+                             __fp16 *__restrict__ pDstC, uint32_t M, uint32_t N,
+                             uint32_t P, uint32_t core_id,
+                             uint32_t numThreads) {
 
   uint32_t i = 0; // loop counter for M
   uint32_t j = 0; // loop counter for N
@@ -241,11 +239,11 @@ void matmul_4x2_parallel_f16_zfinx(const __fp16 *__restrict__ pSrcA,
   }
 }
 
-void matmul_4x2_parallel_f16vec_zfinx(const __fp16 *__restrict__ pSrcA,
-                                      const __fp16 *__restrict__ pSrcB,
-                                      __fp16 *__restrict__ pDstC, uint32_t M,
-                                      uint32_t N, uint32_t P, uint32_t core_id,
-                                      uint32_t numThreads) {
+void matmul_4x2_parallel_f16vec(const __fp16 *__restrict__ pSrcA,
+                                const __fp16 *__restrict__ pSrcB,
+                                __fp16 *__restrict__ pDstC, uint32_t M,
+                                uint32_t N, uint32_t P, uint32_t core_id,
+                                uint32_t numThreads) {
 
   uint32_t i = 0; // loop counter for M
   uint32_t j = 0; // loop counter for N
@@ -269,21 +267,15 @@ void matmul_4x2_parallel_f16vec_zfinx(const __fp16 *__restrict__ pSrcA,
         v2h aVec3 = *(v2h *)&(pSrcA[(i * 4 + 3) * N + (j * 2)]);
         v2h bVecTemp0 = *(v2h *)&(pSrcB[(j * 2) * P + (k * 2)]);
         v2h bVecTemp1 = *(v2h *)&(pSrcB[(j * 2 + 1) * P + (k * 2)]);
-        unsigned bVec0 = 0U;
-        unsigned bVec1 = 0U;
+        v2h bVec0, bVec1;
         unsigned TempH, TempL;
-
         asm volatile(
-            "srli %[TempH], %[bVecTemp0], 0x10;"
-            "slli %[TempH], %[TempH], 0x10;"
-            "srli %[TempL], %[bVecTemp1], 0x10;"
-            "or   %[bVec0], %[TempH], %[TempL];"
-
-            "slli %[TempH], %[bVecTemp0], 0x10;"
-            "slli %[TempL], %[bVecTemp1], 0x10;"
-            "srli %[TempL], %[TempL], 0x10;"
-            "or   %[bVec1], %[TempH], %[TempL];"
-
+            "pv.extract.h %[TempH], %[bVecTemp0], 1;"
+            "pv.extract.h %[TempL], %[bVecTemp1], 1;"
+            "pv.pack %[bVec0], %[TempL], %[TempH];"
+            "pv.extract.h %[TempH], %[bVecTemp0], 0;"
+            "pv.extract.h %[TempL], %[bVecTemp1], 0;"
+            "pv.pack %[bVec1], %[TempL], %[TempH];"
             "vfdotpex.s.h %[sum00], %[aVec0], %[bVec0];"
             "vfdotpex.s.h %[sum01], %[aVec0], %[bVec1];"
             "vfdotpex.s.h %[sum10], %[aVec1], %[bVec0];"
@@ -292,21 +284,20 @@ void matmul_4x2_parallel_f16vec_zfinx(const __fp16 *__restrict__ pSrcA,
             "vfdotpex.s.h %[sum21], %[aVec2], %[bVec1];"
             "vfdotpex.s.h %[sum30], %[aVec3], %[bVec0];"
             "vfdotpex.s.h %[sum31], %[aVec3], %[bVec1];"
-
-            : [sum00] "=r"(sum00), [sum01] "=r"(sum01), [sum10] "=r"(sum10),
-              [sum11] "=r"(sum11), [sum20] "=r"(sum20), [sum21] "=r"(sum21),
-              [sum30] "=r"(sum30), [sum31] "=r"(sum31), [bVec0] "=&r"(bVec0),
-              [bVec1] "=&r"(bVec1), [TempH] "=&r"(TempH), [TempL] "=&r"(TempL)
+            : [sum00] "+&r"(sum00), [sum01] "+&r"(sum01), [sum10] "+&r"(sum10),
+              [sum11] "+&r"(sum11), [sum20] "+&r"(sum20), [sum21] "+&r"(sum21),
+              [sum30] "+&r"(sum30), [sum31] "+&r"(sum31), [bVec0] "=&r"(bVec0),
+              [bVec1] "+&r"(bVec1), [TempH] "+&r"(TempH), [TempL] "+&r"(TempL)
             : [aVec0] "r"(aVec0), [aVec1] "r"(aVec1), [aVec2] "r"(aVec2),
               [aVec3] "r"(aVec3), [bVecTemp0] "r"(bVecTemp0),
               [bVecTemp1] "r"(bVecTemp1)
             :);
       }
       v2h res0, res1, res2, res3;
-      asm volatile("vfcpka.h.s %[res0], %[sum00], %[sum01];"
-                   "vfcpka.h.s %[res1], %[sum10], %[sum11];"
-                   "vfcpka.h.s %[res2], %[sum20], %[sum21];"
-                   "vfcpka.h.s %[res3], %[sum30], %[sum31];"
+      asm volatile("vfcpka.h.s %[res0], %[sum01], %[sum00];"
+                   "vfcpka.h.s %[res1], %[sum11], %[sum10];"
+                   "vfcpka.h.s %[res2], %[sum21], %[sum20];"
+                   "vfcpka.h.s %[res3], %[sum31], %[sum30];"
                    : [res0] "=&r"(res0), [res1] "=&r"(res1), [res2] "=&r"(res2),
                      [res3] "=&r"(res3)
                    : [sum00] "r"(sum00), [sum01] "r"(sum01), [sum10] "r"(sum10),
