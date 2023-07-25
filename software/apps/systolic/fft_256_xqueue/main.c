cb13d4f30507f7c4ee5c7ea8ffd5fe269310e00e
Sergio Mazzola <smazzola@iis.ee.ethz.ch>
2023-07-25 19:24:33 +0200

    [software] WIP: Codebase fft_256_xqueue, clean shuffling and systolic_init

--- software/apps/systolic/fft_256_xqueue/main.c
+++ software/apps/systolic/fft_256_xqueue/main.c
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 // Author: Vaibhav Krishna, ETH Zurich
+//         Sergio Mazzola, ETH Zurich
 
 #include <stdint.h>
 #include <string.h>
@@ -14,42 +15,44 @@
 #include "runtime.h"
 #include "synchronization.h"
 #include "xpulp/builtins_v2.h"
+
 #include "data_cfftq16.h"
 #include "mempool_cfft_q16_twiddleCoef.h"
 #include "systolic/fft_256_xqueue.h"
 
+/* Settings */
+#define PRINTF_VERBOSE 1
+
 int main(){
   uint32_t core_id = mempool_get_core_id();
   uint32_t num_cores = mempool_get_core_count();
-  uint32_t tile_id = core_id / 4;
-
-
-  mempool_barrier_init(core_id);
 
   // Initialization
+  mempool_barrier_init(core_id);
   mempool_init(core_id);
 
-  // Allocate systolic grid mapping
-  uint32_t stage_idx;
-  uint32_t idx_in_stage;
+  /* Systolic grid mapping */
+  // stage_i = index of this PE's stage (FFT stage); ROW index
+  // pe_i    = index of this PE in its stage; COLUMN index
+  uint32_t stage_i = core_id % NUM_CORES_PER_TILE;
+  uint32_t pe_i    = core_id / NUM_CORES_PER_TILE; // tile ID
+  // NUM_CORES_PER_TILE is equal to NUM_STAGES, i.e. 4
 
-  stage_idx    = core_id % 4;
-  idx_in_stage = tile_id;
-
+  core_mapping[stage_i][pe_i] = core_id;
   mempool_barrier(num_cores);
 
-
-  tile_mapping[stage_idx*PE_PER_STAGE + idx_in_stage] = tile_id;
-  core_mapping[stage_idx*PE_PER_STAGE + idx_in_stage] = core_id;
-
-  mempool_barrier(num_cores);
-
-  //Setup
+  // Setup
   if(core_id == 0){
-    printf(">Initialize\n");
+    #if PRINTF_VERBOSE
+    printf("Initialize\n");
+    #endif
     shuffling_order_calc();
-    systolic_init();
   }
+  mempool_barrier(num_cores);
+
+  systolic_init(stage_i, pe_i);
+
+  mempool_barrier(num_cores);
 
   for(int16_t i=core_id;i<2048;i+=num_cores)
     pSrc[i]=vector_inp[i];
@@ -64,12 +67,12 @@ int main(){
   mempool_barrier(num_cores);
   mempool_start_benchmark();
 
-  if (stage_idx == 0) {
-    systolic_first_fft_pe(stage_idx, idx_in_stage);
-  } else if (stage_idx == (NUM_STAGE-1)){
-    systolic_end_pe(stage_idx, idx_in_stage, core_id);
+  if (stage_i == 0) {
+    systolic_first_fft_pe(stage_i, pe_i);
+  } else if (stage_i == (NUM_STAGES-1)){
+    systolic_end_pe(stage_i, pe_i, core_id);
   } else {
-    systolic_mid_pe(stage_idx, idx_in_stage, core_id);
+    systolic_mid_pe(stage_i, pe_i, core_id);
   }
 
   // Stop benchmark for all cores
@@ -78,7 +81,7 @@ int main(){
 
   // Print out benchmark
   if (core_id == 0) {
-    // for (uint32_t i=0;i<(2*FFTLEN);i++){
+    // for (uint32_t i=0;i<(2*LEN_FFT);i++){
     //   if (abs((vector_output[i] - vector_res[i])) > TOLERANCE)
     //     printf("ERROR!!! Result[%d]: %6d Expected[%d]: %6d\n", i, vector_output[i], i,
     //            vector_res[i]);
--- software/runtime/systolic/fft_256_xqueue.h
+++ software/runtime/systolic/fft_256_xqueue.h
@@ -24,149 +24,142 @@
 #include "synchronization.h"
 #include "xpulp/builtins_v2.h"
 
+#include "xqueue.h"
+#include "qlr.h"
 
-#define FFTLEN 256
-#define NUM_STAGE 4 
-#define PE_PER_STAGE 64
+/* Convolution configuration */
+#define RADIX        4                          // hardcoded, do not change
+#define LEN_FFT      256                        // hardcoded, do not change
+#define NUM_STAGES   (NUM_CORES_PER_TILE)       // hardcoded, do not change
+#define PE_PER_STAGE ((LEN_FFT) / (NUM_STAGES)) // hardcoded, do not change
+#define NUM_ITER     10
+
+#if NUM_CORES_PER_TILE != 4
+#error "Only supports 4 cores per tile (as RADIX, and NUM_STAGES)"
+#endif
+
 #define ASM
-#define NUM_ITER 100
-
-// QLR configuration
-#define QLR_CFG_T0 (QLR_CFG_BASE | (5 << 5))
-#define QLR_CFG_T1 (QLR_CFG_BASE | (6 << 5))
-#define QLR_CFG_T2 (QLR_CFG_BASE | (7 << 5))
-#define QLR_CFG_T3 (QLR_CFG_BASE | (28 << 5))
-#define QLR_CFG_TYPE 0
-#define QLR_CFG_REQ 2
-#define QLR_CFG_RF 3
-#define QLR_CFG_IADDR 4
-#define QLR_CFG_OADDR 5
-
-// QLRs
-register int32_t qlr_t0 asm("t0");
-register int32_t qlr_t1 asm("t1");
-register int32_t qlr_t2 asm("t2");
-register int32_t qlr_t3 asm("t3");
 
 // Array of queue ptrs
-int32_t *queues_output_0[NUM_STAGE-1][PE_PER_STAGE];
-int32_t *queues_output_1[NUM_STAGE-1][PE_PER_STAGE];
-int32_t *queues_output_2[NUM_STAGE-1][PE_PER_STAGE];
-int32_t *queues_output_3[NUM_STAGE-1][PE_PER_STAGE];
+uint32_t *queues_out_0[NUM_STAGES-1][PE_PER_STAGE];
+uint32_t *queues_out_1[NUM_STAGES-1][PE_PER_STAGE];
+uint32_t *queues_out_2[NUM_STAGES-1][PE_PER_STAGE];
+uint32_t *queues_out_3[NUM_STAGES-1][PE_PER_STAGE];
 
-//Global arrays
-uint16_t tile_mapping[FFTLEN] __attribute__((section(".l1")));
-uint16_t core_mapping[FFTLEN] __attribute__((section(".l1")));
-uint16_t shuffling_order[FFTLEN * (NUM_STAGE)] __attribute__((section(".l1")));
+// Global arrays
+uint16_t core_mapping[NUM_STAGES][PE_PER_STAGE] __attribute__((section(".l1")));
+uint16_t shuffling_order[NUM_STAGES][LEN_FFT] __attribute__((section(".l1")));
 int16_t pSrc[2048] __attribute__((aligned(2048), section(".l1")));
-int16_t  vector_output[2*FFTLEN] __attribute__((section(".l1")));
+int16_t  vector_output[2*LEN_FFT] __attribute__((section(".l1")));
 
-// queue push
-static inline void queue_push(void *const queue, int32_t data,
-                              int32_t *const ret) {
-  asm volatile("q.push.w %0, %1, (%2)" : "+r"(*ret) : "r"(data), "r"(queue));
-}
+void input_shuffling_order_r4(uint32_t stage_i, uint16_t* order){
+  // stage index in the inverted order (= remaining stages)
+  uint32_t stage_i_inv = NUM_STAGES - (stage_i + 1);
+  // number of point groups in this stage
+  // '2 *' because at every stage, num_groups reduces by a factor of RADIX (= 4)
+  // 2 because it is SQRT(RADIX)
+  uint32_t num_groups = (uint32_t)1 << (2 * stage_i_inv); // 2^(2*(NUM_STAGES - (stage_i + 1)))
+  // number of points in each group
+  uint16_t num_points_group = (uint16_t)(LEN_FFT / num_groups);
 
-// queue pop
-inline void queue_pop(void *const queue, int32_t *const ret) {
-  asm volatile("q.pop.w %0, 0(%1)" : "=r"(*ret) : "r"(queue));
-}
-
-void input_shuffling_order_r4(uint16_t nvar, uint16_t stage, int16_t* order){
-  uint16_t n_group, nvar_group, i, j;
-  n_group = 1 << (2*(NUM_STAGE-stage));
-  nvar_group = nvar/n_group;
-  for (i=0;i<n_group;i++){
-    for (j=0;j<(nvar_group/4);j++){
-      order[i*nvar_group + 4*j] = i*nvar_group + j;
-      order[i*nvar_group + 4*j + 1] = order[i*nvar_group + 4*j] + nvar_group/4;
-      order[i*nvar_group + 4*j + 2] = order[i*nvar_group + 4*j + 1] + nvar_group/4;
-      order[i*nvar_group + 4*j + 3] = order[i*nvar_group + 4*j + 2] + nvar_group/4;
+  // for every group (of num_points_group elements) in this stage
+  for (uint32_t i = 0; i < num_groups; i++){
+    // for each subgroup of RADIX points each
+    // i.e., take the first one and then offset the following ones
+    for (uint32_t j = 0; j < num_points_group; j += RADIX){
+      order[i*num_points_group + j + 0] = (uint16_t)((uint16_t)i*num_points_group + (uint16_t)j / RADIX);
+      order[i*num_points_group + j + 1] = (uint16_t)(order[(uint16_t)i*num_points_group + (uint16_t)j + 0] + num_points_group / RADIX);
+      order[i*num_points_group + j + 2] = (uint16_t)(order[(uint16_t)i*num_points_group + (uint16_t)j + 1] + num_points_group / RADIX);
+      order[i*num_points_group + j + 3] = (uint16_t)(order[(uint16_t)i*num_points_group + (uint16_t)j + 2] + num_points_group / RADIX);
     }
   }
 }
 
-void invert_shuffling_order(int16_t* order, int16_t* reverse){
-  int16_t temp;
-  for (uint16_t i=0;i<FFTLEN;i++){
+void invert_shuffling_order(uint16_t* order, uint16_t* reverse_order){
+  uint16_t temp;
+  for (uint32_t i = 0; i < LEN_FFT; i++){
     temp = order[i];
-    reverse[temp] = i;
+    reverse_order[temp] = (uint16_t)i;
   }
 }
 
 void shuffling_order_calc(){
+  /* Stage structure */
+  // stage 0 includes inputs (not shuffled)
+  // the FFT computing stages are 4 (0,1,2,3)
+  // stage 4 only contains the output of stage 3 with the last shuffling layer
+
   uint16_t *temp_next;
   uint16_t *temp_inv;
-  temp_next    = (uint16_t *)simple_malloc(FFTLEN * sizeof(uint16_t));
-  temp_inv     = (uint16_t *)simple_malloc(FFTLEN * sizeof(uint16_t));
-  int16_t i,j;
-  input_shuffling_order_r4(FFTLEN, 2, temp_next);
+  temp_next = (uint16_t *)simple_malloc(LEN_FFT * sizeof(uint16_t));
+  temp_inv  = (uint16_t *)simple_malloc(LEN_FFT * sizeof(uint16_t));
 
-  for (i=0;i<FFTLEN;i++){
-    shuffling_order[i] = temp_next[i];
+  input_shuffling_order_r4(1, temp_next);
+  for (uint32_t i = 0; i < LEN_FFT; i++){
+    shuffling_order[0][i] = temp_next[i];
   }
 
-  invert_shuffling_order(temp_next, temp_inv);
-  input_shuffling_order_r4(FFTLEN, 3, temp_next);
-
-  for (i=0;i<FFTLEN;i++){
-    j = temp_next[i];
-    shuffling_order[i + FFTLEN] = temp_inv[j];
+  for (uint32_t stage_i = 1; stage_i < (NUM_STAGES - 1); stage_i++){
+    invert_shuffling_order(temp_next, temp_inv);
+    input_shuffling_order_r4(stage_i + 1, temp_next);
+    for (uint32_t i = 0; i < LEN_FFT; i++){
+      shuffling_order[stage_i][i] = temp_inv[temp_next[i]];
+    }
   }
 
-  invert_shuffling_order(temp_next, temp_inv);
-  input_shuffling_order_r4(FFTLEN, 4, temp_next);
-
-  for (i=0;i<FFTLEN;i++){
-    j = temp_next[i];
-    shuffling_order[i + 2*FFTLEN] = temp_inv[j];
+  for (uint32_t i = 0; i < LEN_FFT; i++){
+    shuffling_order[NUM_STAGES - 1][i] = temp_next[i];
   }
 
-  //Queue addressing and array addressing are reverse of each other
-  for (i=0;i<FFTLEN;i++){
-    shuffling_order[i + 3*FFTLEN] = temp_next[i];
-  }
   simple_free(temp_inv);
   simple_free(temp_next);
 }
 
-void systolic_init() {
+void systolic_init(uint32_t stage_i, uint32_t pe_i) {
   // Create systolic array via queues
-  extern uint32_t __seq_start;
-  uint32_t tile_id_o[4], core_id_o[4], queue_id_o[4], tile_offset_o[4], core_offset_o[4], idx_out;
-  for (uint32_t y = 0; y < (NUM_STAGE-1); ++y) {
-    for (uint32_t x = 0; x < PE_PER_STAGE; ++x) {
-      for (uint32_t i = 0; i < 4; ++i){
-        idx_out       = shuffling_order[(y)*FFTLEN + x*4 + i];
-        tile_id_o[i]  = tile_mapping[(y+1)*PE_PER_STAGE + idx_out/4];
-        core_id_o[i]  = core_mapping[(y+1)*PE_PER_STAGE + idx_out/4];
-        queue_id_o[i] = idx_out%4;
+  extern uint8_t __seq_start;
+  uint32_t index_out;
+  //TODO: Fix all the "4"
+  uint32_t tile_id_out[NUM_QUEUES_PER_CORE], core_id_out[NUM_QUEUES_PER_CORE], queue_id_out[NUM_QUEUES_PER_CORE];
+  uint32_t tile_offset[NUM_QUEUES_PER_CORE], core_offset[NUM_QUEUES_PER_CORE], queue_offset[NUM_QUEUES_PER_CORE];
 
-        tile_offset_o[i] = tile_id_o[i] * SEQ_MEM_SIZE;
-        core_offset_o[i] = (core_id_o[i] % 4) * 4;
-      }
+  // Compute output PEs for all stages, but the last one
+  if (stage_i < (NUM_STAGES - 1)) {
+    // Each PE has 4 outputs: make them correspond to the 4 available queues of each PE
+    for (uint32_t i = 0; i < NUM_QUEUES_PER_CORE; i++){
+      index_out = shuffling_order[stage_i][(pe_i * RADIX) + i];
+      core_id_out[i]  = core_mapping[stage_i + 1][index_out / RADIX];
+      tile_id_out[i]  = core_id_out[i] / NUM_CORES_PER_TILE;
+      queue_id_out[i] = index_out % RADIX;
 
-      queues_output_0[y][x] = &__seq_start + tile_offset_o[0] + core_offset_o[0] + queue_id_o[0];
-      queues_output_1[y][x] = &__seq_start + tile_offset_o[1] + core_offset_o[1] + queue_id_o[1];
-      queues_output_2[y][x] = &__seq_start + tile_offset_o[2] + core_offset_o[2] + queue_id_o[2];
-      queues_output_3[y][x] = &__seq_start + tile_offset_o[3] + core_offset_o[3] + queue_id_o[3];
+      // base address of this tile's sequential memory region
+      tile_offset[i] = tile_id_out[i] * NUM_CORES_PER_TILE * SEQ_MEM_SIZE;
+      // next address goes to next bank, then wrap around tile sequential region
+      core_offset[i] = (core_id_out[i] % NUM_CORES_PER_TILE) * NUM_QUEUES_PER_CORE * sizeof(int32_t);
+      // each PE has 4 outputs, same number as queues: decide which out queue must be fed with output i
+      queue_offset[i] = queue_id_out[i] * sizeof(int32_t);
     }
+
+    queues_out_0[stage_i][pe_i] = (uint32_t*)((uint32_t)(&__seq_start) + tile_offset[0] + core_offset[0] + queue_offset[0]);
+    queues_out_1[stage_i][pe_i] = (uint32_t*)((uint32_t)(&__seq_start) + tile_offset[1] + core_offset[1] + queue_offset[1]);
+    queues_out_2[stage_i][pe_i] = (uint32_t*)((uint32_t)(&__seq_start) + tile_offset[2] + core_offset[2] + queue_offset[2]);
+    queues_out_3[stage_i][pe_i] = (uint32_t*)((uint32_t)(&__seq_start) + tile_offset[3] + core_offset[3] + queue_offset[3]);
   }
 }
 
 //first fft stage
 void systolic_first_fft_pe(uint32_t stage_idx, uint32_t idx_in_stage){
   int32_t O0, O1, O2, O3;
-  int32_t *queue_next_0;
-  int32_t *queue_next_1;
-  int32_t *queue_next_2;
-  int32_t *queue_next_3;
+  uint32_t *queue_next_0;
+  uint32_t *queue_next_1;
+  uint32_t *queue_next_2;
+  uint32_t *queue_next_3;
 
   // Assign queues
-  queue_next_0 = queues_output_0[stage_idx][idx_in_stage];
-  queue_next_1 = queues_output_1[stage_idx][idx_in_stage];
-  queue_next_2 = queues_output_2[stage_idx][idx_in_stage];
-  queue_next_3 = queues_output_3[stage_idx][idx_in_stage];
+  queue_next_0 = queues_out_0[stage_idx][idx_in_stage];
+  queue_next_1 = queues_out_1[stage_idx][idx_in_stage];
+  queue_next_2 = queues_out_2[stage_idx][idx_in_stage];
+  queue_next_3 = queues_out_3[stage_idx][idx_in_stage];
 
   int16_t t0, t1, t2;
   v2s A, B, C, D, E, F, G, H;
@@ -175,7 +168,7 @@ void systolic_first_fft_pe(uint32_t stage_idx, uint32_t idx_in_stage){
   for (i=0;i<4;i++){
     sum = 0;
     temp = idx_in_stage*4 +i;
-    for (j=0;j<NUM_STAGE;j++){
+    for (j=0;j<NUM_STAGES;j++){
       digit = temp%4;
       sum = sum*4 + digit;
       temp = temp/4;
@@ -196,11 +189,11 @@ void systolic_first_fft_pe(uint32_t stage_idx, uint32_t idx_in_stage){
               :);
   for (i=0;i<NUM_ITER;i++){
     j = i%4;
-    A = *(v2s *)&pSrc[(i0[0] + j*FFTLEN) * 2U];
-    B = *(v2s *)&pSrc[(i0[1] + j*FFTLEN) * 2U];
-    C = *(v2s *)&pSrc[(i0[2] + j*FFTLEN) * 2U];
-    D = *(v2s *)&pSrc[(i0[3] + j*FFTLEN) * 2U];
-    
+    A = *(v2s *)&pSrc[(i0[0] + j*LEN_FFT) * 2U];
+    B = *(v2s *)&pSrc[(i0[1] + j*LEN_FFT) * 2U];
+    C = *(v2s *)&pSrc[(i0[2] + j*LEN_FFT) * 2U];
+    D = *(v2s *)&pSrc[(i0[3] + j*LEN_FFT) * 2U];
+
     asm volatile(//"addi %[s2], zero, 0x02;"
                 //"slli %[s2], %[s2], 0x10;"
                 //"addi %[s2], %[s2], 0x02;"
@@ -230,7 +223,7 @@ void systolic_first_fft_pe(uint32_t stage_idx, uint32_t idx_in_stage){
     O1 = (int32_t)C;
     O2 = (int32_t)D;
     O3 = (int32_t)H;
-    
+
     //Push the results to the output queue
     queue_push(queue_next_0, O0, &resp_0);
     queue_push(queue_next_1, O1, &resp_1);
@@ -244,20 +237,20 @@ void systolic_first_fft_pe(uint32_t stage_idx, uint32_t idx_in_stage){
 
 void systolic_mid_pe(uint32_t stage_idx, uint32_t idx_in_stage, uint32_t core_id){
   int32_t O0, O1, O2, O3;
-  int32_t *queue_next_0;
-  int32_t *queue_next_1;
-  int32_t *queue_next_2;
-  int32_t *queue_next_3;
+  uint32_t *queue_next_0;
+  uint32_t *queue_next_1;
+  uint32_t *queue_next_2;
+  uint32_t *queue_next_3;
   volatile int32_t *qlr_cfg_t0 = (int32_t *)QLR_CFG_T0;
   volatile int32_t *qlr_cfg_t1 = (int32_t *)QLR_CFG_T1;
   volatile int32_t *qlr_cfg_t2 = (int32_t *)QLR_CFG_T2;
   volatile int32_t *qlr_cfg_t3 = (int32_t *)QLR_CFG_T3;
   uint32_t core_offset = core_id * sizeof(int32_t);
   // Assign queues
-  queue_next_0 = queues_output_0[stage_idx][idx_in_stage];
-  queue_next_1 = queues_output_1[stage_idx][idx_in_stage];
-  queue_next_2 = queues_output_2[stage_idx][idx_in_stage];
-  queue_next_3 = queues_output_3[stage_idx][idx_in_stage];
+  queue_next_0 = queues_out_0[stage_idx][idx_in_stage];
+  queue_next_1 = queues_out_1[stage_idx][idx_in_stage];
+  queue_next_2 = queues_out_2[stage_idx][idx_in_stage];
+  queue_next_3 = queues_out_3[stage_idx][idx_in_stage];
 
   int32_t reqs = NUM_ITER;
   //Configure QLRs
@@ -275,7 +268,7 @@ void systolic_mid_pe(uint32_t stage_idx, uint32_t idx_in_stage, uint32_t core_id
   qlr_cfg_t1[QLR_CFG_TYPE] = 1;
   qlr_cfg_t2[QLR_CFG_TYPE] = 1;
   qlr_cfg_t3[QLR_CFG_TYPE] = 1;
-  
+
   //twiddle coef calculation
   v2s CoSi1, CoSi2, CoSi3;
   v2s C1, C2, C3;
@@ -283,7 +276,7 @@ void systolic_mid_pe(uint32_t stage_idx, uint32_t idx_in_stage, uint32_t core_id
   int32_t ic, nvar_group_by_4, twiddle_multiplier;
 
   nvar_group_by_4 = 1 << (2*(stage_idx));//4^(stage_idx)
-  twiddle_multiplier = 1 <<(2*(NUM_STAGE-stage_idx-1));//4^(NUM_STAGE-stage_idx-1)
+  twiddle_multiplier = 1 <<(2*(NUM_STAGES-stage_idx-1));//4^(NUM_STAGES-stage_idx-1)
 
   ic = (idx_in_stage % nvar_group_by_4) * twiddle_multiplier;
 
@@ -311,7 +304,7 @@ void systolic_mid_pe(uint32_t stage_idx, uint32_t idx_in_stage, uint32_t core_id
                 :);
 
   v2s A, B, C, D, E, F, G, H;
-  
+
 // Radix4 calculation
   v2s s1;
   int32_t resp_0 __attribute__((unused)) = 0;
@@ -347,7 +340,7 @@ void systolic_mid_pe(uint32_t stage_idx, uint32_t idx_in_stage, uint32_t core_id
                 "srai  %[t5],%[F],0x10;"
                 "pv.pack %[B],%[t1],%[t0];"
                 "pv.pack %[D],%[t5],%[t4];"
-                "pv.pack %[C],%[t3],%[t2];"         
+                "pv.pack %[C],%[t3],%[t2];"
                 "pv.sra.h  %[A],%[A],%[s1];"
                 "pv.sub.h  %[H],%[B],%[D];"
                 "pv.add.h  %[E],%[A],%[C];"
@@ -411,7 +404,7 @@ void systolic_end_pe(uint32_t stage_idx, uint32_t idx_in_stage, uint32_t core_id
   qlr_cfg_t1[QLR_CFG_TYPE] = 1;
   qlr_cfg_t2[QLR_CFG_TYPE] = 1;
   qlr_cfg_t3[QLR_CFG_TYPE] = 1;
-  
+
   //twiddle coef calculation
   v2s CoSi1, CoSi2, CoSi3;
   v2s C1, C2, C3;
@@ -440,13 +433,13 @@ void systolic_end_pe(uint32_t stage_idx, uint32_t idx_in_stage, uint32_t core_id
                   [t4] "=&r"(t4), [t5] "=&r"(t5)
                 : [CoSi1] "r"(CoSi1), [CoSi2] "r"(CoSi2), [CoSi3] "r"(CoSi3)
                 :);
-  
+
   v2s A, B, C, D, E, F, G, H;
   uint32_t i0,i1,i2,i3;
-  i0 = shuffling_order[3*FFTLEN + 4*idx_in_stage];
-  i1 = shuffling_order[3*FFTLEN + 4*idx_in_stage + 1];
-  i2 = shuffling_order[3*FFTLEN + 4*idx_in_stage + 2];
-  i3 = shuffling_order[3*FFTLEN + 4*idx_in_stage + 3];
+  i0 = shuffling_order[3][4*idx_in_stage + 0];
+  i1 = shuffling_order[3][4*idx_in_stage + 1];
+  i2 = shuffling_order[3][4*idx_in_stage + 2];
+  i3 = shuffling_order[3][4*idx_in_stage + 3];
 //Radix4 calculation
   v2s s1;
   asm volatile("addi %[s1], zero, 0x01;"
@@ -460,7 +453,7 @@ void systolic_end_pe(uint32_t stage_idx, uint32_t idx_in_stage, uint32_t core_id
     D = (v2s)qlr_t3;
     A = (v2s)qlr_t0;
     C = (v2s)qlr_t2;
-    
+
     asm volatile(//"addi %[s1], zero, 0x01;"
                 //"slli %[s1], %[s1], 0x10;"
                 //"addi %[s1], %[s1], 0x01;"
@@ -478,7 +471,7 @@ void systolic_end_pe(uint32_t stage_idx, uint32_t idx_in_stage, uint32_t core_id
                 "srai  %[t5],%[F],0x10;"
                 "pv.pack %[B],%[t1],%[t0];"
                 "pv.pack %[D],%[t5],%[t4];"
-                "pv.pack %[C],%[t3],%[t2];"         
+                "pv.pack %[C],%[t3],%[t2];"
                 "pv.sra.h  %[A],%[A],%[s1];"
                 "pv.sub.h  %[H],%[B],%[D];"
                 "pv.add.h  %[E],%[A],%[C];"
