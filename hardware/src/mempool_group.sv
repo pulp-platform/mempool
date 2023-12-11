beeb6627aedea4f89d254450de47e7cebeb798bc
Yichao Zhang <yiczhang@iis.ee.ethz.ch>
2023-12-11 16:58:24 +0100

    [Env] Update some configurations, include the fifo size and DRAM configuration

--- hardware/src/mempool_group.sv
+++ hardware/src/mempool_group.sv
@@ -565,7 +565,7 @@ module mempool_group
       .DmaRegionWidth (NumBanksPerGroup*4/NumDmasPerGroup      ),
       .DmaRegionStart (TCDMBaseAddr                            ),
       .DmaRegionEnd   (TCDMBaseAddr+TCDMSize                   ),
-      .TransFifoDepth (8                                       ),
+      .TransFifoDepth (16                                      ),
       .burst_req_t    (dma_req_t                               ),
       .meta_t         (dma_meta_t                              )
     ) i_idma_distributed_midend (
@@ -975,7 +975,7 @@ module mempool_group
       .DmaRegionWidth (NumBanksPerGroup*4/NumDmasPerGroup),
       .DmaRegionStart (TCDMBaseAddr                      ),
       .DmaRegionEnd   (TCDMBaseAddr+TCDMSize             ),
-      .TransFifoDepth (8                                 ),
+      .TransFifoDepth (16                                ),
       .burst_req_t    (dma_req_t                         ),
       .meta_t         (dma_meta_t                        )
     ) i_idma_distributed_midend (
@@ -1025,8 +1025,8 @@ module mempool_group
         .DataWidth       (AxiDataWidth   ),
         .AddrWidth       (AddrWidth      ),
         .IdWidth         (AxiTileIdWidth ),
-        .AxReqFifoDepth  (2              ),
-        .TransFifoDepth  (1              ),
+        .AxReqFifoDepth  (8              ),
+        .TransFifoDepth  (4              ),
         .BufferDepth     (4              ),
         .axi_req_t       (axi_tile_req_t ),
         .axi_res_t       (axi_tile_resp_t),
