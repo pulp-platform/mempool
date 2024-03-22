21089797acf77f67f66ab1979db51b5cd6e80bd8
Yichao Zhang <yiczhang@iis.ee.ethz.ch>
2024-03-22 08:40:05 +0100

    [DRAMsys] Remove the local version of DRAMsys hardware folder, add the open-sourced DRAMsys as a submodule

--- hardware/dram_rtl_sim/vsim/start.tcl
+++
@@ -1,8 +0,0 @@
-# Copyright 2021 ETH Zurich and University of Bologna.
-# Solderpad Hardware License, Version 0.51, see LICENSE for details.
-# SPDX-License-Identifier: SHL-0.51
-
-set StdArithNoWarnings 1
-set NumericStdNoWarnings 1
-log -r /*
-run -a
