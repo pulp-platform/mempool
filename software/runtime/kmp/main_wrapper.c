d753d0a3d6caac2dd4b417502b237ffa9a92b1fb
sant0s12 <diego.delossantos@mailbox.org>
2024-03-26 09:19:28 +0100

    [software] Call real main only from core 0

--- software/runtime/kmp/main_wrapper.c
+++ software/runtime/kmp/main_wrapper.c
@@ -1,8 +1,14 @@
 #include "printf.h"
+#include "runtime.h"
 
 int __real_main();
 
 int __wrap_main() {
-  printf("Wrapper main");
-  return __real_main();
+  mempool_id_t core_id = mempool_get_core_id();
+  if (core_id == 0) {
+    return __real_main();
+  } else {
+    // TODO: Fetch work
+    return 0;
+  }
 }
