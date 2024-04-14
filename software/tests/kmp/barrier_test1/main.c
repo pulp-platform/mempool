848bac9eead27b3c9237bd8ff3a4ef4fa92c8de0
sant0s12 <diego.delossantos@mailbox.org>
2024-04-14 14:58:33 +0200

    [software] Port barrier_test1 to kmp

---
+++ software/tests/kmp/barrier_test1/main.c
@@ -0,0 +1,64 @@
+// Copyright 2022 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+#include <stdint.h>
+#include <string.h>
+
+#include "encoding.h"
+#include "omp.h"
+#include "printf.h"
+#include "runtime.h"
+#include "synchronization.h"
+
+#define REPETITIONS 1000 /* Number of times to run each test */
+#define SLEEPTIME 1000
+
+uint32_t test_omp_barrier() {
+  uint32_t result1;
+  uint32_t result2;
+  result1 = 0;
+  result2 = 0;
+
+#pragma omp parallel
+  {
+    uint32_t rank;
+    rank = omp_get_thread_num();
+    if (rank == 1) {
+      printf("waiting...\n");
+      mempool_wait(((double)SLEEPTIME) /
+                   REPETITIONS); // give 1 sec to whole test
+      printf("waited.\n");
+      result2 = 3;
+    }
+#pragma omp barrier
+
+    if (rank == 2) {
+      printf("result2: %d\n", result2);
+      result1 = result2;
+      printf("result1: %d\n", result1);
+    }
+  }
+  return (result1 == 3);
+}
+
+int main() {
+  uint32_t core_id = mempool_get_core_id();
+  uint32_t num_cores = mempool_get_core_count();
+  uint32_t i;
+  uint32_t num_failed = 0;
+
+  printf("Master Thread start\n");
+  for (i = 0; i < REPETITIONS; i++) {
+    printf("test: %d\n", i);
+    if (!test_omp_barrier()) {
+      num_failed++;
+    }
+    printf("test finished: %d\n", i);
+  }
+  printf("Master Thread end\n\n\n");
+  printf("num_failed: %d\n", num_failed);
+  mempool_wait(4 * num_cores);
+
+  return 0;
+}
