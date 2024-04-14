e0ecf077f9a01a097f36b0b4be002bf7e992a936
sant0s12 <diego.delossantos@mailbox.org>
2024-04-14 14:55:42 +0200

    [software] Implement mutex

---
+++ software/runtime/kmp/mutex.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <atomic>
+#include <cstdint>
+
+namespace kmp {
+class Mutex {
+public:
+  void lock();
+  bool tryLock();
+  void unlock();
+
+private:
+  std::atomic<bool> locked = false;
+};
+} // namespace kmp
