3ce669835d52394ea9932c0265b42f1fa4bdd14b
sant0s12 <diego.delossantos@mailbox.org>
2024-05-11 19:03:47 +0200

    [software] Add minpool config with no xpulp extensions

---
+++ config/minpool-no-xpulp.mk
@@ -0,0 +1,42 @@
+# Copyright 2021 ETH Zurich and University of Bologna.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
+
+# Author: Matheus Cavalcante, ETH Zurich
+
+###############
+##  MinPool  ##
+###############
+
+# Number of cores
+num_cores ?= 16
+
+# Number of groups
+num_groups ?= 4
+
+# Number of cores per MemPool tile
+num_cores_per_tile ?= 4
+
+# L1 scratchpad banking factor
+banking_factor ?= 4
+
+#########################
+##  AXI configuration  ##
+#########################
+# AXI bus data width (in bits)
+axi_data_width ?= 256
+
+# Read-only cache line width in AXI interconnect (in bits)
+ro_line_width ?= 256
+
+# Number of DMA backends in each group
+dmas_per_group ?= 1
+
+# Radix for hierarchical AXI interconnect
+axi_hier_radix ?= 2
+
+# Number of AXI masters per group
+axi_masters_per_group ?= 1
+
+# Disable Xpulpimg
+xpulpimg ?= 0
