d992a7d61300647893ef58c3a9d93ffe9caba74c
sant0s12 <diego.delossantos@mailbox.org>
2024-05-12 17:35:23 +0200

    [software] Implement automated testing

---
+++ software/runtime/testing.h
@@ -0,0 +1,88 @@
+// Inspired by https://jera.com/techinfo/jtns/jtn002
+
+#pragma once
+
+#include "printf.h"
+
+#define MAX_TESTS 20
+
+typedef struct {
+  const char *name;
+  void (*func)(char const **out_error_message);
+} test_t;
+
+test_t tests[MAX_TESTS] __attribute__((section(".l2"))); // NOLINT
+
+int tests_run = 0;    // NOLINT(*-global-variables)
+int tests_failed = 0; // NOLINT(*-global-variables)
+int num_tests = 0;    // NOLINT(*-global-variables)
+
+// NOLINTNEXTLINE
+#define ASSERT_TRUE(condition, error_message)                                  \
+  do {                                                                         \
+    if (!(condition)) {                                                        \
+      *out_error_message = (error_message);                                    \
+      return;                                                                  \
+    }                                                                          \
+  } while (0)
+
+// NOLINTNEXTLINE
+#define ASSERT_EQ(left, right)                                                 \
+  do {                                                                         \
+    if (!((left) == (right))) {                                                \
+      *out_error_message = #left " is not equal to " #right;                   \
+      return;                                                                  \
+    }                                                                          \
+  } while (0)
+
+// NOLINTNEXTLINE
+#define EXPECT_TRUE(condition, error_message)                                  \
+  do {                                                                         \
+    if (!(condition))                                                          \
+      printf("\t[CHECK FAILED]: %s\n", (error_message));                       \
+  } while (0)
+
+// NOLINTNEXTLINE
+#define RUN_TEST(test)                                                         \
+  do {                                                                         \
+    printf("\n[RUNNING]: %s \n", (test).name);                                 \
+    const char *message = NULL;                                                \
+    ((test).func)(&message);                                                   \
+    tests_run++;                                                               \
+    if (message != NULL) {                                                     \
+      tests_failed++;                                                          \
+      printf("\t[ASSERTION FAILED]: %s\n", message);                           \
+      printf("[FAIL]: %s\n", (test).name);                                     \
+    } else {                                                                   \
+      printf("[SUCCESS]: %s\n", (test).name);                                  \
+    }                                                                          \
+  } while (0)
+
+// NOLINTNEXTLINE
+#define RUN_ALL_TESTS()                                                        \
+  do {                                                                         \
+    for (int i = 0; i < num_tests; i++) {                                      \
+      RUN_TEST(tests[i]);                                                      \
+    }                                                                          \
+  } while (0)
+
+// NOLINTNEXTLINE
+#define PRINT_TEST_RESULTS()                                                   \
+  do {                                                                         \
+    printf("Ran %d tests\n", tests_run);                                       \
+    printf("Failed %d tests\n", tests_failed);                                 \
+  } while (0)
+
+// NOLINTNEXTLINE
+#define TEST(testname)                                                         \
+  void testname(char const **out_error_message);                               \
+  __attribute__((constructor)) void add_##testname##_to_array(void) {          \
+    if (num_tests < MAX_TESTS) {                                               \
+      tests[num_tests].name = #testname;                                       \
+      tests[num_tests].func = testname;                                        \
+      num_tests++;                                                             \
+    } else {                                                                   \
+      printf("Too many tests added, max is %s\n", MAX_TESTS);                  \
+    }                                                                          \
+  }                                                                            \
+  void testname(char const **out_error_message)
