717334f174fa5e262fad4cded6f7bb557443c734
sant0s12 <diego.delossantos@mailbox.org>
2024-05-17 16:27:41 +0200

    [software] Update master test

--- software/tests/kmp/omp_parallel/main.c
+++ software/tests/kmp/omp_parallel/main.c
@@ -10,26 +10,44 @@
 #include "printf.h"
 #include "runtime.h"
 #include "synchronization.h"
+#include "testing.h"
 
-extern volatile uint32_t tcdm_start_address_reg;
-extern volatile uint32_t tcdm_end_address_reg;
+#define REPETITIONS 10 /* Number of times to run each test */
+
+TEST(test_omp_parallel_8) {
+  for (int i = 0; i < REPETITIONS; i++) {
+    printf("Master Thread: Parallel start\n");
+    int nthreads = 0;
+
+#pragma omp parallel num_threads(8)
+    {
+      nthreads = omp_get_num_threads();
+      printf("%d\n", omp_get_num_threads());
+    }
+    printf("Master Thread: Parallel end\n\n\n");
+    ASSERT_EQ(8, nthreads);
+  }
+}
+
+TEST(test_omp_parallel) {
+  for (int i = 0; i < REPETITIONS; i++) {
+    printf("Master Thread: Parallel start\n");
+    int nthreads = 0;
+
+    printf("Master Thread: Parallel start\n");
+#pragma omp parallel
+    {
+      nthreads = omp_get_num_threads();
+      printf("%d\n", omp_get_num_threads());
+    }
+    printf("Master Thread: Parallel end\n\n\n");
+    ASSERT_EQ(NUM_CORES, nthreads);
+  }
+}
 
 int main() {
-  uint32_t core_id = mempool_get_core_id();
-
-  if (core_id == 0) {
-    printf("Master Thread: Parallel start\n");
-    mempool_wait(1000);
-#pragma omp parallel num_threads(8)
-    { printf("%d\n", omp_get_num_threads()); }
-    printf("Master Thread: Parallel end\n\n\n");
-
-    printf("Master Thread: Parallel start\n");
-    mempool_wait(1000);
-#pragma omp parallel
-    { printf("%d\n", omp_get_num_threads()); }
-    printf("Master Thread: Parallel end\n\n\n");
-  }
+  RUN_ALL_TESTS();
+  PRINT_TEST_RESULTS();
 
   return 0;
 }
--- software/tests/kmp/omp_parallel_for/main.c
+++ software/tests/kmp/omp_parallel_for/main.c
@@ -10,150 +10,166 @@
 #include "printf.h"
 #include "runtime.h"
 #include "synchronization.h"
+#include "testing.h"
 
-void gcc_omp_parallel_for_schedule_static(void) {
+TEST(omp_parallel_for_schedule_static) {
   int buf[64], *p;
   uint32_t i;
   int result = 0;
   memset(buf, '\0', sizeof(buf));
 
 #pragma omp parallel for
-  for (i = 0; i < omp_get_num_threads(); i++) {
-    if (omp_get_thread_num() != i) {
-      printf("Error: for loop is not executed in parallel\n");
-      result += 1;
-    }
+  for (int i = 0; i < omp_get_num_threads(); i++) {
+    buf[i] = i;
   }
 
+  for (int i = 0; i < NUM_CORES; i++) {
+    ASSERT_EQ(buf[i], i);
+  }
+
+  memset(buf, '\0', sizeof(buf));
 #pragma omp parallel for schedule(static, 3) private(p)
-  for (p = &buf[10]; p < &buf[54]; p++)
+  for (p = &buf[10]; p < &buf[54]; p++) {
     *p = 5;
-  for (i = 0; i < 64; i++)
-    if (buf[i] != 5 * (i >= 10 && i < 54)) {
-      printf("error 1 at gcc schedule static\n");
-      result += 1;
-    }
+  }
+
+  for (i = 0; i < 64; i++) {
+    ASSERT_EQ(buf[i], 5 * (i >= 10 && i < 54));
+  }
 
   memset(buf, '\0', sizeof(buf));
 #pragma omp parallel for schedule(static, 3)
-  for (p = &buf[3]; p <= &buf[63]; p += 2)
+  for (p = &buf[3]; p <= &buf[63]; p += 2) {
     p[-2] = 6;
-  for (i = 0; i < 64; i++)
-    if (buf[i] != 6 * ((i & 1) && i <= 61)) {
-      printf("error 2 at gcc schedule static\n");
-      result += 1;
-    }
-  memset(buf, '\0', sizeof(buf));
-#pragma omp parallel for schedule(static, 3)
-  for (p = &buf[16]; p < &buf[51]; p = 4 + p)
-    p[2] = 7;
-  for (i = 0; i < 64; i++)
-    if (buf[i] != 7 * ((i & 3) == 2 && i >= 18 && i < 53)) {
-      printf("error 3 at gcc schedule static\n");
-      result += 1;
-    }
-  memset(buf, '\0', sizeof(buf));
-#pragma omp parallel for schedule(static, 3)
-  for (p = &buf[16]; p <= &buf[40]; p = p + 4U)
-    p[2] = -7;
-  for (i = 0; i < 64; i++)
-    if (buf[i] != -7 * ((i & 3) == 2 && i >= 18 && i <= 42)) {
-      printf("error 4 at gcc schedule static\n");
-      result += 1;
-    }
-  memset(buf, '\0', sizeof(buf));
-#pragma omp parallel for schedule(static, 3)
-  for (p = &buf[53]; p > &buf[9]; --p)
-    *p = 5;
-  for (i = 0; i < 64; i++)
-    if (buf[i] != 5 * (i >= 10 && i < 54)) {
-      printf("error 5 at gcc schedule static\n");
-      result += 1;
-    }
-  memset(buf, '\0', sizeof(buf));
-#pragma omp parallel for schedule(static, 3)
-  for (p = &buf[63]; p >= &buf[3]; p -= 2)
-    p[-2] = 6;
-  for (i = 0; i < 64; i++)
-    if (buf[i] != 6 * ((i & 1) && i <= 61)) {
-      printf("error 6 at gcc schedule static\n");
-      result += 1;
-    }
-  memset(buf, '\0', sizeof(buf));
-#pragma omp parallel for schedule(static, 3)
-  for (p = &buf[48]; p > &buf[15]; p = -4 + p)
-    p[2] = 7;
-  for (i = 0; i < 64; i++)
-    if (buf[i] != 7 * ((i & 3) == 2 && i >= 18 && i < 53)) {
-      printf("error 7 at at gcc schedule static\n");
-      result += 1;
-    }
-  memset(buf, '\0', sizeof(buf));
-#pragma omp parallel for schedule(static, 3)
-  for (p = &buf[40]; p >= &buf[16]; p = p - 4U)
-    p[2] = -7;
-  for (i = 0; i < 64; i++)
-    if (buf[i] != -7 * ((i & 3) == 2 && i >= 18 && i <= 42)) {
-      printf("error 8 at gcc schedule static\n");
-      result += 1;
-    }
+  }
 
-  if (result == 0) {
-    printf("All test passed\n");
-  } else {
-    printf("Failed %d tests\n", result);
+  for (i = 0; i < 64; i++) {
+    ASSERT_EQ(buf[i], 6 * ((i & 1) && i <= 61));
+  }
+
+  memset(buf, '\0', sizeof(buf));
+#pragma omp parallel for schedule(static, 3)
+  for (p = &buf[16]; p < &buf[51]; p = 4 + p) {
+    p[2] = 7;
+  }
+
+  for (i = 0; i < 64; i++) {
+    ASSERT_EQ(buf[i], 7 * ((i & 3) == 2 && i >= 18 && i < 53));
+  }
+
+  memset(buf, '\0', sizeof(buf));
+#pragma omp parallel for schedule(static, 3)
+  for (p = &buf[16]; p <= &buf[40]; p = p + 4U) {
+    p[2] = -7;
+  }
+
+  for (i = 0; i < 64; i++) {
+    ASSERT_EQ(buf[i], -7 * ((i & 3) == 2 && i >= 18 && i <= 42));
+  }
+
+  memset(buf, '\0', sizeof(buf));
+#pragma omp parallel for schedule(static, 3)
+  for (p = &buf[53]; p > &buf[9]; --p) {
+    *p = 5;
+  }
+
+  for (i = 0; i < 64; i++) {
+    ASSERT_EQ(buf[i], 5 * (i >= 10 && i < 54));
+  }
+
+  memset(buf, '\0', sizeof(buf));
+#pragma omp parallel for schedule(static, 3)
+  for (p = &buf[63]; p >= &buf[3]; p -= 2) {
+    p[-2] = 6;
+  }
+
+  for (i = 0; i < 64; i++) {
+    ASSERT_EQ(buf[i], 6 * ((i & 1) && i <= 61));
+  }
+
+  memset(buf, '\0', sizeof(buf));
+#pragma omp parallel for schedule(static, 3)
+  for (p = &buf[48]; p > &buf[15]; p = -4 + p) {
+    p[2] = 7;
+  }
+
+  for (i = 0; i < 64; i++) {
+    ASSERT_EQ(buf[i], 7 * ((i & 3) == 2 && i >= 18 && i < 53));
+  }
+
+  memset(buf, '\0', sizeof(buf));
+#pragma omp parallel for schedule(static, 3)
+  for (p = &buf[40]; p >= &buf[16]; p = p - 4U) {
+    p[2] = -7;
+  }
+
+  for (i = 0; i < 64; i++) {
+    ASSERT_EQ(buf[i], -7 * ((i & 3) == 2 && i >= 18 && i <= 42));
   }
 }
 
-void gcc_omp_parallel_for_schedule_static_thread(void) {
-  printf("Testing: schedule default chunk size\n");
+TEST(parallel_for_schedule_static_thread) {
+  uint32_t buf[NUM_CORES];
+
+  memset(buf, '\0', sizeof(buf));
 #pragma omp parallel for num_threads(4) schedule(static)
   for (int i = 0; i < 10; i++) {
-    printf("%d\n", omp_get_thread_num());
+    buf[i] = omp_get_thread_num();
   }
 
-  printf("Testing: schedule chunk size 2\n");
+  int chunkSize = (10 + 4 - 1) / 4; // ceil(10/4)
+  for (int i = 0; i < 10; i++) {
+    ASSERT_EQ(buf[i], (i / chunkSize) % 4);
+  }
+
+  memset(buf, '\0', sizeof(buf));
 #pragma omp parallel for num_threads(4) schedule(static, 2)
   for (int i = 0; i < 10; i++) {
-    printf("%d\n", omp_get_thread_num());
+    buf[i] = omp_get_thread_num();
   }
 
-  printf("Testing: private\n");
+  for (int i = 0; i < 10; i++) {
+    ASSERT_EQ(buf[i], (i / 2) % 4);
+  }
+
   int A = 9;
+  memset(buf, '\0', sizeof(buf));
 #pragma omp parallel for num_threads(4) schedule(static) private(A)
   for (int i = 0; i < 4; i++) {
+    buf[i] = A;
     A = i;
-    printf("%d\n", A);
   }
-  printf("A %d\n", A);
 
-  printf("Testing: First private\n");
+  for (int i = 0; i < 4; i++) {
+    ASSERT_NEQ(buf[i], 9);
+  }
+  ASSERT_EQ(A, 9);
+
   A = 9;
+  memset(buf, '\0', sizeof(buf));
 #pragma omp parallel for num_threads(4) schedule(static) firstprivate(A)
   for (int i = 0; i < 4; i++) {
-    printf("%d\n", A);
+    buf[i] = A;
     A = i;
   }
-  printf("A %d\n", A);
 
-  printf("Testing: Last private\n");
+  for (int i = 0; i < 4; i++) {
+    ASSERT_EQ(buf[i], 9);
+  }
+  ASSERT_EQ(A, 9);
+
   A = 9;
 #pragma omp parallel for num_threads(4) schedule(static) lastprivate(A)
   for (int i = 0; i < 4; i++) {
     A = i;
   }
-  printf("A %d\n", A);
+
+  ASSERT_EQ(A, 3);
 }
 
 int main() {
-  ///////////////////////////////////////////////////////////
-  //////////////////////   test   ///////////////////////////
-  ///////////////////////////////////////////////////////////
-  printf("Running thread test\n");
-  gcc_omp_parallel_for_schedule_static_thread();
-
-  printf("Running schedule static test\n");
-  gcc_omp_parallel_for_schedule_static();
+  RUN_ALL_TESTS();
+  PRINT_TEST_RESULTS();
 
   return 0;
 }
