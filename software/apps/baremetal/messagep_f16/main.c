c5633bc36d967e9aec444990211abadb65b87070
mbertuletti <mbertuletti@iis.ee.ethz.ch>
2024-12-13 10:48:59 +0100

    [software] Add message-passing messagep_fp16 kernel

--- software/apps/baremetal/messagep_f16/main.c
+++ software/apps/baremetal/messagep_f16/main.c
@@ -16,11 +16,11 @@
 #include "baremetal/mempool_messagep_f16.h"
 #include "data_messagep_f16.h"
 
-__fp16 l1_A[matrix_N]
+__fp16 l1_A[matrix_P * matrix_M * matrix_N * matrix_D]
     __attribute__((aligned(sizeof(int32_t)), section(".l1_prio")));
-__fp16 l1_W[matrix_M * matrix_N]
+__fp16 l1_B[matrix_P * matrix_M * matrix_N * matrix_D]
     __attribute__((aligned(sizeof(int32_t)), section(".l1_prio")));
-__fp16 l1_B[matrix_M]
+__fp16 l1_HL[matrix_P * matrix_M * matrix_N * width_HL]
     __attribute__((aligned(sizeof(int32_t)), section(".l1_prio")));
 
 int main() {
@@ -31,22 +31,26 @@ int main() {
 
   // Initialize Matrices 1
   if (core_id == 0) {
-    dma_memcpy_blocking(l1_A, l2_A, (matrix_N) * sizeof(int16_t));
-    dma_memcpy_blocking(l1_W, l2_W, (matrix_M, matrix_N) * sizeof(int16_t));
-    if (BIAS == 1) {
-      dma_memcpy_blocking(l1_B, l2_B, (matrix_M) * sizeof(int16_t));
-    }
+    dma_memcpy_blocking(l1_A, l2_A,
+                        (matrix_P * matrix_M * matrix_N * matrix_D) *
+                            sizeof(int16_t));
+    dma_memcpy_blocking(l1_HL, l2_HL,
+                        (matrix_P * matrix_M * matrix_N * width_HL) *
+                            sizeof(int16_t));
   }
   mempool_barrier(num_cores);
 
   if (core_id == 0) {
     // Execute function to test.
     mempool_start_benchmark();
-    fullyconn_f16s_unrolled4(l1_A, l1_B, l1_W, matrix_M, matrix_N, BIAS, RELU);
+    messagep_f16s_unrolled4(l1_A, l1_B, matrix_P, matrix_M, matrix_N, matrix_D,
+                            FC_LAYER, l1_HL, l2_W_fc1, l2_W_fc2, width_HL, BIAS,
+                            RELU);
     mempool_stop_benchmark();
   }
   mempool_barrier(num_cores);
-  mempool_check_f16(l1_B, l2_Y, matrix_M, 0.01f, 0);
+  mempool_check_f16(l1_B, l2_B, matrix_P * matrix_M * matrix_N * matrix_D,
+                    0.01f, 0);
   mempool_barrier(num_cores);
 
   return 0;
