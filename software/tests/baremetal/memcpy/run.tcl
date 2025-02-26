a333fc48f1db76ac889a422a5d6983a00ecc2274
Yichao Zhang <yiczhang@iis.ee.ethz.ch>
2025-02-26 15:47:20 +0100

    [software] fix memcpy kernel

--- software/tests/baremetal/memcpy/run.tcl
+++
@@ -1,6 +0,0 @@
-root_dir=$(git rev-parse --show-toplevel)
-cd $root_dir/software/tests/baremetal;
-config=terapool make memcpy;
-cd $root_dir/hardware;
-app=tests/baremetal/memcpy config=terapool make sim;
-cd -;
