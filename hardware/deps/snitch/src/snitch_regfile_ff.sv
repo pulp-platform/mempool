344bd75b17cc32d34161a58f7421f99d3ed9a30f
Zexin Fu <zexifu@iis.ee.ethz.ch>
2025-04-22 11:45:24 +0200

    [snitch] Add reset signal to ff-based regfile.
    This is to avoid a backend bug: in fusion compiler, after the 07d synthesis optimization, some bits of the pc_d will be connected to the regfile, in this case if the regfile is not reset, the x signal will be propagated to the pc_q, and breaks the whole post-simulation.

--- hardware/deps/snitch/src/snitch_regfile_ff.sv
+++ hardware/deps/snitch/src/snitch_regfile_ff.sv
@@ -4,6 +4,7 @@
 
 // Author: Florian Zaruba <zarubaf@iis.ee.ethz.ch>
 // Description: Variable Register File
+// verilog_lint: waive module-filename
 module snitch_regfile #(
   parameter DATA_WIDTH     = 32,
   parameter NR_READ_PORTS  = 2,
@@ -13,6 +14,7 @@ module snitch_regfile #(
 ) (
   // clock and reset
   input  logic                                      clk_i,
+  input  logic                                      rst_i,
   // read port
   input  logic [NR_READ_PORTS-1:0][ADDR_WIDTH-1:0]  raddr_i,
   output logic [NR_READ_PORTS-1:0][DATA_WIDTH-1:0]  rdata_o,
@@ -22,36 +24,40 @@ module snitch_regfile #(
   input  logic [NR_WRITE_PORTS-1:0]                 we_i
 );
 
-  localparam    NUM_WORDS  = 2**ADDR_WIDTH;
+  localparam int unsigned NUM_WORDS  = 2**ADDR_WIDTH;
 
   logic [NUM_WORDS-1:0][DATA_WIDTH-1:0]     mem;
   logic [NR_WRITE_PORTS-1:0][NUM_WORDS-1:0] we_dec;
 
 
-    always_comb begin : we_decoder
-      for (int unsigned j = 0; j < NR_WRITE_PORTS; j++) begin
-        for (int unsigned i = 0; i < NUM_WORDS; i++) begin
-          if (waddr_i[j] == i) we_dec[j][i] = we_i[j];
-          else we_dec[j][i] = 1'b0;
-        end
+  always_comb begin : we_decoder
+    for (int unsigned j = 0; j < NR_WRITE_PORTS; j++) begin
+      for (int unsigned i = 0; i < NUM_WORDS; i++) begin
+        if (waddr_i[j] == i) we_dec[j][i] = we_i[j];
+        else we_dec[j][i] = 1'b0;
       end
     end
+  end
 
-    // loop from 1 to NUM_WORDS-1 as R0 is nil
-    always_ff @(posedge clk_i) begin : register_write_behavioral
+  // loop from 1 to NUM_WORDS-1 as R0 is nil
+  always_ff @(posedge clk_i or posedge rst_i) begin : register_write_behavioral
+    if (rst_i) begin
+      mem <= '0;
+    end else begin
       for (int unsigned j = 0; j < NR_WRITE_PORTS; j++) begin
         for (int unsigned i = 0; i < NUM_WORDS; i++) begin
           if (we_dec[j][i]) begin
             mem[i] <= wdata_i[j];
           end
         end
-        if (ZERO_REG_ZERO) begin
-          mem[0] <= '0;
-        end
+      end
+      if (ZERO_REG_ZERO) begin
+        mem[0] <= '0;
       end
     end
+  end
 
-  for (genvar i = 0; i < NR_READ_PORTS; i++) begin
+  for (genvar i = 0; i < NR_READ_PORTS; i++) begin : gen_read_port
     assign rdata_o[i] = mem[raddr_i[i]];
   end
 
