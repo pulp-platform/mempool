1efdec446587527b0d7428ff52533605bd12c6ac
mbertuletti <mbertuletti@iis.ee.ethz.ch>
2025-05-02 17:01:55 +0200

    [hardware] Burst remote requests

--- hardware/src/tcdm_burst/src/tcdm_burst_pkg.sv
+++ hardware/src/tcdm_burst/src/tcdm_burst_pkg.sv
@@ -3,6 +3,7 @@
 // SPDX-License-Identifier: SHL-0.51
 //
 // Author: Diyou Shen, ETH Zurich
+// Author: Marco Bertuletti ETH Zurich
 
 // Description
 // Include TCDM burst types and grouped response types
@@ -12,17 +13,18 @@ package tcdm_burst_pkg;
    *  TCDM BURST PARAMETERS  *
    ***************************/
   localparam bit UseBurst = `ifdef TCDM_BURST `TCDM_BURST `else 0 `endif;
+  localparam integer unsigned BurstLen = `ifdef TCDM_BURSTLEN `TCDM_BURSTLEN `else 1 `endif;
+  localparam integer unsigned NumCuts = 1;
 
   // Maximum length of burst
-  parameter int unsigned MaxBurstLen   = UseBurst ? 4 : 1;
-
+  parameter int unsigned MaxBurstLen   = UseBurst ? BurstLen : 1;
   // 0 to MaxBurstLen
-  parameter int unsigned BurstLenWidth = $clog2(MaxBurstLen);
+  parameter int unsigned MaxBurstLenWidth = $clog2(MaxBurstLen);
 
   typedef struct packed {
     logic isburst;
-    logic [BurstLenWidth:0] blen;
-  } tcdm_breq_t;
+    logic [MaxBurstLenWidth-1:0] blen;
+  } burst_t;
 
   /*********************************
    *  TCDM Grouped Rsp PARAMETERS  *
@@ -30,25 +32,14 @@ package tcdm_burst_pkg;
 
   // Grouped Response Extension
   localparam bit GroupRsp = `ifdef GROUP_RSP `GROUP_RSP `else 0 `endif;
-  localparam bit ParMgr = `ifdef PARALLEL_MANAGER `PARALLEL_MANAGER `else 0 `endif;
 
   // Grouping Factor of response data
   localparam integer unsigned RspGF = GroupRsp ? 2 : 1;
 
-  // Parallel Burst Manager
-  localparam bit ParallelManager = (RspGF > 1) ? ParMgr : 0;
-
-  `ifdef GROUP_RSP
-    typedef struct packed {
-      logic  [RspGF-2:0][31:0] data;
-      logic  valid;
-    } tcdm_gre_t;
-  `else
-    // still define the type but mimize the wire if not used
-    typedef struct packed {
-      logic data;
-      logic valid;
-    } tcdm_gre_t;
-  `endif
+  // replace rdata payload with this when the response is grouped
+  typedef struct packed {
+    logic  [RspGF-1:0][31:0] data;
+    logic  [RspGF-1:0] we;
+  } burst_gresp_t;
 
 endpackage : tcdm_burst_pkg
