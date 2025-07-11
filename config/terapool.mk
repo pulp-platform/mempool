1010b3cb7f1f933f0dca5411915b75ff31d82413
Yichao Zhang <yiczhang@iis.ee.ethz.ch>
2025-07-11 13:20:31 +0200

    [CI] Update license checking for config files

--- config/terapool.mk
+++ config/terapool.mk
@@ -1,10 +1,8 @@
-# ============================================================================
-#  Copyright 2025 ETH Zurich and University of Bologna.
-#  Licensed under the Apache License, Version 2.0, see LICENSE for details.
-#  SPDX-License-Identifier: Apache-2.0
+# Copyright 2025 ETH Zurich and University of Bologna.
+# Licensed under the Apache License, Version 2.0, see LICENSE for details.
+# SPDX-License-Identifier: Apache-2.0
 #
-#  Author: Yichao Zhang, ETH Zurich
-# ============================================================================
+# Author: Yichao Zhang, ETH Zurich
 
 ###########################
 ## 1. Architecture Config
