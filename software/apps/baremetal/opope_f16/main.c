5391e87413a43a78395bc4dce2d3073d66b616f5
sem25h14 Vittorio Bonacina (vbonacina) <sem25h14@pisoc8.ee.ethz.ch>
2025-10-13 18:44:59 +0200

    Modified Runtime and deps/opope, sw compiles (commented tiny printf in hal_opope.h

---
+++ software/apps/baremetal/opope_f16/main.c
@@ -0,0 +1,93 @@
+// Copyright 2021 ETH Zurich and University of Bologna.
+// Licensed under the Apache License, Version 2.0, see LICENSE for details.
+// SPDX-License-Identifier: Apache-2.0
+
+// Author: Marco Bertuletti, ETH Zurich
+
+#include <stdint.h>
+#include <string.h>
+
+#include "dma.h"
+#include "encoding.h"
+#include "printf.h"
+#include "runtime.h"
+#include "synchronization.h"
+
+#include "archi_opope.h"
+#include "hal_opope.h"
+
+
+#include "baremetal/mempool_checks.h"
+#include "data_gemm_f16.h"
+
+#if (NUM_CORES > 256)
+#define ID_OPOPE_CORE (896)
+#else
+#define ID_OPOPE_CORE (240)
+#endif
+
+#define PARALLEL
+
+__fp16 l1_W[matrix_N * matrix_P]
+    __attribute__((aligned(sizeof(int32_t)), section(".l1_prio")));
+__fp16 l1_Y[matrix_M * matrix_P]
+    __attribute__((aligned(sizeof(int32_t)), section(".l1_prio")));
+__fp16 l1_X[matrix_M * matrix_N]
+    __attribute__((aligned(sizeof(int32_t)), section(".l1_prio")));
+
+int main() {
+  uint32_t core_id = mempool_get_core_id();
+  uint32_t num_cores = mempool_get_core_count();
+  mempool_barrier_init(core_id);
+
+  if (core_id == 0) {
+    dma_memcpy_blocking(l1_X, l2_X, (matrix_M * matrix_N) * sizeof(int16_t));
+    dma_memcpy_blocking(l1_W, l2_W, (matrix_N * matrix_P) * sizeof(int16_t));
+    dma_memcpy_blocking(l1_Y, l2_Y, (matrix_M * matrix_P) * sizeof(int16_t));
+  }
+  mempool_barrier(num_cores);
+
+#ifdef SINGLE
+  if (core_id == ID_OPOPE_CORE) {
+    mempool_start_benchmark();
+    unsigned int X_ptr = (unsigned int) (l1_X);
+    unsigned int Y_ptr = (unsigned int) (l1_Y);
+    unsigned int W_ptr = (unsigned int) (l1_W);
+    hwpe_soft_clear();
+    mempool_wait(10);
+    opope_cfg(X_ptr, W_ptr, Y_ptr, matrix_M, matrix_N, matrix_P, GEMM, Float16);
+    mempool_wait(10);
+    // Start OPOPE operation
+    hwpe_trigger_job();
+    // Go to sleep
+    mempool_wfi();
+  }
+  mempool_barrier(num_cores);
+  mempool_stop_benchmark();
+#endif
+
+#ifdef PARALLEL
+  uint32_t opope_id = (core_id >= ID_OPOPE_CORE) ? core_id - ID_OPOPE_CORE : (uint32_t)(-1);
+  uint32_t num_opopes = 4;
+  if (opope_id < 4) {
+    unsigned int X_ptr = (unsigned int) (l1_X + opope_id * matrix_N * (matrix_M / num_opopes));
+    unsigned int Y_ptr = (unsigned int) (l1_Y + opope_id * matrix_P * (matrix_M / num_opopes));
+    unsigned int W_ptr = (unsigned int) (l1_W);
+    mempool_start_benchmark();
+    hwpe_soft_clear();
+    mempool_wait(10);
+    opope_cfg(X_ptr, W_ptr, Y_ptr, (matrix_M / num_opopes), matrix_N, matrix_P, GEMM, Float16, Float16);
+    mempool_wait(10);
+    // Start OPOPE operation
+    hwpe_trigger_job();
+    // Go to sleep
+    mempool_wfi();
+  }
+  mempool_barrier(num_cores);
+  mempool_stop_benchmark();
+#endif
+
+  mempool_check_f16(l1_Y, l2_Z, 10, 0.05f, 0);
+  mempool_barrier(num_cores);
+  return 0;
+}
