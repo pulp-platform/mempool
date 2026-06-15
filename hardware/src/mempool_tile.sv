29710536c149eae727347f3eacc34f6e091e53d6
mbertuletti <mbertuletti@iis.ee.ethz.ch>
2026-06-15 09:32:08 +0200

    [hardware] Bump HCI

--- hardware/src/mempool_tile.sv
+++ hardware/src/mempool_tile.sv
@@ -1107,7 +1107,7 @@ module mempool_tile
       EHW: 0
     };
 
-    hci_outstanding_intf #(
+    hci_variablelatency_intf #(
       .DW (RMDataWidth),
       .UW (idx_width(RMOutstandingTransactions)),
       .IW (idx_width(RMNumStreams))
