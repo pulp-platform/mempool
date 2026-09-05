97f3d053a3a4f13eea41887049367cf2632894da
mbertuletti <mbertuletti@iis.ee.ethz.ch>
2026-09-05 09:19:40 +0200

    [software] Silence warnings from RedMulE API
    x

--- software/apps/baremetal/gemm_f16/main.c
+++ software/apps/baremetal/gemm_f16/main.c
@@ -19,21 +19,27 @@
 #include "baremetal/mempool_checks.h"
 #include "data_gemm_f16.h"
 
-#define ELEMENTS_PER_ROW (NUM_BANKS * sizeof(int32_t) / sizeof(int16_t) )
+#define ELEMENTS_PER_ROW (NUM_BANKS * sizeof(int32_t) / sizeof(int16_t))
 #define PORT_WIDTH (REDMULE_H * (REDMULE_P + 1))
 
-__fp16 l1_X[(matrix_M * matrix_N) + PORT_WIDTH*NUM_REDMULE_TILES*(NUM_REDMULE_TILES+1)]
-    __attribute__((aligned(NUM_BANKS*sizeof(int32_t)), section(".l1_prio")));
+#ifndef SINGLE
+#ifndef PARALLEL
+#define SINGLE
+#endif
+#endif
+
+__fp16 l1_X[(matrix_M * matrix_N) +
+            PORT_WIDTH * NUM_REDMULE_TILES * (NUM_REDMULE_TILES + 1)]
+    __attribute__((aligned(NUM_BANKS * sizeof(int32_t)), section(".l1_prio")));
 __fp16 l1_W[matrix_N * matrix_P]
-    __attribute__((aligned(NUM_BANKS*sizeof(int32_t)), section(".l1_prio")));
+    __attribute__((aligned(NUM_BANKS * sizeof(int32_t)), section(".l1_prio")));
 __fp16 l1_Y[matrix_M * matrix_P]
-    __attribute__((aligned(NUM_BANKS*sizeof(int32_t)), section(".l1_prio")));
+    __attribute__((aligned(NUM_BANKS * sizeof(int32_t)), section(".l1_prio")));
 
 int main() {
   uint32_t core_id = mempool_get_core_id();
   uint32_t num_cores = mempool_get_core_count();
   uint32_t redmule_id = mempool_get_redmule_id();
-  uint32_t num_redmules = mempool_get_redmule_count();
   mempool_barrier_init(core_id);
 
 #ifdef SINGLE
@@ -46,12 +52,13 @@ int main() {
   mempool_barrier(num_cores);
   // Compute
   if (redmule_id == 0) {
-    unsigned int X_ptr = (unsigned int) (l1_X);
-    unsigned int Y_ptr = (unsigned int) (l1_Y);
-    unsigned int W_ptr = (unsigned int) (l1_W);
+    unsigned int X_ptr = (unsigned int)(l1_X);
+    unsigned int Y_ptr = (unsigned int)(l1_Y);
+    unsigned int W_ptr = (unsigned int)(l1_W);
     hwpe_soft_clear();
     mempool_wait(10);
-    redmule_cfg(X_ptr, W_ptr, Y_ptr, matrix_M, matrix_N, matrix_P, 0, GEMM, Float16);
+    redmule_cfg(X_ptr, W_ptr, Y_ptr, matrix_M, matrix_N, matrix_P, 0, GEMM,
+                Float16);
     mempool_wait(10);
     mempool_start_benchmark();
     // Start RedMulE operation
@@ -66,7 +73,8 @@ int main() {
 #ifdef PARALLEL
 
   uint32_t X_shift;
-  uint32_t W_shift;
+  uint16_t W_shift;
+  uint32_t num_redmules = mempool_get_redmule_count();
 
   // Transfer
   if (redmule_id == 0) {
@@ -75,8 +83,7 @@ int main() {
       dma_memcpy_blocking(
           l1_X + i * (matrix_M * matrix_N / num_redmules) + X_shift,
           l2_X + i * (matrix_M * matrix_N / num_redmules),
-          (matrix_M * matrix_N / num_redmules) * sizeof(int16_t)
-      );
+          (matrix_M * matrix_N / num_redmules) * sizeof(int16_t));
     }
     dma_memcpy_blocking(l1_W, l2_W, (matrix_N * matrix_P) * sizeof(int16_t));
     dma_memcpy_blocking(l1_Y, l2_Y, (matrix_M * matrix_P) * sizeof(int16_t));
@@ -87,12 +94,18 @@ int main() {
   if (redmule_id < num_redmules) {
     X_shift = (XSHIFT == 1) ? (redmule_id * PORT_WIDTH) % matrix_N : 0;
     W_shift = (WSHIFT == 1) ? (redmule_id * PORT_WIDTH) % matrix_P : 0;
-    unsigned int X_ptr = (unsigned int) (l1_X + redmule_id * (matrix_M * matrix_N / num_redmules) + X_shift);
-    unsigned int Y_ptr = (unsigned int) (l1_Y + redmule_id * (matrix_M * matrix_P / num_redmules));
-    unsigned int W_ptr = (unsigned int) (l1_W);
+    unsigned int X_ptr =
+        (unsigned int)(l1_X +
+                       redmule_id * (matrix_M * matrix_N / num_redmules) +
+                       X_shift);
+    unsigned int Y_ptr =
+        (unsigned int)(l1_Y +
+                       redmule_id * (matrix_M * matrix_P / num_redmules));
+    unsigned int W_ptr = (unsigned int)(l1_W);
     hwpe_soft_clear();
     mempool_wait(10);
-    redmule_cfg(X_ptr, W_ptr, Y_ptr, (matrix_M / num_redmules), matrix_N, matrix_P, W_shift, GEMM, Float16);
+    redmule_cfg(X_ptr, W_ptr, Y_ptr, (matrix_M / num_redmules), matrix_N,
+                matrix_P, W_shift, GEMM, Float16);
     mempool_wait(10);
     mempool_start_benchmark();
     // Start RedMulE operation
